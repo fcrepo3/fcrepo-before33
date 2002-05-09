7113db8e9f58ac90b0824a5d766fd0d3bdba5cde
Sandy Payette <spayette@duraspace.org>
2002-05-09 21:33:02 +0000

    A class to define a method parameter, for use within a MethodDef in Fedora.

---
+++ src/java/fedora/server/behavior/MethodParmDef.java
@@ -0,0 +1,24 @@
+package fedora.server.behavior;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class MethodParmDef
+{
+
+    public String parmName;
+    public String parmDefaultValue;
+    public boolean parmRequired;
+    public String parmLabel;
+
+    public MethodParmDef()
+    {
+
+    }
+}
