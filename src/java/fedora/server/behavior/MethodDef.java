de6dd643c6b59648c225c914b6b9dbc8108430fb
Sandy Payette <spayette@duraspace.org>
2002-05-09 21:32:07 +0000

    A class for a behavior method definition in Fedora.

---
+++ src/java/fedora/server/behavior/MethodDef.java
@@ -0,0 +1,23 @@
+package fedora.server.behavior;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class MethodDef
+{
+
+    public String methodName;
+    public String methodLabel;
+    public MethodParmDef[] methodParms;
+
+    public MethodDef()
+    {
+    }
+
+}
