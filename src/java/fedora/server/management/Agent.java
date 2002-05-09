f66f29b92109c46e8b16f0a002c6e073d2f9ed62
Sandy Payette <spayette@duraspace.org>
2002-05-09 21:34:24 +0000

    A class to define an Agent, which is a person or entity that has responsibility for creating a digital object.  This is part of the Fedora object system metadata.

---
+++ src/java/fedora/server/management/Agent.java
@@ -0,0 +1,25 @@
+package fedora.server.management;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class Agent
+{
+
+  public String agentRole;
+
+  public String agentType;
+
+  public String agentName;
+
+  public Agent()
+  {
+  }
+
+}
