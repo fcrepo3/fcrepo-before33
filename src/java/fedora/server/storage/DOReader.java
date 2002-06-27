94457de9ff55d64d1f54d72977c65f24c57bcb07
Sandy Payette <spayette@duraspace.org>
2002-06-27 21:15:17 +0000

    Interface to read Fedora digital object

---
+++ src/java/fedora/server/storage/DOReader.java
@@ -0,0 +1,60 @@
+package fedora.server.storage;
+
+/**
+ * <p>Title: DOReader.java</p>
+ * <p>Description: Interface for reading Fedora digital objects</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author Sandy Payette
+ * @version 1.0
+ */
+
+import fedora.server.storage.abstraction.*;
+import java.util.Date;
+import java.io.InputStream;
+
+public interface DOReader
+{
+
+  /**
+   * Methods that pertain to the digital object as a whole
+   */
+
+    public String GetObjectXML();
+
+    public String ExportObject();
+
+  /**
+   * Methods that pertain to getting the digital object components
+   */
+
+    public String GetObjectPID();
+
+    public String[] ListDatastreamIDs(String state);
+
+    public Datastream[] GetDatastreams(Date versDateTime);
+
+    public Datastream GetDatastream(String datastreamID, Date versDateTime);
+
+    public Disseminator[] GetDisseminators(Date versDateTime);
+
+    public String[] ListDisseminatorIDs(String state);
+
+    public Disseminator GetDisseminator(String disseminatorID, Date versDateTime);
+
+  /**
+   *  Methods to obtain information stored in the Behavior Definition and
+   *  Behavior Mechanism objects to which the digital object's disseminators
+   *  refer.
+   */
+
+    // Returns PIDs of Behavior Definitions to which object subscribes
+    public String[] GetBehaviorDefs(Date versDateTime);
+
+    // Returns list of methods that Behavior Mechanism implements for a BDef
+    public MethodDef[] GetBMechMethods(String BdefPID, Date versDateTime);
+
+    // Overloaded method: returns InputStream as alternative
+    public InputStream GetBMechMethodsWSDL(String BdefID, Date versDateTime);
+
+}
