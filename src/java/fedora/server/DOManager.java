8ac17d43ff036d96da926da64758487468448c21
Chris Wilper <cwilper@duraspace.org>
2002-08-02 15:31:06 +0000

    minor mods... btw, these will move to the storage package soon.

--- src/java/fedora/server/DOManager.java
+++ src/java/fedora/server/DOManager.java
@@ -1,32 +1,39 @@
 package fedora.server;
 
 /**
- * A Module that provides access to the registry and
- * digital object reader/writer factories.
+ * Provides access to digital object reader/writer factories,
+ * which ultimately provide DOReader and DOWriter objects on
+ * a per-digital-object basis.
  *
  * @author cwilper@cs.cornell.edu
  */
 public interface DOManager 
         extends Module {
 
-    /** storage roles */
-    public static int DEFINITIVE=1;
-    public static int FAST=2;
-
     /**
      * Gets the digital object reader factory best fit for
-     * the given role.
+     * the given storage type.
      *
-     * @param role The role
-     * @returns DOReaderFactory A reader factory well-fit to
-     *          return DOReaders in the store with the given role.
+     * @param storageType The storage type.
+     * @returns DOReaderFactory An appropriate provider of DOReaders.
      */
-    public DOReaderFactory getReaderFactory(int role);
-
+    public DOReaderFactory getReaderFactory(String storageType);
 
     /**
-     * Same as above, but for writer..will comment better later.
-     */    
-    public DOWriterFactory getWriterFactory(int role);
+     * Gets the digital object writer factory best fit for
+     * the given storage type.
+     *
+     * @param storageType The storage type.
+     * @returns DOWriterFactory An appropriate provider of DOWriters.
+     */
+    public DOWriterFactory getWriterFactory(String storageType);
+    
+    /**
+     * Gets a Registry for the given storage type.
+     *
+     * @param storageType The storage type.
+     * @returns Registry A registry appropriate for the storage role
+     */
+    public DORegistry getRegistry(String storageType);
 
 }
--- src/java/fedora/server/DOReaderFactory.java
+++ src/java/fedora/server/DOReaderFactory.java
@@ -1,14 +1,18 @@
 package fedora.server;
 
 /**
- * A Module that provides access to the registry and
- * digital object reader/writer factories.
+ * A provider of DOReaders.
  *
  * @author cwilper@cs.cornell.edu
  */
-public interface DOReaderFactory
-        implements DOStorageAccessor {
+public interface DOReaderFactory {
 
-    public 
-
+    /**
+     * Gets a DOReader for reading the object with the given PID.
+     *
+     * @param pid The PID of the object.
+     * @returns DOReader The reader for the object.
+     */
+    public DOReader getReader(String pid);
+    
 }
