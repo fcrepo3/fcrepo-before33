e71f3982038d809285dbc22182554ac995fc80e4
Ross Wayland <rlw@virginia.edu>
2002-08-09 12:13:18 +0000

    Updated to reflect changes in DB Schema

--- src/java/fedora/server/access/DissResultSet.java
+++ src/java/fedora/server/access/DissResultSet.java
@@ -15,7 +15,6 @@ public class DissResultSet
   public String operationLocation = null;
   public String dsLocation = null;
   public String dsBindingKey = null;
-  public String returnType = null;
   public String protocolType = null;
   public String doPID = null;
   public String dissName = null;
@@ -38,26 +37,24 @@ public class DissResultSet
       this.addressLocation = sqlResults[3];
       this.operationLocation = sqlResults[4];
       this.protocolType = sqlResults[5];
-      this.returnType = sqlResults[6];
-      this.dsLocation = sqlResults[7];
-      this.dsBindingKey = sqlResults[8];
+      this.dsLocation = sqlResults[6];
+      this.dsBindingKey = sqlResults[7];
   }
 
   public static void main(String[] args)
   {
 
     // initialize array for testing
-    String[] sqlResults = new String[9];
+    String[] sqlResults = new String[8];
     sqlResults[0] = "uva-lib:1220";
     sqlResults[1] = "DISS1";
     sqlResults[2] = "getThumbnail";
     sqlResults[3] = "LOCAL";
     sqlResults[4] = "(THUMBRES_IMG)";
     sqlResults[5] = "http";
-    sqlResults[6] = "image/jpeg";
-    sqlResults[7] = "http://dl.lib.virginia.edu/"+
+    sqlResults[6] = "http://dl.lib.virginia.edu/"+
                     "data/image/saskia/006-007a11.jpg";
-    sqlResults[8] = "THUMBRES_IMG";
+    sqlResults[7] = "THUMBRES_IMG";
 
     DissResultSet result = new DissResultSet(sqlResults);
 
@@ -67,7 +64,6 @@ public class DissResultSet
     System.out.println("Address Location = "+result.addressLocation);
     System.out.println("Operation Location = "+result.operationLocation);
     System.out.println("Protocol Type = "+result.protocolType);
-    System.out.println("Return MIME Type = "+result.returnType);
     System.out.println("Datastream Location = "+result.dsLocation);
     System.out.println("Datastream Binding Key = "+result.dsBindingKey);
   }
