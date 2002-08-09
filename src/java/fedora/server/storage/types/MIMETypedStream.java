4cb5e46df45c7f871bff351d177803921b5aecdc
Ross Wayland <rlw@virginia.edu>
2002-08-09 12:02:35 +0000

    Moved fedora-defined type classes to new package server.storage.types

---
+++ src/java/fedora/server/storage/types/MIMETypedStream.java
@@ -0,0 +1,25 @@
+package fedora.server.storage.types;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class MIMETypedStream
+{
+    public String MIMEType;
+    public byte[] stream;
+
+    public MIMETypedStream() { }
+
+    public MIMETypedStream(String MIMEType, byte[] stream)
+    {
+        this.MIMEType = MIMEType;
+        this.stream = stream;
+    }
+
+}
