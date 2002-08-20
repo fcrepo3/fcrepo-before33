d7a44c0ecd91953a80a80533d8b2cdd642e37c24
Chris Wilper <cwilper@duraspace.org>
2002-08-20 13:29:25 +0000

    replaces ParameterizedComponent, also does not enforce impl of the last few (helper) methods... (Pluggable does that now)

---
+++ src/java/fedora/server/Parameterized.java
@@ -0,0 +1,67 @@
+package fedora.server;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Abstract superclass of all Fedora components that can be configured by
+ * a set of name-value pairs.
+ *
+ * @author cwilper@cs.cornell.edu
+ */
+public abstract class Parameterized {
+
+    /** a reference to the provided params for this component */
+    private Map m_parameters;
+    
+    /** an empty array of strings */
+    private final static String[] EMPTY_STRING_ARRAY=new String[] {};
+
+    /** 
+     * Creates a Parameterized with no parameters.
+     */
+    public Parameterized() {
+    }
+    
+    /** 
+     * Creates a Parameterized with name-value pairs from the supplied Map.
+     *
+     * @param parameters The map from which to derive the name-value pairs.
+     */
+    public Parameterized(Map parameters) {
+        setParameters(parameters);
+    }
+    
+    /** 
+     * Sets the parameters with name-value pairs from the supplied Map.
+     *
+     * This is protected because it is intended to only be called by 
+     * subclasses where super(Map m) is not possible to call at the start of 
+     * the constructor. Server.java:Server(URL) is an example of this.
+     *
+     * @param parameters The map from which to derive the name-value pairs.
+     */
+    protected final void setParameters(Map parameters) {
+        m_parameters=parameters;
+    }
+
+    /**
+     * Gets the value of a named configuration parameter.
+     *
+     * @param name The parameter name.
+     * @returns String The value, null if undefined.
+     */
+    public final String getParameter(String name) {
+        return (String) m_parameters.get(name);
+    }
+    
+    /**
+     * Gets an Iterator over the names of parameters for this component.
+     *
+     * @returns Iterator The names
+     */
+    public final Iterator parameterNames() {
+        return m_parameters.keySet().iterator();
+    }
+    
+}
