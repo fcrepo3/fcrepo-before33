7d04d8cd400ede676e0e7bfce2c64e1c18d4ce6e
Chris Wilper <cwilper@duraspace.org>
2002-08-20 13:31:01 +0000

    Abstract class that enforces impl of the helper methods previously declared by ParameterizedComponent... this extends Parameterized and is the new superclass of Server and Module.

---
+++ src/java/fedora/server/Pluggable.java
@@ -0,0 +1,70 @@
+package fedora.server;
+
+import java.util.Map;
+
+/**
+ * Abstract superclass of all Fedora components that can be configured by
+ * a set of name-value pairs.
+ *
+ * @author cwilper@cs.cornell.edu
+ */
+public abstract class Pluggable
+        extends Parameterized {
+
+    /** an empty array of strings */
+    private final static String[] EMPTY_STRING_ARRAY=new String[] {};
+
+    /** 
+     * Creates a Pluggable with no parameters.
+     */
+    public Pluggable() {
+    }
+    
+    /** 
+     * Creates a Pluggable with name-value pairs from the supplied Map.
+     *
+     * @param parameters The map from which to derive the name-value pairs.
+     */
+    public Pluggable(Map parameters) {
+        setParameters(parameters);
+    }
+    
+    /**
+     * Gets the names of required parameters for this component.
+     *
+     * @returns String[] The required parameter names.
+     */
+    public String[] getRequiredParameters() {
+        return EMPTY_STRING_ARRAY;
+    }
+    /**
+     * Gets the names of optional parameters for this component.
+     *
+     * @returns String[] The required parameter names.
+     */
+    public String[] getOptionalParameters() {
+        return EMPTY_STRING_ARRAY;
+    }
+    
+    /**
+     * Gets a short explanation of how to use a named parameter.
+     *
+     * @param name The name of the parameter.
+     * @returns String The explanation, null if no help is available or 
+     *                 the parameter is unknown.
+     */
+    public String getParameterHelp(String name) {
+        return null;
+    }
+    
+    /**
+     * Gets an explanation of how this component is to be configured via
+     * parameters.
+     *
+     * This should not include the information available via getParameterHelp,
+     * but is more intended as an overall explanation or an explanation of
+     * those parameters whose names might be dynamic.
+     */
+    public abstract String getHelp();
+
+}
