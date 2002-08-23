183266d48ca12c11ec86654e363a271b2a65edee
Ross Wayland <rlw@virginia.edu>
2002-08-23 13:07:53 +0000

    Exception for handling when local HttpService unable to retrieve the specified content.
    We may need to refine the different types of exceptions thrown at a later date so this could change.

---
+++ src/java/fedora/server/errors/HttpServiceNotFoundException.java
@@ -0,0 +1,25 @@
+package fedora.server.errors;
+
+/**
+ * <p>Title: HttpServiceNotFoundException.java</p>
+ * <p>Description: Signals that a successful HTTP connection could NOT</p>
+ * <p>be made to the designated URL.</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author Ross Wayland
+ * @version 1.0
+ */
+
+public class HttpServiceNotFoundException extends AccessException
+{
+
+  /**
+   * Creates a HttpServiceNotFoundException.
+   * @param message An informative message explaining what happened and
+   *                (possibly) how to fix it.
+   */
+  public HttpServiceNotFoundException(String message)
+  {
+    super(message);
+  }
+}
