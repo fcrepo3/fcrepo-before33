5c6c74f41551f488204f4f4da082f711d8545a09
Chris Wilper <cwilper@duraspace.org>
2002-08-29 12:33:16 +0000

    taking log params from config and initting log

--- src/java/fedora/server/BasicServer.java
+++ src/java/fedora/server/BasicServer.java
@@ -29,13 +29,92 @@ public class BasicServer
     
     public void initServer() 
             throws ServerInitializationException {
+        initLogger();
+    }
+    
+    private int getLoggerIntParam(String paramName) 
+            throws ServerInitializationException {
+        String s=getParameter(paramName);
+        int ret;
+        if (s==null) {
+            ret=0;
+            logConfig(paramName + " not specified, defaulting to 0 (infinite)");
+        } else {
+            try {
+                ret=Integer.parseInt(s);
+                if (ret<0) {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException nfe) {
+                throw new ServerInitializationException(paramName 
+                        + " must be an integer from 0 to " + Integer.MAX_VALUE);
+            }
+            String retString;
+            if (ret==0) {
+                retString="0 (infinite)";
+            } else {
+                retString="" + ret;
+            }
+            logConfig(paramName + " specified = " + retString + ", ok.");
+        }
+        return ret;
+    }
+    
+    private void initLogger() 
+            throws ServerInitializationException {
         Logger logger=Logger.getAnonymousLogger();
         logger.setUseParentHandlers(false);
-        logger.setLevel(Level.FINEST);
-        logDir=new File(getHomeDir(), Server.LOG_DIR);
-        int maxSize=1024*1024;
-        int maxFiles=5;
-        int maxDays=1;
+        int maxSize=getLoggerIntParam("log_max_size");
+        int maxFiles=getLoggerIntParam("log_max_files");
+        int maxDays=getLoggerIntParam("log_max_days");
+        String levelParam=getParameter("log_level");
+        Level logLevel;
+        if (levelParam==null) {
+            logConfig("log_level not specified, defaulting to config.");
+            logLevel=Level.CONFIG;
+        } else {
+            if (levelParam.equalsIgnoreCase("severe")) {
+                logLevel=Level.SEVERE;
+            } else if (levelParam.equalsIgnoreCase("warning")) {
+                logLevel=Level.WARNING;
+            } else if (levelParam.equalsIgnoreCase("info")) {
+                logLevel=Level.INFO;
+            } else if (levelParam.equalsIgnoreCase("config")) {
+                logLevel=Level.CONFIG;
+            } else if (levelParam.equalsIgnoreCase("fine")) {
+                logLevel=Level.FINE;
+            } else if (levelParam.equalsIgnoreCase("finer")) {
+                logLevel=Level.FINER;
+            } else if (levelParam.equalsIgnoreCase("finest")) {
+                logLevel=Level.FINEST;
+            } else {
+                throw new ServerInitializationException("log_level was "
+                        + "specified as " + levelParam + ".  If specified, "
+                        + "log_level must be severe, warning, info, config, "
+                        + "fine, finer, or finest.");
+            }
+            logConfig("log_level specified = " + levelParam + ", ok.");
+        }
+        logger.setLevel(logLevel);
+        
+        String dirParam=getParameter("log_dir");
+        File logDir;
+        if (dirParam==null) {
+            logDir=new File(getHomeDir(), Server.LOG_DIR);
+            logConfig("log_dir not specified, defaulting to " + logDir);
+        } else {
+            if (dirParam.startsWith(File.separator)) {
+                logDir=new File(dirParam);
+            } else {
+                logDir=new File(getHomeDir(), dirParam);
+            }
+            logConfig("log_dir specified = " + dirParam + ", "
+                    + "using " + logDir);
+        }
+        if (!(logDir.exists() && logDir.isDirectory())) {
+            throw new ServerInitializationException("log_dir is not an "
+                    + "existing directory.");
+        }
         String ext=".log";
         DatingFileHandler fh=null;
         try {
@@ -43,11 +122,12 @@ public class BasicServer
                     new SimpleXMLFormatter(true, "UTF-8"));
             fh.setLevel(Level.FINEST);
         } catch (IOException ioe) {
-            throw new ServerInitializationException("IO Problem initializing loghandler: " + ioe.getMessage());
+            throw new ServerInitializationException("Could not initialize "
+                    + "logger due to I/O error: " + ioe.getMessage());
         }
         logger.addHandler(fh);
+        logFinest("Logger initialized. Switching to file-based log...");
         setLogger(logger);
-        logInfo("Here's my first log message");
     }
     
     public void shutdownServer() 
