e1379b16a2569b555db22a576687abfbfe5610cc
Chris Wilper <cwilper@duraspace.org>
2002-08-30 16:24:21 +0000

    added method defs (w/o exceptions... needs discussion)

--- src/java/fedora/server/storage/DOWriter.java
+++ src/java/fedora/server/storage/DOWriter.java
@@ -1,7 +1,49 @@
 package fedora.server.storage;
 
+import fedora.server.storage.types.Datastream;
+import fedora.server.storage.types.Disseminator;
+
+/**
+ * The standard interface for write operations on digital objects.
+ * <p></p>
+ * FIXME: need to throw appropriate "all encompassing" exceptions,
+ * and need to figure out how the datastream object's stream is
+ * passed... not necessarily a byte array?  Or is this an interface
+ * implementation specific thing?  Seems like a default impl would be
+ * nice at any rate.
+ *
+ * @author cwilper@cs.cornell.edu
+ */
 public interface DOWriter {
 
-  // nothing yet
+    public void withdraw();
+    
+    public void delete();
+    
+    public void purge();
+    
+    public void lock();
+    
+    public void unlock();
+    
+    public String addDatastream(Datastream datastream);
+    
+    public void modifyDatastream(String id, Datastream datastream);
+    
+    public void withdrawDatastream(String id);
+    
+    public void deleteDatastream(String id);
+    
+    public void purgeDatastream(String id);
+
+    public String addDisseminator(Disseminator disseminator);
+    
+    public void modifyDisseminator(String id, Disseminator disseminator);
+    
+    public void withdrawDisseminator(String id);
+    
+    public void deleteDisseminator(String id);
+    
+    public void purgeDisseminator(String id);
 
 }
