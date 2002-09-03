6c54ee948ef315702503e690dbbbab4e0524cca2
Ross Wayland <rlw@virginia.edu>
2002-09-03 13:04:50 +0000

    initial import.
    New datastructure for holding dissemination info.
    Name may change, but wanted to get a formal datastructure
    to use instead of generic vector type when processing dissemination requests.
    
    Used by FastDOReader and FedoraAccessServlet.

---
+++ src/java/fedora/server/storage/types/Dissemination.java
@@ -0,0 +1,30 @@
+package fedora.server.storage.types;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+import fedora.server.storage.types.MethodParmDef;
+
+public class Dissemination
+{
+
+  public String DSBindKey = null;
+  public MethodParmDef[] methodParms = null;
+  public String DSLocation = null;
+  public String AddressLocation = null;
+  public String OperationLocation = null;
+  public String ProtocolType = null;
+
+
+
+  public Dissemination()
+  {
+  }
+
+}
