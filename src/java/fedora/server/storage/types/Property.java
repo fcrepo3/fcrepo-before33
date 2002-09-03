1143b32fdd400a13d91e7d7db4a6d7a0ec42cc7a
Ross Wayland <rlw@virginia.edu>
2002-09-03 13:09:57 +0000

    initial import.
    Property datastructure to hold user-supplied parameters
    Consists of name/value pair

---
+++ src/java/fedora/server/storage/types/Property.java
@@ -0,0 +1,21 @@
+package fedora.server.storage.types;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class Property
+{
+
+  public String Name;
+  public String Value;
+
+  public Property()
+  {
+  }
+}
