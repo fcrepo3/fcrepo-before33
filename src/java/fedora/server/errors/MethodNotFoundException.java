822996e904576c57385f06c77c03fde6135f60e6
Ross Wayland <rlw@virginia.edu>
2002-09-04 19:53:10 +0000

    initial import
    This may change once Exception handling is decided.

---
+++ src/java/fedora/server/errors/MethodNotFoundException.java
@@ -0,0 +1,25 @@
+package fedora.server.errors;
+
+/**
+ * <p>Title: MethodNotFoundException.java</p>
+ * <p>Description: Signals that a method associated with a Behavior</p>
+ * <p>Mechanism could not be found.
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author Ross Wayland
+ * @version 1.0
+ */
+
+public class MethodNotFoundException extends StorageException
+{
+
+  /**
+   * Creates a MethodNotFoundException.
+   * @param message An informative message explaining what happened and
+   *                (possibly) how to fix it.
+   */
+  public MethodNotFoundException(String message)
+  {
+    super(message);
+  }
+}
