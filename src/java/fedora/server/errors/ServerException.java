6f3c29ca8f34d56829453e7347340078da96c2f4
Chris Wilper <cwilper@duraspace.org>
2002-09-05 22:44:37 +0000

    Changed to make messageformatting and localization easier, still need other constructors.

--- src/java/fedora/server/errors/ServerException.java
+++ src/java/fedora/server/errors/ServerException.java
@@ -1,6 +1,8 @@
 package fedora.server.errors;
 
 import java.util.Locale;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
 
 /**
  * Signifies that an error occurred in the server.
@@ -9,23 +11,32 @@ import java.util.Locale;
  */
 public abstract class ServerException 
         extends Exception {
+        
+    private String m_bundleName;
+    private int m_code;
+    private String m_messageId;
+    private Object[] m_replacements;
 
-    /**
-     * Creates a ServerException.
-     *
-     * @param message An informative message explaining what happened and
-     *                (possibly) how to fix it.
-     */
-    public ServerException(String message) {
-        super(message);
+    public ServerException(String bundleName, String messageId, String[] replacements, Throwable cause) {
+        initCause(cause);
+        fillInStackTrace();
+        m_bundleName=bundleName;
+        try {
+            m_code=Integer.parseInt(MessageFormat.format(ResourceBundle.getBundle(messageId + ".code").getString(messageId), replacements));
+        } catch (NumberFormatException nfe) { m_code=9000; }
     }
 
+    public int getCode() {
+        return m_code;
+    }
+    
+    
+    public String getMessage() {
+        return MessageFormat.format(ResourceBundle.getBundle(m_bundleName).getString(m_messageId), m_replacements);
+    }
+    
     public String getMessage(Locale locale) {
-        return getMessage();        
-    }
-
-    public String getCode() {
-        return "UNKNOWN";
+        return MessageFormat.format(ResourceBundle.getBundle(m_bundleName, locale).getString(m_messageId), m_replacements);
     }
 
 }
