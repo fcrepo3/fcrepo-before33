670c20f5f21f80bb98822a158b353f4eb9df7bf4
Chris Wilper <cwilper@duraspace.org>
2002-09-06 14:03:22 +0000

    Decided on constructor signature and added methods and docs.

--- src/java/fedora/server/errors/ServerException.java
+++ src/java/fedora/server/errors/ServerException.java
@@ -11,32 +11,97 @@ import java.util.ResourceBundle;
  */
 public abstract class ServerException 
         extends Exception {
-        
-    private String m_bundleName;
-    private int m_code;
-    private String m_messageId;
-    private Object[] m_replacements;
 
-    public ServerException(String bundleName, String messageId, String[] replacements, Throwable cause) {
-        initCause(cause);
-        fillInStackTrace();
+    /** The bundle in which the message, identified by m_code, resides. */
+    private String m_bundleName;
+    
+    /** The identifier for the message in the bundle. */
+    private String m_code;
+    
+    /** The message in the default locale, if it's already been determined. */
+    private String m_defaultMessage;
+    
+    /** Replacements for placeholders in the message, starting at {0}. */
+    private Object[] m_values;
+
+    /** 
+     * Constructs a new ServerException.
+     *
+     * @param bundleName The bundle in which the message resides.
+     * @param code The identifier for the message in the bundle, aka the key.
+     * @param values Replacements for placeholders in the message, where 
+     *        placeholders are of the form {num} where num starts at 0,
+     *        indicating the 0th (1st) item in this array.
+     * @param cause The underlying exception if known, null meaning unknown or 
+     *        none.
+     */
+    public ServerException(String bundleName, String code, String[] values, 
+            Throwable cause) {
+        super(code, cause);
         m_bundleName=bundleName;
-        try {
-            m_code=Integer.parseInt(MessageFormat.format(ResourceBundle.getBundle(messageId + ".code").getString(messageId), replacements));
-        } catch (NumberFormatException nfe) { m_code=9000; }
+        m_code=code;
+        m_values=values;
     }
 
-    public int getCode() {
+    /**
+     * Gets the identifier for the message.
+     * 
+     * @return The code, which is also the key in the <code>MessageBundle</code>
+     *         for this exception.
+     */
+    public String getCode() {
         return m_code;
     }
     
-    
+    /**
+     * Gets the message, preferring the default locale.
+     * <p></p>
+     * The preferred locale is given by the System properties 
+     * <code>locale.language</code>, <code>locale.country</code>, and 
+     * (optionally) <code>locale.variant</code>, if they exist, or the
+     * system default locale otherwise.
+     *
+     * @return The message, with {num}-indexed placeholders populated, if 
+     *         needed.
+     */
     public String getMessage() {
-        return MessageFormat.format(ResourceBundle.getBundle(m_bundleName).getString(m_messageId), m_replacements);
+        if (m_defaultMessage==null) {
+            String language=System.getProperty("locale.language");
+            String country=System.getProperty("locale.country");
+            String variant=System.getProperty("locale.variant");
+            Locale locale;
+            if ((language!=null) && (country!=null)) {
+                if (variant!=null) {
+                    locale=new Locale(language, country, variant);
+                } else {
+                    locale=new Locale(language, country);
+                }
+            } else {
+                locale=Locale.getDefault();
+            }
+            m_defaultMessage=getMessage(locale);
+        }
+        return m_defaultMessage;
     }
     
+    /**
+     * Gets the message, preferring the provided locale.
+     * <p></p>
+     * When a message in the desired locale is not found, the locale selection 
+     * logic described by <a href="http://java.sun.com/j2se/1.4/docs/api/java/util/ResourceBundle.html">the
+     * java.util.ResourceBundle</a> class javadoc is used.
+     * 
+     * @param locale The preferred locale.
+     * @return The message, with {num}-indexed placeholders populated, if 
+     *         needed.
+     */
     public String getMessage(Locale locale) {
-        return MessageFormat.format(ResourceBundle.getBundle(m_bundleName, locale).getString(m_messageId), m_replacements);
+        if (m_values==null) {
+            return ResourceBundle.getBundle(m_bundleName, 
+                    locale).getString(m_code);
+        }
+        return MessageFormat.format(ResourceBundle.getBundle(m_bundleName, 
+                locale).getString(m_code), m_values);
     }
 
 }
