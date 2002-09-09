3c7f4b3861548e38e14fda6b44938d6e605f69f9
Chris Wilper <cwilper@duraspace.org>
2002-09-09 13:47:32 +0000

    added some impl details, not much

--- src/java/fedora/server/storage/DefinitiveDOWriter.java
+++ src/java/fedora/server/storage/DefinitiveDOWriter.java
@@ -1,9 +1,15 @@
 package fedora.server.storage;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.IOException;
 import java.util.Date;
 
-import fedora.server.errors.ServerException;
+import fedora.server.errors.ObjectExistsException;
+import fedora.server.errors.ObjectNotFoundException;
+import fedora.server.errors.StorageDeviceException;
+import fedora.server.errors.ValidationException;
 import fedora.server.storage.types.Datastream;
 import fedora.server.storage.types.Disseminator;
 import fedora.server.storage.types.*;
@@ -12,64 +18,103 @@ import fedora.server.storage.types.*;
  * A <code>DOWriter</code> for working with the definitive copy of a
  * digital object in the reference implementation.
  *
+ * This implementation stores the objects in <code>StreamStorage</code> using 
+ * METS XML encoding.
+ *
  * @author cwilper@cs.cornell.edu
  */
 public class DefinitiveDOWriter
         implements DOWriter {
-        
+
+/** remove and replace these with a DigitalObject instance  */
     private String m_pid;
-//    private StreamStorage m_storage;
+    private String m_label;
+    private String m_state;
+    
+    private boolean m_isDirty;
+    
+/**        */
+    
+    private TestStreamStorage m_storage;
+    private TestStreamStorage m_tempStorage;
+    private StreamValidator m_validator;
 
     /**
      * Constructs a DOWriter as a handle on an existing digital object.
      */
-//    public DefinitiveDOWriter(String pid, Validator validator, 
-//            StreamStorage storage) {
-//    }
+    public DefinitiveDOWriter(String pid, StreamValidator validator, 
+            TestStreamStorage storage, TestStreamStorage tempStorage) 
+            throws StorageDeviceException, ObjectNotFoundException {
+        m_pid=pid;
+        m_validator=validator;
+        m_storage=storage;
+        m_tempStorage=tempStorage;
+        
+        m_isDirty=false;
+    }
     
     /**
      * Constructs a DOWriter as a handle on a new digital object.
      */
-//    public DefinitiveDOWriter(String pid, Validator validator, 
-//            StreamStorage storage, InputStream template, String initialState) {
-//    }
-
-    public DefinitiveDOWriter() {
+    public DefinitiveDOWriter(String pid, StreamValidator validator, 
+            TestStreamStorage storage, TestStreamStorage tempStorage, 
+            InputStream template, String initialState, 
+            String initialValidationType) 
+            throws ValidationException, ObjectNotFoundException,
+            ObjectExistsException, StorageDeviceException {
+        m_pid=pid;
+        m_validator=validator;
+        m_storage=storage;
+        m_tempStorage=tempStorage;
+        set(template, initialValidationType, false);
+        setState(initialState);
     }
 
     /**
      * Sets the content of the entire digital object.
      *
      * @param content A stream of encoded content of the digital object.
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
-    public void set(InputStream content, String newState) {
-        // write it to disk first, then validate it.  
-        // If validation fails, tell the user
-        // note that we do validation anytime writing is done
-        //
-        // m_storage.something...
+    public void set(InputStream content, String validationType, boolean shouldExist) 
+            throws ValidationException, ObjectNotFoundException, 
+            ObjectExistsException, StorageDeviceException {
+        String tempId=m_pid + "-toValidate";
+        m_tempStorage.add(tempId, content);
+        m_validator.validate(m_tempStorage.retrieve(tempId), validationType);
+        if (shouldExist) {
+            m_storage.replace(tempId, m_tempStorage.retrieve(tempId));
+        } else {
+            m_storage.add(tempId, m_tempStorage.retrieve(tempId));
+        }
+        m_tempStorage.delete(tempId);
     }
 
     /**
      * Sets the state of the entire digital object.
      *
      * @param state The state.
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
     public void setState(String state) {
-           
+        m_state=state;
+        m_isDirty=true;
+    }
+
+    /**
+     * Sets the label of the digital object.
+     *
+     * @param label The label.
+     */
+    public void setLabel(String label) {
+        m_label=label;
+        m_isDirty=true;
     }
 
     /**
      * Removes the entire digital object.
      *
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */    
     public void remove() {
+        m_isDirty=true;
     }
 
     /**
@@ -77,8 +122,6 @@ public class DefinitiveDOWriter
      *
      * @param datastream The datastream.
      * @return An internally-unique datastream id.
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
     public String addDatastream(Datastream datastream) {
         return null;
@@ -89,8 +132,6 @@ public class DefinitiveDOWriter
      *
      * @param disseminator The disseminator.
      * @return An internally-unique disseminator id.
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
     public String addDisseminator(Disseminator disseminator) {
         return null;
@@ -106,8 +147,6 @@ public class DefinitiveDOWriter
      * @param end The end date (inclusive) of versions to remove.  If 
      *        <code>null</code>, this is taken to be the greatest possible 
      *        value.
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
     public void removeDatastream(String id, Date start, Date end) {
     }
@@ -122,8 +161,6 @@ public class DefinitiveDOWriter
      * @param end The end date (inclusive) of versions to remove.  If 
      *        <code>null</code>, this is taken to be the greatest possible 
      *        value.
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
     public void removeDisseminator(String id, Date start, Date end) {
     }
@@ -132,10 +169,12 @@ public class DefinitiveDOWriter
      * Saves the changes thus far to the permanent copy of the digital object.
      *
      * @param logMessage An explanation of the change(s).
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
-    public void commit(String logMessage, String newState) {
+    public void commit(String logMessage) {
+        save();
+        ByteArrayOutputStream out=new ByteArrayOutputStream();
+  //      METSDOSerializer.getInstance().serialize(this, out);
+        ByteArrayInputStream in=new ByteArrayInputStream(out.toByteArray());
     }
 
     /**
@@ -144,8 +183,6 @@ public class DefinitiveDOWriter
      * Subsequent calls will behave as if the changes made thus far never 
      * happened.
      *
-     * @throws ServerException If any type of error occurred fulfilling the 
-     *         request.
      */
     public void rollBack() {
     }
@@ -159,11 +196,11 @@ public class DefinitiveDOWriter
     }
 
     public String GetObjectPID() {
-        return null;
+        return m_pid;
     }
 
     public String GetObjectLabel() {
-        return null;
+        return m_label;
     }
 
     public String[] ListDatastreamIDs(String state) {
@@ -207,5 +244,16 @@ public class DefinitiveDOWriter
 
     public DSBindingMapAugmented[] GetDSBindingMaps(Date versDateTime) {
         return null;
+    }
+
+    /** this should go in DOReader, methinks */
+    public void validate(String validationType) {
+    }
+    
+    public boolean isDirty() {
+        return m_isDirty;
+    }
+    
+    public void save() {
     }
 }
