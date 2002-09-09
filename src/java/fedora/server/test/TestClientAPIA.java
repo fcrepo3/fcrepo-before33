375de950e43a5c200e3a7ef0217634d8442453ec
Ross Wayland <rlw@virginia.edu>
2002-09-09 12:48:24 +0000

    updated with changes to access soap service

--- src/java/fedora/server/test/TestClientAPIA.java
+++ src/java/fedora/server/test/TestClientAPIA.java
@@ -29,16 +29,18 @@ public class TestClientAPIA
 
   public static void main(String[] args)
   {
-    String PID = "uva-lib:1220";
+    String PID = "uva-lib:1225";
     String qName1 = "http://www.fedora.info/definitions/1/0/api/";
     String endpoint = "http://localhost:8080/fedora/access/soap";
     Calendar asOfDate = null;
+
     try
     {
       Service service = new Service();
       Call call = (Call) service.createCall();
       call.setTargetEndpointAddress( new java.net.URL(endpoint) );
-      call.setOperationName(new javax.xml.namespace.QName(qName1, "GetBehaviorDefinitions") );
+      call.setOperationName(new javax.xml.namespace.QName(qName1,
+          "GetBehaviorDefinitions") );
 
       String[] bDefs = (String[])call.invoke(new Object[] { PID });
       System.out.println("\n*****GETBEHAVIORDEFINITIONS RESULTS*****");
@@ -48,48 +50,140 @@ public class TestClientAPIA
       }
 
       // Test GetBehaviormethods
-      String bDefPID = "uva-bdef-stdimg:8";
-      call.setOperationName(new javax.xml.namespace.QName(qName1, "GetBehaviorMethods") );
+      String bDefPID = "uva-bdef-image-w:101";
+      call.setOperationName(new javax.xml.namespace.QName(qName1,
+          "GetBehaviorMethods") );
       QName qn = new QName("http://www.fedora.info/definitions/1/0/types/",
-                            "MIMETypedStream");
-      call.registerTypeMapping(fedora.server.types.gen.MIMETypedStream.class,
-                               qn,
-                               new org.apache.axis.encoding.ser.BeanSerializerFactory(fedora.server.types.gen.MIMETypedStream.class, qn),
-                               new org.apache.axis.encoding.ser.BeanDeserializerFactory(fedora.server.types.gen.MIMETypedStream.class, qn));
-      //call.setReturnType(new javax.xml.namespace.QName(qName2, "MIMETypedStream"), fedora.server.types.gen.MIMETypedStream.class);
-
-      fedora.server.types.gen.MIMETypedStream methodDefs =
-          (fedora.server.types.gen.MIMETypedStream) call.invoke( new Object[] { PID, bDefPID, asOfDate} );
-      String mime = methodDefs.getMIMEType();
-      System.out.println("\n\n*****GETBEHAVIORMETHODS RESULTS*****\nMIME: "+mime);
-      ByteArrayInputStream bais = new ByteArrayInputStream(methodDefs.getStream());
-      int c = 0;
-      while ((c = bais.read()) > 0)
+                            "MethodDef");
+      QName qn2 = new QName("http://www.fedora.info/definitions/1/0/types/",
+                            "MethodParmDef");
+      call.registerTypeMapping(fedora.server.types.gen.MethodDef.class,
+          qn,
+          new org.apache.axis.encoding.ser.BeanSerializerFactory(
+          fedora.server.types.gen.MethodDef.class, qn),
+          new org.apache.axis.encoding.ser.BeanDeserializerFactory(
+          fedora.server.types.gen.MethodDef.class, qn));
+      call.registerTypeMapping(fedora.server.types.gen.MethodParmDef.class,
+          qn2,
+          new org.apache.axis.encoding.ser.BeanSerializerFactory(
+          fedora.server.types.gen.MethodParmDef.class, qn2),
+          new org.apache.axis.encoding.ser.BeanDeserializerFactory(
+          fedora.server.types.gen.MethodParmDef.class, qn2));
+      fedora.server.types.gen.MethodDef[] methodDefs =
+          (fedora.server.types.gen.MethodDef[])
+          call.invoke( new Object[] { PID, bDefPID, asOfDate} );
+      System.out.println("\n\n*****GETBEHAVIORMETHODS RESULTS*****");
+      for (int i=0; i<methodDefs.length; i++)
       {
-        System.out.write(c);
+        System.out.println("httpBindingAddress :"+
+            methodDefs[i].getHttpBindingAddress());
+        System.out.println("httpBindingOperationLocation :"+
+            methodDefs[i].getHttpBindingOperationLocation());
+        System.out.println("httpBindingURL :"+
+            methodDefs[i].getHttpBindingURL());
+        System.out.println("methodLabel :"+
+            methodDefs[i].getMethodLabel());
+        System.out.println("methodName :"+
+            methodDefs[i].getMethodName());
+        fedora.server.types.gen.MethodParmDef[] parmDefs = null;
+        parmDefs = methodDefs[i].getMethodParms();
+        if (parmDefs != null)
+        {
+          for (int j=0; j<parmDefs.length; j++)
+          {
+            System.out.println("parmDefaultValue :"+
+                parmDefs[j].getParmDefaultValue());
+            System.out.println("parmLabel :"+parmDefs[j].getParmLabel());
+            System.out.println("parmName :"+parmDefs[j].getParmName());
+            System.out.println("parmRequired :"+parmDefs[j].isParmRequired());
+          }
+        }
+      }
+
+      // Test GetBehaviormethodsAsWSDL
+       bDefPID = "uva-bdef-image-w:101";
+       call.setOperationName(new javax.xml.namespace.QName(qName1,
+           "GetBehaviorMethodsAsWSDL") );
+       qn = new QName("http://www.fedora.info/definitions/1/0/types/",
+                             "MIMETypedStream");
+       call.registerTypeMapping(fedora.server.types.gen.MIMETypedStream.class,
+           qn,
+           new org.apache.axis.encoding.ser.BeanSerializerFactory(
+           fedora.server.types.gen.MIMETypedStream.class, qn),
+           new org.apache.axis.encoding.ser.BeanDeserializerFactory(
+           fedora.server.types.gen.MIMETypedStream.class, qn));
+       fedora.server.types.gen.MIMETypedStream methodWSDL =
+           (fedora.server.types.gen.MIMETypedStream)
+           call.invoke( new Object[] { PID, bDefPID, asOfDate} );
+       String mime = methodWSDL.getMIMEType();
+       System.out.println("\n\n*****GETBEHAVIORMETHODS RESULTS*****\nMIME: "+
+                          mime);
+       ByteArrayInputStream bais =
+           new ByteArrayInputStream(methodWSDL.getStream());
+       int c = 0;
+       while ((c = bais.read()) > 0)
+       {
+         System.out.write(c);
       }
 
       // Test GetDissemination
       PID = "1007.lib.dl.test/text_ead/viu00003";
       bDefPID = "web_ead";
       String method = "get_admin";
-      call.setOperationName(new javax.xml.namespace.QName(qName1, "GetDissemination") );
+      call.setOperationName(new javax.xml.namespace.QName(qName1,
+          "GetDissemination") );
       fedora.server.types.gen.MIMETypedStream dissemination =
-          (fedora.server.types.gen.MIMETypedStream) call.invoke( new Object[] { PID, bDefPID, method, asOfDate} );
-      mime = methodDefs.getMIMEType();
-      System.out.println("\n\n****DISSEMINATION RESULTS*****\nDissemination MIME: "+mime);
-      BufferedReader br = new BufferedReader(
-          new InputStreamReader(
-          new ByteArrayInputStream(dissemination.getStream())));
-
-      String line = null;
-      while ((line=br.readLine()) != null )
+          (fedora.server.types.gen.MIMETypedStream)
+          call.invoke( new Object[] { PID, bDefPID, method, asOfDate} );
+      if (dissemination != null)
       {
-        System.out.println(line);
+        mime = dissemination.getMIMEType();
+        System.out.println("\n\n****DISSEMINATION RESULTS*****\n"+
+                           "Dissemination MIME: "+mime);
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(
+            new ByteArrayInputStream(dissemination.getStream())));
+
+        String line = null;
+        while ((line=br.readLine()) != null )
+        {
+          System.out.println(line);
+        }
       }
+
+
+    //Test View Objecct
+
+    call.setOperationName(new javax.xml.namespace.QName(qName1,
+        "GetObjectMethods") );
+    fedora.server.types.gen.ObjectMethodsDef[] objectView = null;
+    qn = new QName("http://www.fedora.info/definitions/1/0/types/",
+                            "ObjectMethodsDef");
+    call.registerTypeMapping(fedora.server.types.gen.ObjectMethodsDef.class,
+        qn,
+        new org.apache.axis.encoding.ser.BeanSerializerFactory(
+        fedora.server.types.gen.ObjectMethodsDef.class, qn),
+        new org.apache.axis.encoding.ser.BeanDeserializerFactory(
+        fedora.server.types.gen.ObjectMethodsDef.class, qn));
+    objectView = (fedora.server.types.gen.ObjectMethodsDef[])
+      call.invoke( new Object[] { PID, asOfDate} );
+    //Object[] o = objectView;
+    for (int i=0; i<objectView.length; i++)
+    {
+      fedora.server.types.gen.ObjectMethodsDef ov =
+               new fedora.server.types.gen.ObjectMethodsDef();
+      ov = objectView[i];
+      System.out.println("objDef["+i+"] "+
+                         "\np1"+ov.getPID()+
+                         "\np2"+ov.getBDefPID()+
+                         "\np3"+ov.getMethodName()+
+                         "\np4"+ov.getAsOfDate());
+    }
     } catch (Exception e)
     {
-      System.err.println(e.toString());
+      e.printStackTrace();
+      System.out.println(e.getStackTrace());
     }
+
   }
 }
