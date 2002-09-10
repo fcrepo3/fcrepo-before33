7c8b6d6ebe8684e572e27f9b7797ea931ba3323e
Chris Wilper <cwilper@duraspace.org>
2002-09-10 17:01:21 +0000

    Re-defined interface a little more generally, added javadocs, and finished the basic impl.

--- src/java/fedora/server/storage/types/BasicDigitalObject.java
+++ src/java/fedora/server/storage/types/BasicDigitalObject.java
@@ -1,8 +1,12 @@
 package fedora.server.storage.types;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A basic implementation of DigitalObject that stores things in memory.
@@ -17,13 +21,12 @@ public class BasicDigitalObject
     private String m_label;
     private Date m_createDate;
     private Date m_lastModDate;
-    private AuditRecord[] m_auditRecords;
+    private ArrayList m_auditRecords;
     private HashMap m_datastreams;
     private HashMap m_disseminators;
-    private static String[] s_emptyStringArray=new String[0];
 
     public BasicDigitalObject() { 
-        m_auditRecords=new AuditRecord[0];
+        m_auditRecords=new ArrayList();
         m_datastreams=new HashMap();
         m_disseminators=new HashMap();
     }
@@ -68,45 +71,47 @@ public class BasicDigitalObject
         m_lastModDate=lastModDate;
     }
     
-    public AuditRecord[] getAuditRecords() {
+    public List getAuditRecords() {
         return m_auditRecords;
     }
-    
-    public void setAuditRecords(AuditRecord[] auditRecords) {
-        m_auditRecords=auditRecords;
+
+    public Iterator datastreamIdIterator() {
+        return copyOfKeysForNonEmptyLists(m_datastreams).iterator();
     }
     
-    public String[] getDatastreamIds() {
-        int num=m_datastreams.size();
-        if (num==0) {
-            return s_emptyStringArray;
-        }
-        String[] ids=new String[num];
-        Iterator iter=m_datastreams.keySet().iterator();
-        int i=0;
+    private static Set copyOfKeysForNonEmptyLists(HashMap map) {
+        HashSet set=new HashSet();
+        Iterator iter=map.keySet().iterator();
         while (iter.hasNext()) {
-            ids[i]=(String) iter.next();
+            String key=(String) iter.next();
+            List list=(List) map.get(key);
+            if (list.size()>0) {
+                set.add(key);
+            }
         }
-        return ids;
+        return set;
     }
     
-    public Datastream[] getDatastreams(String id) {
-        return null;
+    public List datastreams(String id) {
+        ArrayList ret=(ArrayList) m_datastreams.get(id);
+        if (ret==null) {
+            ret=new ArrayList();
+            m_datastreams.put(id, ret);
+        }
+        return ret;
     }
     
-    public void setDatastreams(String id, Datastream[] datastreams) {
-    }
-
-    public String[] getDisseminatorIds() {
-        return null;
+    public Iterator disseminatorIdIterator() {
+        return m_disseminators.keySet().iterator();
     }
     
-    public Disseminator[] getDisseminators(String id) {
-        return null;
+    public List disseminators(String id) {
+        ArrayList ret=(ArrayList) m_disseminators.get(id);
+        if (ret==null) {
+            ret=new ArrayList();
+            m_disseminators.put(id, ret);
+        }
+        return ret;
     }
     
-    public void setDisseminators(String id, Disseminator[] disseminators) {
-        
-    }
-
 }
--- src/java/fedora/server/storage/types/DigitalObject.java
+++ src/java/fedora/server/storage/types/DigitalObject.java
@@ -1,6 +1,8 @@
 package fedora.server.storage.types;
 
+import java.util.List;
 import java.util.Date;
+import java.util.Iterator;
 
 /**
  * A holder of Fedora digital object information.
@@ -20,30 +22,122 @@ import java.util.Date;
  */
 public interface DigitalObject {
 
+    /**
+     * Gets the pid.
+     *
+     * @return The pid, or null if it hasn't been set.
+     */
     public String getPid();
+
+    /**
+     * Sets the pid.
+     *
+     * @param pid The pid.
+     */
     public void setPid(String pid);
 
+    /**
+     * Gets the state.
+     *
+     * @return The state, or null if it hasn't been set.
+     */
     public String getState();
+
+    /**
+     * Sets the state.
+     *
+     * @param state The state.
+     */
     public void setState(String state);
-    
+
+    /**
+     * Gets the label.
+     *
+     * @return The label, or null if it hasn't been set.
+     */    
     public String getLabel();
+
+    /**
+     * Sets the label.
+     *
+     * @param label The label.
+     */    
     public void setLabel(String label);
     
+    /**
+     * Gets the date the object was created.
+     *
+     * @return The date, or null if it hasn't been set.
+     */
     public Date getCreateDate();
+
+    /**
+     * Sets the date the object was created.
+     *
+     * @param createDate The date.
+     */
     public void setCreateDate(Date createDate);
-    
+
+    /**
+     * Gets the date the object was last modified.
+     *
+     * @return The date, or null if it hasn't been set.
+     */    
     public Date getLastModDate();
+
+    /**
+     * Sets the date the object was last modified.
+     *
+     * @param date The date.
+     */
     public void setLastModDate(Date lastModDate);
-    
-    public AuditRecord[] getAuditRecords();
-    public void setAuditRecords(AuditRecord[] auditRecords);
-    
-    public String[] getDatastreamIds();
-    public Datastream[] getDatastreams(String id);
-    public void setDatastreams(String id, Datastream[] datastreams);
 
-    public String[] getDisseminatorIds();
-    public Disseminator[] getDisseminators(String id);
-    public void setDisseminators(String id, Disseminator[] disseminators);
+    /**
+     * Gets this object's mutable List of AuditRecord objects.
+     *
+     * @return The List of AuditRecords, possibly of zero size but never null.
+     */     
+    public List getAuditRecords();
 
+    /**
+     * Gets an Iterator over the ids for which there exist one or more 
+     * datastreams in this object.
+     * <p></p>
+     * The Iterator is not tied to the underlying Collection and cannot
+     * be used to remove datastreams.
+     *
+     * @return A new Iterator of datastream ids, possibly of zero size but 
+     *         never null.
+     */
+    public Iterator datastreamIdIterator();
+
+    /**
+     * Gets a mutable List of Datastreams in this object whose ids match the 
+     * provided id.
+     *
+     * @param id The datastream id.
+     * @return The list, possibly of zero size but never null.
+     */
+    public List datastreams(String id);
+
+    /**
+     * Gets an Iterator over the ids for which there exist one or more 
+     * disseminators in this object.
+     * <p></p>
+     * The Iterator is not tied to the underlying Collection and cannot
+     * be used to remove datastreams.
+     *
+     * @return A new Iterator of disseminator ids, possibly of zero size but 
+     *         never null.
+     */
+    public Iterator disseminatorIdIterator();
+    
+    /**
+     * Gets a mutable List of Disseminators in this object whose ids match the 
+     * provided id.
+     *
+     * @param id The datastream id.
+     * @return The list, possibly of zero size but never null.
+     */
+    public List disseminators(String id);
 }
