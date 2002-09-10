6a585e182e467882f56809e27de2e13c3cb3877d
Chris Wilper <cwilper@duraspace.org>
2002-09-10 06:33:25 +0000

    Made ObjectIntegrityException the superclass of object integrity-related problems and made ValidationException a subclass.  ObjectIntegrity exceptions may be thrown any time an object is formed inappropriately in some context (such as during serialization, deserialization, or calls on various DOReader/DOWriter methods), whereas ValidationExceptions are only thrown when an object is being completely examined for validity according to some explicitly defined rule(s).

--- src/java/fedora/server/errors/ValidationException.java
+++ src/java/fedora/server/errors/ValidationException.java
@@ -4,7 +4,7 @@ package fedora.server.errors;
  * Signals an error while validating.
  */
 public class ValidationException 
-        extends ServerException {
+        extends ObjectIntegrityException {
 
     public ValidationException(String bundleName, String code, String[] values,
             String[] details, Throwable cause) {
