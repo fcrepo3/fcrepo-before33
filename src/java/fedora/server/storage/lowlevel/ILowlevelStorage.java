91b80030da4f88287c177aac813a5d5e4aee32ae
Bill Niebel <wdn5e@virginia.edu>
2002-09-12 16:11:02 +0000

    made interface public

--- src/java/fedora/server/storage/lowlevel/ILowlevelStorage.java
+++ src/java/fedora/server/storage/lowlevel/ILowlevelStorage.java
@@ -3,7 +3,7 @@ import java.io.InputStream;
 import fedora.server.errors.LowlevelStorageException;
 import fedora.server.errors.ObjectAlreadyInLowlevelStorageException;
 import fedora.server.errors.ObjectNotInLowlevelStorageException;
-interface ILowlevelStorage {
+public interface ILowlevelStorage {
 	public void add(String pid, InputStream content) throws LowlevelStorageException, ObjectAlreadyInLowlevelStorageException;
 	public void replace(String pid, InputStream content) throws LowlevelStorageException, ObjectNotInLowlevelStorageException;
 	public InputStream retrieve(String pid) throws LowlevelStorageException, ObjectNotInLowlevelStorageException;
