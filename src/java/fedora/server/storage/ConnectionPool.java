bad16ca223c82a86fcf925dcbd9fef70a062dd05
Ross Wayland <rlw@virginia.edu>
2002-09-19 13:14:49 +0000

    added javadocs

--- src/java/fedora/server/storage/ConnectionPool.java
+++ src/java/fedora/server/storage/ConnectionPool.java
@@ -1,10 +1,15 @@
 package fedora.server.storage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Vector;
+
 /**
  * <p>Title: ConnectionPool.java</p>
  * <p>Description: A class for preallocating, recycling, and managing</p>
  * <p>JDBC connections.</p>
- * <p>Copyright: Taken from Core Servlets and JavaServer Pages</p>
+ * <p>Copyright: Taken/adapted from Core Servlets and JavaServer Pages</p>
  * <p>from Prentice Hall and Sun Microsystems Press,</p>
  * <p>http://www.coreservlets.com/</p>
  * <p>&copy; 2000 Marty Hall; may be freely used or adapted</p>
@@ -12,13 +17,6 @@ package fedora.server.storage;
  * @author Marty Hall
  * @version 1.0
  */
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Vector;
-
-
 public class ConnectionPool implements Runnable
 {
   private String driver, url, username, password;
@@ -27,6 +25,23 @@ public class ConnectionPool implements Runnable
   private Vector availableConnections, busyConnections;
   private boolean connectionPending = false;
 
+  /**
+   * <p>Constructs a ConnectionPool based on the calling arguments.</p>
+   *
+   * @param driver The JDBC driver class name.
+   * @param url The JDBC connection URL.
+   * @param username The database user name.
+   * @param password The he database password.
+   * @param initialConnections The minimum number of connections possible.
+   * @param maxConnections The maximum number of connections possible.
+   * @param waitIfBusy Boolean flag that determines whether to wait if there
+   * are no more available connections. If set to true, it will wait until a
+   * connection becomes available. If set to false, it will throw
+   * <code>SQLException</code> when a connection is requested and there are no
+   * more available connections.
+   * @throws SQLException If the connection pool cannot be established for
+   * any reason.
+   */
   public ConnectionPool(String driver, String url,
                         String username, String password,
                         int initialConnections,
@@ -52,6 +67,13 @@ public class ConnectionPool implements Runnable
     }
   }
 
+  /**
+   * <p>Gets the next available connection.</p>
+   *
+   * @return The next available connection.
+   * @throws SQLException If the maximum number of connections has been reached
+   * or there is some other problem in obtaining the connection.
+   */
   public synchronized Connection getConnection()
       throws SQLException
   {
@@ -88,8 +110,7 @@ public class ConnectionPool implements Runnable
       //    flag is true. Then do the same thing as in second
       //    part of step 1: wait for next available connection.
 
-      if ((totalConnections() < maxConnections) &&
-!connectionPending)
+      if ((totalConnections() < maxConnections) && !connectionPending)
       {
         makeBackgroundConnection();
       } else if (!waitIfBusy)
@@ -108,14 +129,14 @@ public class ConnectionPool implements Runnable
     }
   }
 
-  // You can't just make a new connection in the foreground
-  // when none are available, since this can take several
-  // seconds with a slow network connection. Instead,
-  // start a thread that establishes a new connection,
-  // then wait. You get woken up either when the new connection
-  // is established or if someone finishes with an existing
-  // connection.
-
+  /**
+   * <p>Makes a background connection. You can't just make a new connection
+   * in the foreground when none are available, since this can take several
+   * seconds with a slow network connection. Instead, start a thread that
+   * establishes a new connection, then wait. You get woken up either when
+   * the new connection is established or if someone finishes with an existing
+   * connection.
+   */
   private void makeBackgroundConnection()
   {
     connectionPending = true;
@@ -147,10 +168,15 @@ public class ConnectionPool implements Runnable
     }
   }
 
-  // This explicitly makes a new connection. Called in
-  // the foreground when initializing the ConnectionPool,
-  // and called in the background when running.
 
+  /**
+   * <p>Explicitly makes a new connection. Called in
+   * the foreground when initializing the ConnectionPool,
+   * and called in the background when running.</p>
+   *
+   * @return A JDBC connection.
+   * @throws SQLException If the connection cannot be established.
+   */
   private Connection makeNewConnection()
       throws SQLException
   {
@@ -171,6 +197,11 @@ public class ConnectionPool implements Runnable
     }
   }
 
+  /**
+   * <p>Releases the specified connection and returns it to the active pool.</p>
+   *
+   * @param connection A JDBC connection.
+   */
   public synchronized void free(Connection connection)
   {
     busyConnections.removeElement(connection);
@@ -179,21 +210,27 @@ public class ConnectionPool implements Runnable
     notifyAll();
   }
 
+  /**
+   * <p>Provides the total number of connections present including those that
+   * are busy and those that are available.</p>
+   *
+   * @return The number of total connections present.
+   */
   public synchronized int totalConnections()
   {
     return(availableConnections.size() +
            busyConnections.size());
   }
 
-  /** Close all the connections. Use with caution:
+  /**
+   * <p>Closes all the connections. Use with caution:
    *  be sure no connections are in use before
-   *  calling. Note that you are not <I>required</I> to
+   *  calling. Note that you are not <i>required</i> to
    *  call this when done with a ConnectionPool, since
    *  connections are guaranteed to be closed when
    *  garbage collected. But this method gives more control
    *  regarding when the connections are closed.
    */
-
   public synchronized void closeAllConnections()
   {
     closeConnections(availableConnections);
@@ -202,6 +239,11 @@ public class ConnectionPool implements Runnable
     busyConnections = new Vector();
   }
 
+  /**
+   * <p>Closes connections in the specified list.</p>
+   *
+   * @param connections A list of connections to be closed.
+   */
   private void closeConnections(Vector connections)
   {
     try
@@ -221,6 +263,11 @@ public class ConnectionPool implements Runnable
     }
   }
 
+  /**
+   * <p>Converts this class object into a meaningful string.</p>
+   *
+   * @return A string describing the connection pool.
+   */
   public synchronized String toString()
   {
     String info =
