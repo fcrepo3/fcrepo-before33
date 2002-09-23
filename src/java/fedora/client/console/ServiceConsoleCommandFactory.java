1d048c4c99f58e0dad0b72b4565d43f372f3bbe3
Chris Wilper <cwilper@duraspace.org>
2002-09-23 07:04:50 +0000

    A static getter of ConsoleCommands that uses documentation elements from WSDL and a java interface to construct an array of ConsoleCommands

---
+++ src/java/fedora/client/console/ServiceConsoleCommandFactory.java
@@ -0,0 +1,26 @@
+package fedora.client.console;
+
+import java.lang.reflect.Method;
+import javax.wsdl.PortType;
+
+public class ServiceConsoleCommandFactory {
+
+    public static ConsoleCommand[] getConsoleCommands(Class javaInterface,
+            PortType wsdlInterface) {
+        if (!javaInterface.isInterface()) {
+            return null;
+        }
+        Method[] methods=javaInterface.getDeclaredMethods();
+        ConsoleCommand[] commands=new ConsoleCommand[methods.length];
+        for (int i=0; i<methods.length; i++) {
+        /*
+            public ConsoleCommand(Method method, String methodDescription, 
+            String[] paramNames, String[] paramDescriptions, String returnName, 
+            String returnDescription) {
+        */
+            commands[i]=new ConsoleCommand(methods[i], null, null, null, null, null);
+        }
+        return commands;
+    }
+
+}
