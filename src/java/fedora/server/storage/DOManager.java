6b9dbcb65e55a9c3fcb4a5c0c01579f4a06e4969
Chris Wilper <cwilper@duraspace.org>
2002-09-24 22:30:53 +0000

    Updated to take Context info into account

--- src/java/fedora/server/storage/DOManager.java
+++ src/java/fedora/server/storage/DOManager.java
@@ -3,7 +3,9 @@ package fedora.server.storage;
 import fedora.server.errors.ModuleInitializationException;
 import fedora.server.errors.ObjectNotFoundException;
 import fedora.server.errors.StorageException;
+import fedora.server.Context;
 import fedora.server.Module;
+import fedora.server.ReadOnlyContext;
 import fedora.server.Server;
 
 import java.util.Map;
@@ -20,29 +22,52 @@ import java.util.Map;
  */
 public abstract class DOManager 
         extends Module {
+        
+    private Context m_defaultContext;
 
     /**
-     * Creates a new DOManager.
+     * Creates a new DOManager with an empty default context.
      */
     public DOManager(Map moduleParameters, Server server, String role)
             throws ModuleInitializationException {
         super(moduleParameters, server, role);
+        m_defaultContext=null;
     }
     
+    public void setDefaultContext(Context defaultContext) {
+        m_defaultContext=defaultContext;
+    }
+
     /**
-     * Gets a digital object reader.
+     * Gets a digital object reader, using the default context.
      *
      * @param pid The PID of the object.
      * @returns DOReader A reader.
      * @throws StorageException If the request could not be fulfilled.
      * @throws ObjectNotFoundException If the object requested was not found.
      */
-    public abstract DOReader getReader(String pid)
+    public final DOReader getReader(String pid)
+            throws StorageException, 
+                   ObjectNotFoundException {
+        return getReader(pid, null);
+    }
+
+    /**
+     * Gets a digital object reader, using the default context with additional
+     * or overridden values from the given context.
+     */
+    public final DOReader getReader(String pid, Context context)
+            throws StorageException, 
+                   ObjectNotFoundException {
+        return getReaderForContext(pid, ReadOnlyContext.getUnion(m_defaultContext, context));
+    }
+
+    protected abstract DOReader getReaderForContext(String pid, ReadOnlyContext context)
             throws StorageException, 
                    ObjectNotFoundException;
 
     /**
-     * Gets a digital object writer.
+     * Gets a digital object writer, using the default context.
      *
      * If the pid parameter is null, a new digital object will be
      * created (ObjectNotFound will never be thrown in this case).
@@ -52,8 +77,41 @@ public abstract class DOManager
      * @throws StorageException If the request could not be fulfilled.
      * @throws ObjectNotFoundException If the object requested was not found.
      */
-    public abstract DOWriter getWriter(String pid)
+    public final DOWriter getWriter(String pid)
+            throws StorageException, 
+                   ObjectNotFoundException {
+        return getWriter(pid, null);
+    }
+    
+    /**
+     * Gets a digital object writer, using the default context with additional
+     * or overridden values from the given context.
+     */
+    public final DOWriter getWriter(String pid, Context context)
+            throws StorageException, 
+                   ObjectNotFoundException {
+        return getWriterForContext(pid, ReadOnlyContext.getUnion(m_defaultContext, context));
+    }
+
+    /**
+     * Gets a digital object writer on a new digital object, using the default context.
+     */
+    public final DOWriter getWriter()
+            throws StorageException {
+        return getWriter(null, m_defaultContext);
+    }
+    
+    /**
+     * Gets a digital object writer on a new digital object, using the default 
+     * context with additional or overridden values from the given context.
+     */
+    public final DOWriter getWriter(Context context)
+            throws StorageException, ObjectNotFoundException {
+        return getWriterForContext(null, ReadOnlyContext.getUnion(m_defaultContext, context));
+    }
+
+    protected abstract DOWriter getWriterForContext(String pid, ReadOnlyContext context)
             throws StorageException, 
                    ObjectNotFoundException;
-
+        
 }
--- src/java/fedora/server/storage/DefaultDOManager.java
+++ src/java/fedora/server/storage/DefaultDOManager.java
@@ -3,7 +3,9 @@ package fedora.server.storage;
 import fedora.server.errors.ModuleInitializationException;
 import fedora.server.errors.ObjectNotFoundException;
 import fedora.server.errors.StorageException;
+import fedora.server.Context;
 import fedora.server.Module;
+import fedora.server.ReadOnlyContext;
 import fedora.server.Server;
 
 import java.util.Map;
@@ -11,16 +13,11 @@ import java.util.Map;
 /**
  * Provides access to digital object readers and writers.
  *
- * Note that both instance methods throw StorageException and
- * ObjectNotFoundException (a subclass of the abstract
- * StorageException). Implementations of DOManager are expected 
- * to throw concrete subclasses of StorageException where needed.
- *
  * @author cwilper@cs.cornell.edu
  */
 public class DefaultDOManager 
         extends DOManager {
-
+        
     /**
      * Creates a new DefaultDOManager.
      */
@@ -28,36 +25,15 @@ public class DefaultDOManager
             throws ModuleInitializationException {
         super(moduleParameters, server, role);
     }
+
+    /** pid will always be non-null, context will always be non-null */
+    protected DOReader getReaderForContext(String pid, ReadOnlyContext context) {
+        return null;
+    }
+
+    /** pid may be null, context will always be non-null */
+    protected DOWriter getWriterForContext(String pid, ReadOnlyContext context) {
+        return null;
+    }
     
-    /**
-     * Gets a digital object reader.
-     *
-     * @param pid The PID of the object.
-     * @returns DOReader A reader.
-     * @throws StorageException If the request could not be fulfilled.
-     * @throws ObjectNotFoundException If the object requested was not found.
-     */
-    public DOReader getReader(String pid) {
-      //      throws StorageException, 
-      //             ObjectNotFoundException {
-        return null;
-    }
-
-    /**
-     * Gets a digital object writer.
-     *
-     * If the pid parameter is null, a new digital object will be
-     * created (ObjectNotFound will never be thrown in this case).
-     *
-     * @param pid The PID of the object.
-     * @returns DOWriter A writer.
-     * @throws StorageException If the request could not be fulfilled.
-     * @throws ObjectNotFoundException If the object requested was not found.
-     */
-    public DOWriter getWriter(String pid) {
-           // throws StorageException, 
-           //        ObjectNotFoundException {
-        return null;
-    }
-
 }
