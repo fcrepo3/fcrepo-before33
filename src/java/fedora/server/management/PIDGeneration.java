b34e246363a558cf169829950cad18a3ac7f08de
Chris Wilper <cwilper@duraspace.org>
2002-09-25 18:45:35 +0000

    Modified to accept logdir as constructor param instead of reading from properties file.

--- src/java/fedora/server/management/PIDGeneration.java
+++ src/java/fedora/server/management/PIDGeneration.java
@@ -24,190 +24,189 @@ import java.text.*;
 */
 public class PIDGeneration implements PIDGenerator {
         private Class classRef;
-	private static String logsDirectory;
+   private static String logsDirectory;
         private static BigInteger lastObjectID;
 
-	/**
-	* <p>
-	* Constructor -  the class reference is retrieved for synchronization.  In order
-	* to ensure file and data structure integrity, only one thread per class
-	* (class, not object) can run at a time.
-	*   When the first object is instantiated (lastObjectID == null), the logs 
-	* directory pathname is determined and the last generated PID is retrieved from 
-	* the most recent log file.  These values are stored in static variables for use by 
-	* this object and later object instantiations.
-	*
-	* @exception ClassNotFoundException class processing error
-	* @exception FileNotFoundException properties or log file processing error
-	* @exception IOException properties or log file processing error
-	*/
-        public PIDGeneration() throws ClassNotFoundException, FileNotFoundException, IOException {
-		String lastPID;
-		String tmpObjectID;
-		int index;
-		Properties props;
+   /**
+   * <p>
+   * Constructor -  the class reference is retrieved for synchronization.  In order
+   * to ensure file and data structure integrity, only one thread per class
+   * (class, not object) can run at a time.
+   *   When the first object is instantiated (lastObjectID == null), the logs 
+   * directory pathname is determined and the last generated PID is retrieved from 
+   * the most recent log file.  These values are stored in static variables for use by 
+   * this object and later object instantiations.
+   *
+   * @param pidLogDir
+   * @exception ClassNotFoundException class processing error
+   * @exception FileNotFoundException properties or log file processing error
+   * @exception IOException properties or log file processing error
+   */
+        public PIDGeneration(String pidLogDir) throws ClassNotFoundException, FileNotFoundException, IOException {
+       String lastPID;
+       String tmpObjectID;
+       int index;
+       Properties props;
 
                 classRef = Class.forName("PIDGeneration");
 
-		// Single threaded per class here.
+       // Single threaded per class here.
                 synchronized(classRef) {
-			if (lastObjectID == null) {
-				// Determine the logs directory pathname.
-				props = new Properties();
-				props.load(new BufferedInputStream(new FileInputStream("properties.txt")));
-				logsDirectory = props.getProperty("logsDirectory", "logs");
+           if (lastObjectID == null) {
+               // Determine the logs directory pathname.
+               logsDirectory = pidLogDir;
 System.out.println("logsDirectory: ["+logsDirectory+"]");
 
-				// Retrieve the last PID generated from the
-				// log file.
-				lastPID = getLastPIDNoLock();
+               // Retrieve the last PID generated from the
+               // log file.
+               lastPID = getLastPIDNoLock();
 
-				// Remove namespace ID and ':" from the PID.
-				index = lastPID.indexOf(':');
-				tmpObjectID = lastPID.substring(++index);
+               // Remove namespace ID and ':" from the PID.
+               index = lastPID.indexOf(':');
+               tmpObjectID = lastPID.substring(++index);
 
-				// Convert to BigInteger.
-				lastObjectID = new BigInteger(tmpObjectID);
-			}
-		}
+               // Convert to BigInteger.
+               lastObjectID = new BigInteger(tmpObjectID);
+           }
+       }
         }
 
-	/**
-	* <p>
-	* Creates a new PID with the specified namespace as a prefix.
-	*
-	* @param namespaceID identifies a repository or group of repositories
-	*
-	* @return the generated PID string
-	*
-	* @exception IOException properties or log file processing error
-	*/
+   /**
+   * <p>
+   * Creates a new PID with the specified namespace as a prefix.
+   *
+   * @param namespaceID identifies a repository or group of repositories
+   *
+   * @return the generated PID string
+   *
+   * @exception IOException properties or log file processing error
+   */
         public String generatePID(String NamespaceID) throws IOException {
                 String newPID;
 
-		// Single threaded per class here.
+       // Single threaded per class here.
                 synchronized(classRef) {
 
                         // Increment lastObjectID by one.
                         lastObjectID = lastObjectID.add(BigInteger.ONE);
 
-			// Format the new PID from the namespace ID and the object ID. 
+           // Format the new PID from the namespace ID and the object ID. 
                         newPID = NamespaceID + ":" + lastObjectID;
 
-			// Write the new PID to the log file.
+           // Write the new PID to the log file.
                         putNewPID(newPID);
 
                 }
                 return newPID;
         }
 
-	/**
-	* <p>
-	* Returns the last generated PID
-	*
-	* @return the retrieved PID string
-	*
-	* @exception IOException log file processing error
-	*/
+   /**
+   * <p>
+   * Returns the last generated PID
+   *
+   * @return the retrieved PID string
+   *
+   * @exception IOException log file processing error
+   */
         public String getLastPID() throws IOException {
-		// Single threaded per class here.
+       // Single threaded per class here.
                 synchronized(classRef) {
         		return getLastPIDNoLock();
-		}
-	}
+       }
+   }
 
-	/**
-	* <p>
-	* Returns the last generated PID.  Internal version (private) that provides
-	* an unsynchronized implementation for calling by getLastPID and the
-	* PIDGeneration constructor. 
-	*
-	* @return the retrieved PID string
-	*
-	* @exception IOException log file processing error
-	*/
+   /**
+   * <p>
+   * Returns the last generated PID.  Internal version (private) that provides
+   * an unsynchronized implementation for calling by getLastPID and the
+   * PIDGeneration constructor. 
+   *
+   * @return the retrieved PID string
+   *
+   * @exception IOException log file processing error
+   */
         private String getLastPIDNoLock() throws IOException {
         	File dir;
         	String files[];
-		String mrfp;
-		BufferedReader in;
-		String lastLine;
-		String line;
-		int index;
-		String pid;
+       String mrfp;
+       BufferedReader in;
+       String lastLine;
+       String line;
+       int index;
+       String pid;
 
-		// Determine the most recent log file. 
+       // Determine the most recent log file. 
         	dir = new File(logsDirectory);
         	files = dir.list();
         	Arrays.sort(files);
         	// After sorting, the most recent file will be the last one in the array.
-		mrfp = logsDirectory + File.separator + files[files.length - 1];
+       mrfp = logsDirectory + File.separator + files[files.length - 1];
 //System.out.println("mostRecentFile: ["+mrfp+"]");
 
-		// Open the file.
-		 in = new BufferedReader(new InputStreamReader(new FileInputStream(mrfp)));
+       // Open the file.
+        in = new BufferedReader(new InputStreamReader(new FileInputStream(mrfp)));
 
-		// Read the last line from this file.
-		lastLine = null;
-		while ((line = in.readLine()) != null) 
-			lastLine = line;
+       // Read the last line from this file.
+       lastLine = null;
+       while ((line = in.readLine()) != null) 
+           lastLine = line;
 
-		// Close the file.
-		in.close();
+       // Close the file.
+       in.close();
 
-		// Return the PID from this line
-		index = lastLine.indexOf('|');
+       // Return the PID from this line
+       index = lastLine.indexOf('|');
                 pid = lastLine.substring(0, index);
 //System.out.println("pid: ["+pid+"]");
-		return pid;
+       return pid;
         }
 
-	/**
-	* <p>
-	* Appends the newly created PID to the current log file.
-	*
-	* @exception IOException log file processing error
-	*/
+   /**
+   * <p>
+   * Appends the newly created PID to the current log file.
+   *
+   * @exception IOException log file processing error
+   */
         private void putNewPID(String pid) throws IOException {
-		Date date;
-		String dateString;
-		String pathName;
-		PrintWriter out;
-		
-		// Determine file name from the date.
-		date = new Date();
+       Date date;
+       String dateString;
+       String pathName;
+       PrintWriter out;
+       
+       // Determine file name from the date.
+       date = new Date();
       		dateString = new SimpleDateFormat("yyyy_MM_dd").format(date);
 
-		// File name format: PIDGeneration_YYYY_MM_DD.log
-		pathName = logsDirectory + File.separator + "PIDGeneration_" + dateString + ".log";
+       // File name format: PIDGeneration_YYYY_MM_DD.log
+       pathName = logsDirectory + File.separator + "PIDGeneration_" + dateString + ".log";
 
-		// Open file for appending.  If it doesn't exist, it is created.
-		out = new PrintWriter(new FileWriter(pathName, true));
+       // Open file for appending.  If it doesn't exist, it is created.
+       out = new PrintWriter(new FileWriter(pathName, true));
 
-		// Write the log entry.
-		out.println(pid + "|" + date);
+       // Write the log entry.
+       out.println(pid + "|" + date);
 
-		// Close file.
-		out.close();
+       // Close file.
+       out.close();
         }
 
-	/**
-	* <p>
-	* Used for unit testing and demonstration purposes.
-	*
-	* @param args program arguments
-	*
-	* @exception Exception exceptions that are thrown from called methods
-	*/
+   /**
+   * <p>
+   * Used for unit testing and demonstration purposes.
+   *
+   * @param args program arguments
+   *
+   * @exception Exception exceptions that are thrown from called methods
+   */
     	static public void main (String[] args) throws Exception {
-		PIDGeneration pidg = new PIDGeneration();
-		int repetitions = 1;
+       PIDGeneration pidg = new PIDGeneration(".");
+       int repetitions = 1;
 
-		if (args.length > 0) 
-			repetitions = Integer.parseInt(args[0]);
+       if (args.length > 0) 
+           repetitions = Integer.parseInt(args[0]);
 
-		System.out.println("Calling generatePID " + repetitions + " time(s).");
-		for(int i=0; i<repetitions; ++i)
-			System.out.println(pidg.generatePID("uva-lib"));
+       System.out.println("Calling generatePID " + repetitions + " time(s).");
+       for(int i=0; i<repetitions; ++i)
+           System.out.println(pidg.generatePID("uva-lib"));
     	}
 }
