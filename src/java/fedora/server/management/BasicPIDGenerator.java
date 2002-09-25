d1e88bdb148636ffd512352193c8805137c0e56e
Chris Wilper <cwilper@duraspace.org>
2002-09-25 18:47:38 +0000

    A Module that implements PIDGenerator by wrapping PIDGeneration. This module takes one (required) configuration... the log dir, which, if not absolute, is assumed to be rooted at fedora.home. The parameter's name is pidgen_log_dir

---
+++ src/java/fedora/server/management/BasicPIDGenerator.java
@@ -0,0 +1,99 @@
+package fedora.server.management;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import fedora.server.Module;
+import fedora.server.Server;
+import fedora.server.errors.ModuleInitializationException;
+
+/**
+ * A <code>PIDGenerator</code> that produces ordered PIDs, and keeps track 
+ * of where it's at using logfiles.
+ * <p></p>
+ * Implementation note: This is a wrapper around the PIDGeneration class
+ * that casts it as a Module.
+ *
+ * @author cwilper@cs.cornell.edu
+ * @version 1.0
+ */
+public class BasicPIDGenerator
+        extends Module implements PIDGenerator {
+        
+    private PIDGeneration m_pidGeneration;
+
+    /**
+     * Constructs a BasicPIDGenerator.
+     *
+     * @param moduleParameters A pre-loaded Map of name-value pairs comprising
+     *        the intended configuration of this Module.
+     * @param server The <code>Server</code> instance.
+     * @param role The role this module fulfills, a java class name.
+     * @throws ModuleInitializationException If initilization values are
+     *         invalid or initialization fails for some other reason.
+     */
+    public BasicPIDGenerator(Map moduleParameters, Server server, String role)
+            throws ModuleInitializationException {
+        super(moduleParameters, server, role);
+    }
+
+    public void initModule() 
+            throws ModuleInitializationException {
+        try {
+            String pidGenLogDir=getParameter("pidgen_log_dir");
+            if (pidGenLogDir==null) {
+                throw new ModuleInitializationException("pidgen_log_dir must be specified", getRole());
+            }
+            File f=null;
+            if (pidGenLogDir.startsWith("/") 
+                    || pidGenLogDir.startsWith("\\") 
+                    || pidGenLogDir.substring(1).startsWith(":\\")) {
+                f=new File(pidGenLogDir);
+            } else {
+                f=new File(getServer().getHomeDir(), pidGenLogDir);
+            }
+            if (!f.exists()) {
+                if (!f.mkdir()) {
+                    throw new ModuleInitializationException("pidgen_log_dir could not be created.", getRole());
+                }
+            } else {
+                if (!f.isDirectory()) {
+                    throw new ModuleInitializationException("pidgen_log_dir exists but is not a dir.", getRole());
+                }
+            }
+            m_pidGeneration=new PIDGeneration(f.getAbsolutePath());
+        } catch (ClassNotFoundException cnfe) {
+            // won't happen
+        } catch (IOException ioe) {
+            // maybe...
+            throw new ModuleInitializationException("IOException thrown by PIDGeneration constructor:" + ioe.getMessage(), getRole());
+        }
+    }
+
+    /**
+     * Creates a new PID with the specified namespace as a prefix.
+     *
+     * @param namespaceID The namespace id for the to-be-generated pid.
+     * @return The generated pid.
+     * @throws IOException If the request can't be fulfilled due to an
+     *         unexpected IO condition.
+     */
+    public String generatePID(String namespaceID) 
+            throws IOException {
+        return m_pidGeneration.generatePID(namespaceID);
+    }
+
+    /**
+     * Gets the last generated PID.
+     *
+     * @return the retrieved PID string
+     * @throws IOException If the request can't be fulfilled due to an
+     *         unexpected IO condition.
+     */
+    public String getLastPID() 
+            throws IOException {
+        return m_pidGeneration.getLastPID();
+    }
+
+}
