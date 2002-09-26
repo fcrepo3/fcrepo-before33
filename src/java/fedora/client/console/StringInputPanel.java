e3e8c0ffd17a78a202e2d7a0c89c29f1a7ac4ac4
Chris Wilper <cwilper@duraspace.org>
2002-09-26 15:46:26 +0000

    supports the option of sending <null>

--- src/java/fedora/client/console/StringInputPanel.java
+++ src/java/fedora/client/console/StringInputPanel.java
@@ -1,20 +1,48 @@
 package fedora.client.console;
 
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class StringInputPanel
         extends InputPanel {
-        
+
+    private JRadioButton m_nullRadioButton;
     private JTextField m_textField;
         
     public StringInputPanel() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JPanel nullPanel=new JPanel();
+        nullPanel.setLayout(new BorderLayout());
+        m_nullRadioButton=new JRadioButton("Use null");
+        nullPanel.add(m_nullRadioButton, BorderLayout.WEST);
+        add(nullPanel);
+
+        JPanel textPanel=new JPanel();
+        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.X_AXIS));
+        JRadioButton textRadioButton=new JRadioButton("Use text: ");
+        textRadioButton.setSelected(true);
+        textPanel.add(textRadioButton);
         m_textField=new JTextField(10);
-        add(m_textField);
+        textPanel.add(m_textField);
+        add(textPanel);
+
+        ButtonGroup g=new ButtonGroup();
+        g.add(m_nullRadioButton);
+        g.add(textRadioButton);
+
     }
     
     public Object getValue() {
-        return m_textField.getText();
+        if (m_nullRadioButton.isSelected()) {
+            return null;
+        } else {
+            return m_textField.getText();
+        }
     }
 
 }
