2207ceda526ac629a764866af06ff1c8ff0e95b8
Chris Wilper <cwilper@duraspace.org>
2002-09-30 19:29:39 +0000

    more implementation on a deployer

--- src/java/fedora/server/utilities/AxisUtility.java
+++ src/java/fedora/server/utilities/AxisUtility.java
@@ -2,8 +2,12 @@ package fedora.server.utilities;
 
 import fedora.server.errors.ServerException;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.xml.namespace.QName;
 import org.apache.axis.AxisFault;
+import org.apache.axis.client.AdminClient;
 
 public abstract class AxisUtility {
 
@@ -60,12 +64,45 @@ public abstract class AxisUtility {
         return fault;
     }
     
+    public static void showDeployUsage() {
+        System.out.println("Usage:");
+        System.out.println("    AxisUtility deploy wsdd_file admin_url timeout_seconds");
+    }
+    
     public static void main(String args[]) {
         if (args.length>0) {
            if (args[0].equals("deploy")) {
-               System.out.println("TODO");
+               if (args.length!=4) {
+                   showDeployUsage();
+               } else {
+                   File wsddFile=new File(args[1]);
+                   if (!wsddFile.exists()) {
+                       System.out.println("Error: wsdd_file " + args[1] + " does not exist.");
+                       showDeployUsage();
+                   } else {
+                       try {
+                           URL adminUrl=new URL(args[2]);
+                           String host=adminUrl.getHost();
+                           int port=adminUrl.getPort();
+                           if (port==-1) {
+                               port=80;
+                           }
+                           URL mainUrl=new URL("http", host, port, "/");
+                           String[] parms=new String[] {"-l" + args[2], wsddFile.toString()};
+                           int timeoutSeconds=Integer.parseInt(args[3]);
+                           // see openConnection... try to config timeout for connect
+                       } catch (MalformedURLException murle) {
+                           System.out.println("Error: admin_url " + args[2] + " is malformed.");
+                           showDeployUsage();
+                       } catch (NumberFormatException nfe) {
+                           System.out.println("Error: timeout_seconds " + args[3] + " is not an integer.");
+                           showDeployUsage();
+                       }
+                   }
+               }
            } else {
-               System.out.println("Unrecognized command: " + args[0]);
+               System.out.println("Error: Unrecognized command: " + args[0]);
+               System.out.println("The only valid command is deploy.");
            }
         }
     }
