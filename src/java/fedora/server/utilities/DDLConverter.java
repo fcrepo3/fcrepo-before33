2c9afcd695a1a4c85b730ebcef148282a8c7d695
Chris Wilper <cwilper@duraspace.org>
2002-10-01 14:53:32 +0000

    Updated javadoc to be more clear on the contract.

--- src/java/fedora/server/utilities/DDLConverter.java
+++ src/java/fedora/server/utilities/DDLConverter.java
@@ -1,5 +1,15 @@
 package fedora.server.utilities;
 
+/**
+ * Interface for a converter of TableSpec objects to RDBMS-specific DDL
+ * code.
+ * <p></p>
+ * Implementations of this class must be thread-safe.  That is, one
+ * instance can be used simultanously without problems.  This typically
+ * just means that no varying fields should be used.
+ * <p></p>
+ * Implementations must also have a public no-arg constructor.
+ */
 public interface DDLConverter {
 
     public abstract boolean supportsTableType();
