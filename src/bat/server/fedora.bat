3aafbf5651d73399c1656971bfc338a9d2611f52
Chris Wilper <cwilper@duraspace.org>
2002-10-11 14:06:09 +0000

    old script, no longer relevent

--- src/bat/server/fedora.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set CLASSPATH=%FEDORA_HOME%\tomcat41\webapps\fedora\WEB-INF\classes;%FEDORA_HOME%\tomcat41\common\lib\xerces2-2.0.2.jar
-java -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl -Dfedora.home=%FEDORA_HOME% fedora.server.Server %1 %2 %3 %4 %5 %6 %7 %8 %9
