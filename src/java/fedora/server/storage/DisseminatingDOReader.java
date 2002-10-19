80153f4f1663937d10c788b5f4b82f09909101f8
Ross Wayland <rlw@virginia.edu>
2002-10-19 19:57:14 +0000

    updated exception handling

--- src/java/fedora/server/storage/DisseminatingDOReader.java
+++ src/java/fedora/server/storage/DisseminatingDOReader.java
@@ -1,28 +1,25 @@
 package fedora.server.storage;
 
+import java.util.Date;
+
+import fedora.server.errors.GeneralException;
+import fedora.server.storage.types.DisseminationBindingInfo;
+import fedora.server.storage.types.ObjectMethodsDef;
+
 /**
  * <p>Title: DisseminatingDOReader.java</p>
  * <p>Description: Interface for getting dissemination information</p>
+ *
  * <p>Copyright: Copyright (c) 2002</p>
  * <p>Company: </p>
  * @author Ross Wayland
  * @version 1.0
  */
-
-// java imports
-import java.util.Date;
-import java.util.Vector;
-
-// fedora imports
-import fedora.server.storage.types.MIMETypedStream;
-import fedora.server.storage.types.ObjectMethodsDef;
-import fedora.server.errors.ObjectNotFoundException;
-import fedora.server.storage.types.DisseminationBindingInfo;
-
-
 public interface DisseminatingDOReader extends DOReader
 {
-  public DisseminationBindingInfo[] getDissemination(String PID, String bDefPID, String methodName,
-                                 Date versDateTime) throws ObjectNotFoundException;
-  public ObjectMethodsDef[] getObjectMethods(String PID, Date versDateTime) throws ObjectNotFoundException;
+  public DisseminationBindingInfo[] getDissemination(String PID, String bDefPID,
+      String methodName, Date versDateTime) throws GeneralException;
+
+  public ObjectMethodsDef[] getObjectMethods(String PID, Date versDateTime)
+      throws GeneralException;
 }
