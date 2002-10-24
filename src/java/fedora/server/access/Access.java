31fd43f31f5aa76ec52c978b1a65b2cd330cbaa8
Ross Wayland <rlw@virginia.edu>
2002-10-24 13:47:32 +0000

    tidying of javadocs

--- src/java/fedora/server/access/Access.java
+++ src/java/fedora/server/access/Access.java
@@ -10,7 +10,13 @@ import fedora.server.storage.types.ObjectMethodsDef;
 import fedora.server.storage.types.Property;
 
 /**
- * <p>Defines the access subsystem interface.</p>
+ * <p>Title: DefaultAccess.java</p>
+ * <p>Description: Defines the Fedora Access subsystem interface.</p>
+ *
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author Ross Wayland
+ * @version 1.0
  */
 public interface Access
 {
@@ -19,35 +25,45 @@ public interface Access
    * <p>Gets the persistent identifiers or PIDs of all Behavior Definition
    * objects associated with the specified digital object.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digitla object.
    * @param asOfDateTime The versioning datetime stamp.
    * @return An Array containing the list of Behavior Definition object PIDs.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public String[] getBehaviorDefinitions(Context context, String PID,
       Calendar asOfDateTime) throws ServerException;
 
   /**
-   * <p>Gets the method definitions associated with the specified Behavior
-   * Definition object.</p>
+   * <p>Gets the method definitions of the Behavior Mechanism object
+   * associated with the specified Behavior Definition object in the form of
+   * an array of method definitions.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object.
    * @param bDefPID The persistent identifier of the Behavior Definition object.
    * @param asOfDateTime The versioning datetime stamp.
    * @return An Array containing the list of method definitions.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public MethodDef[] getBehaviorMethods(Context context, String PID,
       String bDefPID, Calendar asOfDateTime) throws ServerException;
 
   /**
    * <p>Gets the method definitions associated with the specified Behavior
-   * Definition object.</p>
+   * Definition object in the form of XML as defined in the WSDL.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object.
    * @param bDefPID The persistent identifier of the Behavior Definition object.
    * @param asOfDateTime The versioning datetime stamp.
    * @return A MIME-typed stream containing the method definitions in the form
-   * of an XML fragment obtained from the WSDL in the associated Behavior
-   * Mechanism object.
+   *         of an XML fragment obtained from the WSDL in the associated
+   *         Behavior Mechanism object.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public MIMETypedStream getBehaviorMethodsAsWSDL(Context context, String PID,
       String bDefPID, Calendar asOfDateTime) throws ServerException;
@@ -57,26 +73,32 @@ public interface Access
    * of the associated Behavior Mechanism object of the specified digital
    * object.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object.
    * @param bDefPID The persistent identifier of the Behavior Definition object.
    * @param methodName The name of the method to be executed.
    * @param userParms An array of user-supplied method parameters consisting
-   * of name/value pairs.
+   *        of name/value pairs.
    * @param asOfDateTime The versioning datetime stamp.
    * @return A MIME-typed stream containing the result of the dissemination.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public MIMETypedStream getDissemination(Context context, String PID,
       String bDefPID, String methodName, Property[] userParms,
-      Calendar asOfDate) throws ServerException;
+      Calendar asOfDateTime) throws ServerException;
 
   /**
    * <p>Gets a list of all Behavior Definition object PIDs and method names
    * associated with the specified digital object.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object
    * @param asOfDateTime The versioning datetime stamp
    * @return An array of all methods associated with the specified
-   * digital object.
+   *         digital object.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public ObjectMethodsDef[] getObjectMethods(Context context, String PID,
       Calendar asOfDateTime) throws ServerException;
--- src/java/fedora/server/access/DefaultAccess.java
+++ src/java/fedora/server/access/DefaultAccess.java
@@ -1,8 +1,8 @@
 package fedora.server.access;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
@@ -13,8 +13,8 @@ import fedora.server.Server;
 import fedora.server.errors.ModuleInitializationException;
 import fedora.server.errors.GeneralException;
 import fedora.server.errors.ServerException;
-import fedora.server.storage.DOManager;
 import fedora.server.storage.DisseminatingDOReader;
+import fedora.server.storage.DOManager;
 import fedora.server.storage.types.DisseminationBindingInfo;
 import fedora.server.storage.types.MethodDef;
 import fedora.server.storage.types.MethodParmDef;
@@ -26,7 +26,9 @@ import fedora.server.utilities.DateUtility;
 /**
  *
  * <p>Title: DefaultAccess.java</p>
- * <p>Description: The Access Module, providing support for API-A.</p>
+ * <p>Description: The Access Module, providing support for the Fedora Access
+ * subsystem.</p>
+ *
  * <p>Copyright: Copyright (c) 2002</p>
  * <p>Company: </p>
  * @author Ross Wayland
@@ -34,7 +36,10 @@ import fedora.server.utilities.DateUtility;
  */
 public class DefaultAccess extends Module implements Access
 {
+  /** Constant holding value of xml MIME type. */
   private final static String CONTENT_TYPE_XML = "text/xml";
+
+  /** Current DOManager of the Fedora server. */
   private DOManager m_manager;
 
   /**
@@ -59,80 +64,77 @@ public class DefaultAccess extends Module implements Access
    *
    * @throws ModuleInitializationException If the module cannot be initialized.
    */
-  public void initModule()
-          throws ModuleInitializationException {
-      m_manager=(DOManager) getServer().getModule(
-              "fedora.server.storage.DOManager");
-      if (m_manager==null) {
-          throw new ModuleInitializationException("Can't get a DOManager "
-                  + "from Server.getModule", getRole());
-      }
+  public void initModule() throws ModuleInitializationException
+  {
+    m_manager=(DOManager) getServer().getModule(
+        "fedora.server.storage.DOManager");
+    if (m_manager == null)
+    {
+      throw new ModuleInitializationException("Can't get a DOManager "
+          + "from Server.getModule", getRole());
+    }
   }
 
   /**
    * <p>Gets the persistent identifiers or PIDs of all Behavior Definition
    * objects associated with the specified digital object.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digitla object.
    * @param asOfDateTime The versioning datetime stamp.
    * @return An Array containing the list of Behavior Definition object PIDs.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public String[] getBehaviorDefinitions(Context context, String PID,
       Calendar asOfDateTime) throws ServerException
   {
     Date versDateTime = DateUtility.convertCalendarToDate(asOfDateTime);
-    //try
-    //{
-      String[] behaviorDefs = null;
-      DisseminatingDOReader reader =
-          m_manager.getDisseminatingReader(context, PID);
-      behaviorDefs = reader.GetBehaviorDefs(versDateTime);
-      return behaviorDefs;
-    //} catch (Exception e)
-    //{
-    //  getServer().logWarning("Failed to get BDefs" + e.getMessage());
-    //}
-    //return null;
+    String[] behaviorDefs = null;
+    DisseminatingDOReader reader =
+        m_manager.getDisseminatingReader(context, PID);
+    behaviorDefs = reader.GetBehaviorDefs(versDateTime);
+    return behaviorDefs;
   }
 
   /**
-   * <p>Gets the method definitions associated with the specified Behavior
-   * Definition object.</p>
+   * <p>Gets the method definitions of the Behavior Mechanism object
+   * associated with the specified Behavior Definition object in the form of
+   * an array of method definitions.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object.
    * @param bDefPID The persistent identifier of the Behavior Definition object.
    * @param asOfDateTime The versioning datetime stamp.
    * @return An Array containing the list of method definitions.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public MethodDef[] getBehaviorMethods(Context context, String PID,
-                                        String bDefPID, Calendar asOfDateTime)
-      throws ServerException
+      String bDefPID, Calendar asOfDateTime) throws ServerException
   {
-    //try
-    //{
-      Date versDateTime = DateUtility.convertCalendarToDate(asOfDateTime);
-      DisseminatingDOReader reader =
-          m_manager.getDisseminatingReader(context, PID);
-      MethodDef[] methodResults =
-          reader.GetBMechMethods(bDefPID, versDateTime);
-      return methodResults;
-    //} catch (Exception e)
-    //{
-    //  getServer().logWarning(e.getMessage());
-    //}
-    //return null;
+    Date versDateTime = DateUtility.convertCalendarToDate(asOfDateTime);
+    DisseminatingDOReader reader =
+        m_manager.getDisseminatingReader(context, PID);
+    MethodDef[] methodResults =
+        reader.GetBMechMethods(bDefPID, versDateTime);
+    return methodResults;
   }
 
   /**
-   * <p>Gets the method definitions associated with the specified Behavior
-   * Definition object.</p>
+   * <p>Gets the method definitions of the Behavior Mechanism object
+   * associated with the specified Behavior Definition object in the form of
+   * XML as defined in the WSDL.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object.
    * @param bDefPID The persistent identifier of the Behavior Definition object.
    * @param asOfDateTime The versioning datetime stamp.
    * @return A MIME-typed stream containing the method definitions in the form
-   * of an XML fragment obtained from the WSDL in the associated Behavior
-   * Mechanism object.
+   *         of an XML fragment obtained from the WSDL in the associated
+   *         Behavior Mechanism object.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public MIMETypedStream getBehaviorMethodsAsWSDL(Context context,
       String PID, String bDefPID, Calendar asOfDateTime) throws ServerException
@@ -145,7 +147,6 @@ public class DefaultAccess extends Module implements Access
       DisseminatingDOReader reader =
           m_manager.getDisseminatingReader(context, PID);
       methodResults = reader.GetBMechMethodsWSDL(bDefPID, versDateTime);
-      // FIXME!! versioning based on datetime not yet implemented
       int byteStream = 0;
       while ((byteStream = methodResults.read()) >= 0)
       {
@@ -154,7 +155,8 @@ public class DefaultAccess extends Module implements Access
       methodResults.close();
       if (methodResults != null)
       {
-        MIMETypedStream methodDefs = new MIMETypedStream(CONTENT_TYPE_XML, baos.toByteArray());
+        MIMETypedStream methodDefs =
+            new MIMETypedStream(CONTENT_TYPE_XML, baos.toByteArray());
         return methodDefs;
       }
     } catch (IOException ioe)
@@ -173,64 +175,56 @@ public class DefaultAccess extends Module implements Access
    * of the associated Behavior Mechanism object of the specified digital
    * object.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object.
    * @param bDefPID The persistent identifier of the Behavior Definition object.
    * @param methodName The name of the method to be executed.
    * @param userParms An array of user-supplied method parameters consisting
-   * of name/value pairs.
+   *        of name/value pairs.
    * @param asOfDateTime The versioning datetime stamp.
    * @return A MIME-typed stream containing the result of the dissemination.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public MIMETypedStream getDissemination(Context context, String PID,
       String bDefPID, String methodName, Property[] userParms,
       Calendar asOfDateTime) throws ServerException
   {
-    //try
-    //{
-      Date versDateTime = DateUtility.convertCalendarToDate(asOfDateTime);
+    Date versDateTime = DateUtility.convertCalendarToDate(asOfDateTime);
+    DisseminatingDOReader reader =
+        m_manager.getDisseminatingReader(context, PID);
 
-      // Get the dissemination binding info by reading from Fast store
-      DisseminatingDOReader reader =
-          m_manager.getDisseminatingReader(context, PID);
-      DisseminationBindingInfo[] dissResults =
-          reader.getDissemination(PID, bDefPID, methodName, versDateTime);
+    // Get dissemination binding info.
+    DisseminationBindingInfo[] dissBindInfo =
+        reader.getDissemination(PID, bDefPID, methodName, versDateTime);
 
-      // Assemble the dissemination from the binding info
-      DisseminationService dissService = new DisseminationService();
-      MIMETypedStream dissemination =
-          dissService.assembleDissemination(userParms, dissResults);
-      return dissemination;
-    //} catch (Exception e)
-    //{
-    //  getServer().logWarning(e.getMessage());
-    //}
-   //return null;
+    // Assemble and execute the dissemination request from the binding info.
+    DisseminationService dissService = new DisseminationService();
+    MIMETypedStream dissemination =
+        dissService.assembleDissemination(userParms, dissBindInfo);
+    return dissemination;
   }
 
   /**
    * <p>Gets a list of all Behavior Definition object PIDs and method names
    * associated with the specified digital object.</p>
    *
+   * @param context The context of this request.
    * @param PID The persistent identifier of the digital object
    * @param asOfDateTime The versioning datetime stamp
    * @return An array of all methods associated with the specified
-   * digital object.
+   *         digital object.
+   * @throws ServerException If any type of error occurred fulfilling the
+   *         request.
    */
   public ObjectMethodsDef[] getObjectMethods(Context context, String PID,
       Calendar asOfDateTime) throws ServerException
   {
-    //try
-    //{
-      Date versDateTime = DateUtility.convertCalendarToDate(asOfDateTime);
-      DisseminatingDOReader reader =
-          m_manager.getDisseminatingReader(context, PID);
-      ObjectMethodsDef[] methodDefs =
-          reader.getObjectMethods(PID, versDateTime);
-      return methodDefs;
-    //} catch (Exception e)
-    //{
-    //  getServer().logWarning(e.getMessage());
-    //}
-    //return null;
+    Date versDateTime = DateUtility.convertCalendarToDate(asOfDateTime);
+    DisseminatingDOReader reader =
+        m_manager.getDisseminatingReader(context, PID);
+    ObjectMethodsDef[] methodDefs =
+        reader.getObjectMethods(PID, versDateTime);
+    return methodDefs;
   }
 }
