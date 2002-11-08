c209d1f8af10127ad16e6856273ea0254d113dbd
Ross Wayland <rlw@virginia.edu>
2002-11-08 16:41:42 +0000

    converted implementation to a Module

--- src/java/fedora/server/storage/DefaultExternalContentManager.java
+++ src/java/fedora/server/storage/DefaultExternalContentManager.java
@@ -4,21 +4,73 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
+import fedora.server.Context;
+import fedora.server.Module;
+import fedora.server.Server;
+import fedora.server.errors.ModuleInitializationException;
 import fedora.server.storage.types.MIMETypedStream;
 import fedora.server.errors.HttpServiceNotFoundException;
 
 /**
  * <p>Title: DefaultExternalContentManager.java</p>
- * <p>Description: Provides a service that obtains HTTP-accessible content.</p>
+ * <p>Description: Provides a mechanism to obtain external HTTP-accessible
+ * content.</p>
  *
  * <p>Copyright: Copyright (c) 2002</p>
  * <p>Company: </p>
  * @author Ross Wayland
  * @version 1.0
  */
-public class DefaultExternalContentManager implements ExternalContentManager
+public class DefaultExternalContentManager extends Module
+    implements ExternalContentManager
 {
+
+  /**
+   * <p> Creates a new DefaultExternalContentManager.</p>
+   *
+   * @param moduleParameters The name/value pair map of module parameters.
+   * @param server The server instance.
+   * @param role The module role name.
+   * @throws ModuleInitializationException If initialization values are
+   *         invalid or initialization fails for some other reason.
+   */
+  public DefaultExternalContentManager(Map moduleParameters,
+                                       Server server, String role)
+      throws ModuleInitializationException
+  {
+    super(moduleParameters, server, role);
+  }
+
+  /**
+   * Initializes the Module based on configuration parameters. The
+   * implementation of this method is dependent on the schema used to define
+   * the parameter names for the role of
+   * <code>fedora.server.storage.DefaultExternalContentManager</code>.
+   *
+   * @throws ModuleInitializationException If initialization values are
+   *         invalid or initialization fails for some other reason.
+   */
+  public void initModule() throws ModuleInitializationException
+  {
+    try
+    {
+      Server s_server = this.getServer();
+      s_server.logInfo("DefaultExternalContentManager initialized");
+
+      // Currently there are no parameters for this module.
+      // This is where any future parameters in the config file would be read.
+
+    } catch (Throwable th)
+    {
+      throw new ModuleInitializationException("An external content manager "
+          + "could not be instantiated. The underlying error was a "
+          + th.getClass().getName() + "The message was \""
+          + th.getMessage() + "\".", getRole());
+    }
+  }
+
   /**
    * A method that reads the contents of the specified URL and returns the
    * result as a MIMETypedStream
@@ -28,14 +80,14 @@ public class DefaultExternalContentManager implements ExternalContentManager
    * @throws HttpServiceNotFoundException If the URL connection could not
    *         be established.
    */
-  public MIMETypedStream getExternalContent(String urlString)
+  public MIMETypedStream getExternalContent(String URL)
       throws HttpServiceNotFoundException
   {
     try
     {
       MIMETypedStream httpContent = null;
       ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      URL url = new URL(urlString);
+      URL url = new URL(URL);
       HttpURLConnection connection = (HttpURLConnection)url.openConnection();
       String contentType = connection.getContentType();
       InputStream is = connection.getInputStream();
@@ -49,23 +101,9 @@ public class DefaultExternalContentManager implements ExternalContentManager
 
     } catch (Throwable th)
     {
-      throw new HttpServiceNotFoundException("HTTPService ERROR: "
+      throw new HttpServiceNotFoundException("ExternalContentManager ERROR: "
           + th.getClass().getName() + th.getMessage());
     }
   }
 
-  public static void main(String[] args)
-  {
-    DefaultExternalContentManager hs = new DefaultExternalContentManager();
-    String url = "http://icarus.lib.virginia.edu/test/dummy.html";
-    try
-    {
-      MIMETypedStream content = hs.getExternalContent(url);
-      System.out.println("MIME: "+content.MIMEType);
-      System.out.write(content.stream);
-    } catch (Exception e)
-    {
-      System.err.println(e.getMessage());
-    }
-  }
 }
