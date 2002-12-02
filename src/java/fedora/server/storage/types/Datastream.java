f8ba831173b78ec6685046cd9e1658aa96b9c1fb
Sandy Payette <spayette@duraspace.org>
2002-12-02 22:18:14 +0000

    changed type of DSControlGrp to String to accomodate new coding scheme.

--- src/java/fedora/server/storage/types/Datastream.java
+++ src/java/fedora/server/storage/types/Datastream.java
@@ -20,19 +20,19 @@ public class Datastream
 {
 
   private ArrayList m_auditRecordIdList;
-  
+
   public Datastream() {
       m_auditRecordIdList=new ArrayList();
   }
 
   /** ControlGrp for "Repository Managed Content" datastreams (internal) */
-  public final static int MANAGED_CONTENT = 1;
+  //public final static int MANAGED_CONTENT = 1;
 
   /** ControlGrp for "Implementor-Defined XML Metadata" datastreams (internal/inline) */
-  public final static int XML_METADATA = 2;
+  //public final static int XML_METADATA = 2;
 
   /** ControlGrp for "External Referenced Content" datastreams (external) */
-  public final static int EXTERNAL_REF = 3;
+  //public final static int EXTERNAL_REF = 3;
 
   /** Future(?): ControlGrp "External Service Request" datastreams  (external/request) */
   //public final static int EXTERNAL_REQUEST = 4;
@@ -50,7 +50,35 @@ public class Datastream
   public long DSSize;
 
   /** Control Group: MANAGED_CONTENT/XML_METADATA/EXTERNAL_REF */
-  public int DSControlGrp;
+  //public int DSControlGrp;
+
+  /** Datastream Control Group:
+   *  This indicates the nature of the repository's control over the
+   *  datastream content.  Values are:
+   *  P = Protected-ExternalRef.  The datastream content is external to
+   *      to the repository and referenced by a URL.  The content is not
+   *      under the direct custodianship of the repository.  However,
+   *      the datastream content is protected by the repository.
+   *      The repository will not reveal the underlying location of the
+   *      datastream content and will mediate all access to the content
+   *      by collaborating services (e.g., a behavior mechanism service).
+   *  E = ExternalRef.  The datastream content is external to the repository
+   *      and referenced by URL.  The content is not under the direct
+   *      custodianship of the repository.  The URL is considered public
+   *      so the repository does not worry about whether it exposes the
+   *      datastream location to collaborating services.
+   *  M = Managed-Internal.  The datastream content is stored and managed
+   *      by the repository.  The content is considered under the direct
+   *      custodianship of the repository.  The repository does not expose
+   *      the underlying storage location to collaborating services and
+   *      it mediates all access to the content by collaborating services.
+   *  X = Inline-XML-User-Metadata.  The datastream content is user-defined
+   *      XML metadata that is stored within the digital object XML file itself.
+   *      As such, it is intrinsically bound to the digital object, and by
+   *      implication, it is stored and managed by the repository.  The content
+   *      considered under the custodianship of the repository.
+   */
+  public String DSControlGrp;
 
   /** Info Type: DATA or one of the METS MDType values */
   public String DSInfoType;
@@ -58,12 +86,12 @@ public class Datastream
   public String DSState;
 
   public String DSLocation;
-  
+
   public List auditRecordIdList() {
       return m_auditRecordIdList;
   }
-  
-  public InputStream getContentStream() 
+
+  public InputStream getContentStream()
           throws StreamIOException {
       return null;
   }
