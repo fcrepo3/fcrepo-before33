13832270333e1396bd755a5c4a798a9192365f8b
Sandy Payette <spayette@duraspace.org>
2002-12-09 22:00:39 +0000

    Initial add of class to parse data elements and attributes out of the digital object that will be the subject of Level 3 validation.  At this time, PIDs of Behavior Definitions and Mechanisms are obtained. The handler is invoked within the DOValidatorXMLSchema class during schema validation, and the data elements and attributes are held on to by the validator module until they are needed in Level 3 validation.  This is to prevent any reparsing of the object during the validation process.  If Level 3 validation is run stand-alone, it will call the parsing routine.

---
+++ src/java/fedora/server/validation/DOIntegrityHandler.java
@@ -0,0 +1,125 @@
+package fedora.server.validation;
+
+/**
+ * <p>Title: DOIntegrityHandler.java</p>
+ * <p>Description: Parses digital object to pick up data elements and
+ * attributes that are necessary to perform Level 3 validation
+ * (Integrity checks).</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author Sandy Payette  payette@cs.cornell.edu
+ * @version 1.0
+ */
+
+import fedora.server.errors.*;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Vector;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+class DOIntegrityHandler extends DefaultHandler
+{
+  /** The namespace for METS */
+  private final static String METS="http://www.loc.gov/METS/";
+
+  /** The namespace for XLINK */
+  private final static String XLINK="http://www.w3.org/TR/xlink";
+  // Mets says the above, but the spec at http://www.w3.org/TR/xlink/
+  // says it's http://www.w3.org/1999/xlink
+
+  /**
+   * URI-to-namespace prefix mapping info from SAX2 startPrefixMapping events.
+   */
+  private HashMap nsPrefixMap;
+
+  /** The digital object integrity variables are those elements and
+   *  attributes that have been parsed out of the digital object xml
+   *  to be used as part of the referential integrity checks in
+   *  Level 3 Validation.
+   */
+  protected DOIntegrityVariables iVars;
+
+  // Variables for keeping state during SAX parse
+  private boolean rootFound = false;
+  private boolean inBehaviorSec = false;
+
+  // Temporary variables during the SAX parse
+  private Vector tmp_vBDefPIDs;
+  private Vector tmp_vBMechPIDs;
+  private Vector tmp_vDSURLs;
+
+  public void startDocument() throws SAXException
+  {
+    nsPrefixMap = new HashMap();
+    iVars = new DOIntegrityVariables();
+    tmp_vBDefPIDs = new Vector();
+    tmp_vBMechPIDs = new Vector();
+    tmp_vDSURLs = new Vector();
+  }
+
+  public void endDocument() throws SAXException
+  {
+    iVars.bDefPIDs = (String[])tmp_vBDefPIDs.toArray(new String[0]);
+    iVars.bMechPIDs = (String[])tmp_vBMechPIDs.toArray(new String[0]);
+  }
+
+  public void startPrefixMapping(String prefix, String uri) throws SAXException
+  {
+    nsPrefixMap.put(uri, prefix);
+  }
+
+  public void skippedEntity(String name) throws SAXException
+  {
+    StringBuffer sb = new StringBuffer();
+    sb.append('&');
+    sb.append(name);
+    sb.append(';');
+    char[] text = new char[sb.length()];
+    sb.getChars(0, sb.length(), text, 0);
+    this.characters(text, 0, text.length);
+  }
+
+
+  public void characters(char ch[], int start, int length)  throws SAXException
+  {
+  }
+
+  public void startElement(String namespaceURI, String localName, String qName, Attributes attrs)
+    throws SAXException
+  {
+    if (namespaceURI.equals(METS))
+    {
+      if (localName.equalsIgnoreCase("mets"))
+      {
+        rootFound = true;
+      }
+      else if (localName.equalsIgnoreCase("behaviorSec"))
+      {
+        inBehaviorSec = true;
+      }
+      else if (localName.equalsIgnoreCase("interfaceDef") && inBehaviorSec)
+      {
+        tmp_vBDefPIDs.add(attrs.getValue(XLINK, "href"));
+      }
+      else if (localName.equalsIgnoreCase("mechanism") && inBehaviorSec)
+      {
+        tmp_vBMechPIDs.add(attrs.getValue(XLINK, "href"));
+      }
+    }
+  }
+
+  public void endElement(String namespaceURI, String localName, String qName) throws SAXException
+  {
+    if (namespaceURI.equals(METS))
+    {
+      if (localName.equalsIgnoreCase("behaviorSec"))
+      {
+        inBehaviorSec = false;
+      }
+    }
+  }
+}
