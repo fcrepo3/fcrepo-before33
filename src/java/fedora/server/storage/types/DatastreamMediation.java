41294665a96ca928191131029f439fb3a3b53e1c
Ross Wayland <rlw@virginia.edu>
2002-12-17 19:46:39 +0000

    initial import. data structure to hold datastream mediation info

---
+++ src/java/fedora/server/storage/types/DatastreamMediation.java
@@ -0,0 +1,18 @@
+package fedora.server.storage.types;
+
+/**
+ * <p>Title: DatastreraMediation.java</p>
+ * <p>Description: A data structure containing information needed for the
+ * datastream mediation service.</p>
+ *
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author Ross Wayland
+ * @version 1.0
+ */
+public class DatastreamMediation
+{
+  public String mediatedDatastreamID = null;
+  public String dsLocation = null;
+  public String dsControlGroupType = null;
+}
