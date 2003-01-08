e9f347a0b506c791e21d6d13774e816a2094c306
Chris Wilper <cwilper@duraspace.org>
2003-01-08 16:55:50 +0000

    Allows for the selection of multiple objects.  The context menu uses ViewObjectXML one or more times depending on the number of objects selected.  The same idea should happen with other items on the context menu.

--- src/java/fedora/client/RepositoryBrowser.java
+++ src/java/fedora/client/RepositoryBrowser.java
@@ -24,18 +24,23 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.Font;
+import java.awt.Point;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import edu.cornell.dlrg.swing.jtable.DefaultSortTableModel;
 import edu.cornell.dlrg.swing.jtable.JSortTable;
 import fedora.client.Administrator;
+import fedora.client.actions.ViewObjectXML;
 import fedora.client.console.Console;
 import fedora.client.console.ConsoleSendButtonListener;
 import fedora.client.console.ConsoleCommand;
@@ -48,25 +53,16 @@ import fedora.server.types.gen.ObjectInfo;
 public class RepositoryBrowser
         extends JInternalFrame {
         
-    private Administrator m_mainFrame;
     private static FedoraAPIMServiceLocator m_locator=new FedoraAPIMServiceLocator();
 
-    private JPopupMenu m_singlePopup;
+    private JSortTable m_table;
     
-    public RepositoryBrowser(Administrator mainFrame) {
+    public RepositoryBrowser() {
         super("Repository Browser",
               true, //resizable
               true, //closable
               true, //maximizable
               true);//iconifiable
-        m_mainFrame=mainFrame;
-        m_singlePopup=new JPopupMenu();
-        JMenuItem i1=new JMenuItem("View XML");
-        JMenuItem i2=new JMenuItem("Export");
-        JMenuItem i3=new JMenuItem("Purge");
-        m_singlePopup.add(i1);
-        m_singlePopup.add(i2);
-        m_singlePopup.add(i3);
        
         String[] columnNames = {"PID", 
                                 "Label",
@@ -78,7 +74,7 @@ public class RepositoryBrowser
                                 "Last Modified"};
 
         try {
-        AutoLister a=new AutoLister(mainFrame.getHost(), mainFrame.getPort());
+        AutoLister a=new AutoLister(Administrator.getHost(), Administrator.getPort());
         Map m=a.list(null);
         Object[][] data=new Object[m.size()][8];
         Iterator pidIter=m.keySet().iterator();   
@@ -98,14 +94,15 @@ public class RepositoryBrowser
             i++;
         }
 
-        JSortTable table=new JSortTable(new DefaultSortTableModel(data, columnNames));
-        table.setPreferredScrollableViewportSize(new Dimension(400, 400));
-        table.setShowVerticalLines(false);
-        table.setCellSelectionEnabled(false);
-        table.setRowSelectionAllowed(true);
-        table.setUI(new RepositoryBrowser.BrowserTableUI());
+        DefaultSortTableModel model=new DefaultSortTableModel(data, columnNames);
+        m_table=new JSortTable(model);
+        m_table.setPreferredScrollableViewportSize(new Dimension(400, 400));
+        m_table.setShowVerticalLines(false);
+        m_table.setCellSelectionEnabled(false);
+        m_table.setRowSelectionAllowed(true);
+        m_table.setUI(new RepositoryBrowser.BrowserTableUI());
 
-        JScrollPane browsePanel = new JScrollPane(table);
+        JScrollPane browsePanel = new JScrollPane(m_table);
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(browsePanel, BorderLayout.CENTER);
 
@@ -123,9 +120,35 @@ public class RepositoryBrowser
             return new BasicTableUI.MouseInputHandler() {
                 public void mousePressed(MouseEvent e) {
                     if (SwingUtilities.isRightMouseButton(e) ) {
-                        //do your stuff here...    
-                        m_singlePopup.show(e.getComponent(), e.getX(), e.getY());
-                        System.out.println("Right click");
+                        int rowNum=m_table.rowAtPoint(new Point(e.getX(), e.getY()));
+                        if (rowNum >= 0) {
+                            int[] sRows=m_table.getSelectedRows();
+                            boolean clickedOnSelected=false;
+                            HashSet pids=new HashSet();
+                            for (int i=0; i<sRows.length; i++) {
+                                if (sRows[i]==rowNum) {
+                                    clickedOnSelected=true;
+                                }
+                                pids.add(m_table.getValueAt(sRows[i], m_table.convertColumnIndexToView(0)));
+                            }
+                            if (!clickedOnSelected) {
+                                pids=new HashSet();
+                                m_table.clearSelection();
+                                m_table.addRowSelectionInterval(rowNum, rowNum);
+                                pids.add(m_table.getValueAt(rowNum, m_table.convertColumnIndexToView(0)));
+                            }
+                            if (pids.size()==1) {
+                                Iterator pidIter=pids.iterator();
+                                new RepositoryBrowser.SingleSelectionPopup(
+                                        (String) pidIter.next())
+                                        .show(e.getComponent(), 
+                                        e.getX(), e.getY());
+                            } else {
+                                new RepositoryBrowser.MultiSelectionPopup(pids)
+                                        .show(e.getComponent(), 
+                                        e.getX(), e.getY());
+                            }
+                        }
                     } else {
                         // not a right click
                         super.mousePressed(e);    
@@ -135,4 +158,41 @@ public class RepositoryBrowser
         }
     }
     
+    public class SingleSelectionPopup
+            extends JPopupMenu {
+            
+        private String m_pid;
+        
+        public SingleSelectionPopup(String pid) {
+            super();
+            m_pid=pid;
+            JMenuItem i1=new JMenuItem(new ViewObjectXML(pid));
+            i1.setMnemonic(KeyEvent.VK_V);
+            i1.setToolTipText("Launches an XML viewer for the selected object.");
+            JMenuItem i2=new JMenuItem("Export");
+            JMenuItem i3=new JMenuItem("Purge");
+            add(i1);
+            add(i2);
+            add(i3);
+        }
+    }
+    
+    public class MultiSelectionPopup
+            extends JPopupMenu {
+            
+        private Set m_pids;
+        
+        public MultiSelectionPopup(Set pids) {
+            super();
+            m_pids=pids;
+            JMenuItem i1=new JMenuItem(new ViewObjectXML(pids));
+            i1.setMnemonic(KeyEvent.VK_V);
+            i1.setToolTipText("Launches an XML viewer for the selected objects.");
+            JMenuItem i2=new JMenuItem("Multi Export");
+            JMenuItem i3=new JMenuItem("Multi Purge");
+            add(i1);
+            add(i2);
+            add(i3);
+        }
+    }
 }
