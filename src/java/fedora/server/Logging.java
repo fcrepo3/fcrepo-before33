fef0664aa886240e8f9a4b6d5055f2b8d2fff21d
Chris Wilper <cwilper@duraspace.org>
2003-01-14 14:50:22 +0000

    interface for things classes that somehow take care of logging. namely, Module and Server.  Also useful for test impl classes that Modules wrap (see FieldSearchDBImpl) so they can do log messages the same way regardless of whether they're being invoked standalone or in the context of a Server.

---
+++ src/java/fedora/server/Logging.java
@@ -0,0 +1,32 @@
+package fedora.server;
+
+/**
+ * A class that has methods for logging.
+ * <p></p>
+ * The methods starting with <i>logging</i> can be used to quickly
+ * check whether a certain type of logging is enabled.  This
+ * helps in situations where the process of building the log
+ * message (before calling <i>logXXX</i>) takes enough time that
+ * it should not run unless necessary.
+ *
+ * @author cwilper@cs.cornell.edu
+ */
+public interface Logging { 
+
+    public abstract void logSevere(String message);
+    public abstract void logWarning(String message);
+    public abstract void logInfo(String message); 
+    public abstract void logConfig(String message); 
+    public abstract void logFine(String message); 
+    public abstract void logFiner(String message); 
+    public abstract void logFinest(String message); 
+    
+    public abstract boolean loggingSevere();
+    public abstract boolean loggingWarning();
+    public abstract boolean loggingInfo();
+    public abstract boolean loggingConfig();
+    public abstract boolean loggingFine();
+    public abstract boolean loggingFiner();
+    public abstract boolean loggingFinest();
+    
+}
