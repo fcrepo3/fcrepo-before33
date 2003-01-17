052f81b6f27d62c319e3a56db85910afec7d7c8e
Chris Wilper <cwilper@duraspace.org>
2003-01-17 13:56:49 +0000

    DOManager's simple read-only methods.

---
+++ src/java/fedora/server/storage/RepositoryReader.java
@@ -0,0 +1,43 @@
+package fedora.server.storage;
+
+import fedora.server.errors.ServerException;
+import fedora.server.Context;
+
+/**
+ * Provides context-appropriate digital object readers and 
+ * the ability to list all objects (accessible in the given
+ * context) within the repository.
+ *
+ * @author cwilper@cs.cornell.edu
+ */
+public interface RepositoryReader {
+        
+    /**
+     * Gets a digital object reader.
+     *
+     * @param context The context of this request.
+     * @param pid The PID of the object.
+     * @return A reader.
+     * @throws ServerException If anything went wrong.
+     */
+    public abstract DOReader getReader(Context context, String pid)
+            throws ServerException;
+            
+    public abstract DisseminatingDOReader getDisseminatingReader(
+            Context context, String pid) 
+            throws ServerException;
+
+    public abstract BMechReader getBMechReader(Context context, String pid)
+            throws ServerException;
+
+    public abstract BDefReader getBDefReader(Context context, String pid)
+            throws ServerException;
+
+    /**
+     * Gets a list of PIDs (accessible in the given context)
+     * of all objects in the repository.
+     */
+    public String[] listObjectPIDs(Context context)
+            throws ServerException;
+
+}
