21049240d13d6cfa671a4083a76f175498b6a3d3
Chris Wilper <cwilper@duraspace.org>
2003-02-11 14:03:25 +0000

    Fix bug where, when RepositoryException is thrown, servlet response was already committed because the contenttype had already been set, (i guess)... this could turn out to be slow for huge responses because it doesn't allow streaming, but it works for now.

--- src/java/fedora/oai/OAIProviderServlet.java
+++ src/java/fedora/oai/OAIProviderServlet.java
@@ -2,6 +2,8 @@ package fedora.oai;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServlet;
@@ -22,10 +24,21 @@ public abstract class OAIProviderServlet
         
     public void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        response.setContentType("text/xml");
         try {
-            getResponder().respond(request.getParameterMap(), response.getOutputStream());
-            response.flushBuffer();
+            HashMap params=new HashMap();
+            Enumeration enum=request.getParameterNames();
+            while (enum.hasMoreElements()) {
+                String name=(String) enum.nextElement();
+                params.put(name, request.getParameter(name));
+            }
+            ByteArrayOutputStream out=new ByteArrayOutputStream();
+            getResponder().respond(params, out);
+            try {
+                response.setContentType("text/xml");
+                response.getWriter().print(new String(out.toByteArray(), "UTF-8"));
+            } catch (UnsupportedEncodingException uee) {
+                // won't happen, since all java impls support UTF-8
+            }
         } catch (RepositoryException re) {
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, getMessage(re));
         }
