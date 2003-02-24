44bde61dc6b5a03a29cdc60e5764b088cfe5b8ef
Bill Niebel <wdn5e@virginia.edu>
2003-02-24 20:01:02 +0000

    run BatchTool under GUI in its own thread (1st commit)

---
+++ src/java/fedora/client/BatchThread.java
@@ -0,0 +1,50 @@
+package fedora.client;
+import java.util.Properties;
+import java.io.PrintStream;
+import javax.swing.JTextArea;
+import fedora.client.BatchOutput;
+import fedora.client.batch.BatchTool;
+public class BatchThread extends Thread {
+	private String leadText = "";
+	private Properties properties = null;
+	private Properties nullProperties = null;
+	private BatchOutput batchOutput = null;
+	private JTextArea jTextArea = null;
+	private PrintStream originalOut = null;
+	private PrintStream originalErr = null;	
+	private PrintStream printStream = null;	
+		
+	public BatchThread(BatchOutput batchOutput, JTextArea jTextArea, String leadText) throws Exception {
+		this.batchOutput = batchOutput;
+		this.jTextArea = jTextArea;
+		this.leadText = leadText;
+		BatchOutputCatcher batchOutputCatcher = new BatchOutputCatcher(jTextArea);
+		//BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(batchOutputCatcher);
+		printStream = new PrintStream(batchOutputCatcher, true); //bufferedOutputStream		
+	}
+	
+	public void setProperties (Properties properties) {
+		this.properties = properties;
+	}
+
+	public void run() {
+		try {
+			jTextArea.setText(leadText + "\n");
+			originalOut = System.out;
+			originalErr = System.err;
+			System.setOut(printStream);
+			System.setErr(printStream);
+			batchOutput.setVisible(true);			
+			BatchTool batchTool = new BatchTool(properties,nullProperties,nullProperties);
+			batchTool.prep();
+			batchTool.process();
+		} catch (Exception e) {
+		} finally {
+			System.setOut(originalOut);
+			originalOut = null;
+			System.setErr(originalErr);
+			originalErr = null;
+		}
+	}
+}
+
