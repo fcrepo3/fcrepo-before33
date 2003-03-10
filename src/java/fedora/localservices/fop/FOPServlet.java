5a465a2d64cf5a56a290d709ded47fbe6bd515a8
Chris Wilper <cwilper@duraspace.org>
2003-03-10 03:05:39 +0000

    a local service to translate from .fo format to .pdf, using apache's fop formatter engine.

---
+++ src/java/fedora/localservices/fop/FOPServlet.java
@@ -0,0 +1,78 @@
+package fedora.localservices.fop;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import org.apache.fop.apps.Driver;
+import org.apache.fop.apps.Version;
+import org.apache.fop.apps.XSLTInputHandler;
+import org.apache.fop.messaging.MessageHandler;
+
+import org.apache.avalon.framework.logger.ConsoleLogger;
+import org.apache.avalon.framework.logger.Logger;
+
+/**
+ * Servlet for generating and serving a PDF, given the URL to an XSL-FO file.
+ *
+ * Servlet param is:
+ * <ul>
+ *   <li>source: the path to a formatting object file to render
+ * </ul>
+ */
+public class FOPServlet extends HttpServlet {
+
+    public static final String FO_REQUEST_PARAM = "source";
+    Logger log = null;
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException {
+        if (log == null) {
+            log = new ConsoleLogger(ConsoleLogger.LEVEL_WARN);
+            MessageHandler.setScreenLogger(log);
+        }
+        try {
+            String foParam = request.getParameter(FO_REQUEST_PARAM);
+
+            if (foParam != null) {
+                renderFO(new InputSource(foParam), response);
+            } else {
+                PrintWriter out = response.getWriter();
+                out.println("<html><head><title>Error</title></head>\n"+
+                            "<body><h1>FOPServlet Error</h1><h3>No 'source' "+
+                            "request param given.</body></html>");
+            }
+        } catch (ServletException ex) {
+            throw ex;
+        }
+        catch (Exception ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    public void renderFO(InputSource foFile,
+                         HttpServletResponse response) throws ServletException {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            response.setContentType("application/pdf");
+
+            Driver driver = new Driver(foFile, out);
+            driver.setLogger(log);
+            driver.setRenderer(Driver.RENDER_PDF);
+            driver.run();
+
+            byte[] content = out.toByteArray();
+            response.setContentLength(content.length);
+            response.getOutputStream().write(content);
+            response.getOutputStream().flush();
+        } catch (Exception ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+}
