9277f376d445d9e4b1dafaf79a785460ae411999
Chris Wilper <cwilper@duraspace.org>
2003-04-09 21:08:01 +0000

    minor mods.. with this change, API-A-Lite is basically finished. However, FieldSearchServlet seems to have a bug with resuming fieldsearch operations when the output format is xml...

--- initenv.bat
+++ initenv.bat
@@ -1,5 +1,8 @@
 @echo off
 
+rem You may wish to set these values permanently rather than
+rem running this script...
+
 set FEDORA_DEV=c:\mellon
 set FEDORA_HOME=%FEDORA_DEV%\dist\server
 set PATH=%PATH%;%FEDORA_DEV%\res\ant\bin;%FEDORA_HOME%\bin
