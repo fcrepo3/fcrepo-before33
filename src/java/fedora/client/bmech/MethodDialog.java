9f0b8c75f07b8f72086271fb44fec21e7bfb7a22
Sandy Payette <spayette@duraspace.org>
2003-04-23 20:19:33 +0000

    finished up work on this.

--- src/java/fedora/client/bmech/MethodDialog.java
+++ src/java/fedora/client/bmech/MethodDialog.java
@@ -7,6 +7,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
+import java.awt.Component;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import java.awt.event.ActionListener;
@@ -18,45 +19,64 @@ public class MethodDialog extends JDialog {
     private JTextField methodDescription;
     private MethodsPane parent;
 
-    //public MethodDialog(JFrame owner, String title, boolean modal)
     public MethodDialog(MethodsPane parent, String title, boolean modal)
     {
         super();
-        //super(owner, title, modal);
         this.parent = parent;
         setTitle(title);
         setModal(modal);
         setSize(300, 200);
-        getContentPane().setLayout(new BorderLayout());
-
-        // Text Fields Panel
-        JPanel textPanel = new JPanel();
-        textPanel.setLayout(new GridLayout(3,2));
-        textPanel.add(new JLabel("Method Name: "));
-        textPanel.add(methodName = new JTextField());
-        textPanel.add(new JLabel("Method Description: "));
-        textPanel.add(methodDescription = new JTextField());
-
-        // Dialog Buttons Panel
-        JButton done = new JButton("Done");
-        done.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e) {
-            saveMethodFields();
-          }
-        } );
-        JButton cancel = new JButton("Cancel");
-        cancel.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e) {
-            cancel();
-          }
-        } );
-        JPanel mainButtonPanel = new JPanel();
-        mainButtonPanel.add(done);
-        mainButtonPanel.add(cancel);
-
-        getContentPane().add(textPanel, BorderLayout.CENTER);
-        getContentPane().add(mainButtonPanel, BorderLayout.SOUTH);
+        render();
         setVisible(true);
+
+    }
+    public MethodDialog(MethodsPane parent, String title, boolean modal,
+      String txt_methodName, String txt_methodDesc)
+    {
+        super();
+        this.parent = parent;
+        setTitle(title);
+        setModal(modal);
+        setSize(300, 200);
+        render();
+        methodName.setText(txt_methodName);
+        methodDescription.setText(txt_methodDesc);
+        this.setVisible(true);
+    }
+
+    private void render()
+    {
+      getContentPane().setLayout(new BorderLayout());
+
+      // Text Fields Panel
+      JPanel textPanel = new JPanel();
+      textPanel.setLayout(new GridLayout(3,2));
+      textPanel.add(new JLabel("Method Name: "));
+      textPanel.add(methodName = new JTextField());
+      textPanel.add(new JLabel("Method Description: "));
+      textPanel.add(methodDescription = new JTextField());
+      textPanel.setName("text");
+
+      // Dialog Buttons Panel
+      JButton done = new JButton("OK");
+      done.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          saveMethodFields();
+        }
+      } );
+      JButton cancel = new JButton("Cancel");
+      cancel.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          cancel();
+        }
+      } );
+      JPanel mainButtonPanel = new JPanel();
+      mainButtonPanel.add(done);
+      mainButtonPanel.add(cancel);
+
+      getContentPane().add(textPanel, BorderLayout.CENTER);
+      getContentPane().add(mainButtonPanel, BorderLayout.SOUTH);
+      return;
     }
 
     private void saveMethodFields()
@@ -64,10 +84,10 @@ public class MethodDialog extends JDialog {
       System.out.println("Saving values back in parent");
       try
       {
-      parent.setBMechMethod(
-        methodName.getText().trim(), methodDescription.getText().trim());
-      setVisible(false);
-      dispose();
+        parent.setBMechMethod(
+          methodName.getText().trim(), methodDescription.getText().trim());
+        setVisible(false);
+        dispose();
       }
       catch (BMechBuilderException e)
       {
