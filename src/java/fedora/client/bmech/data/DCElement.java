6be624bc28609cafb88b9cfec93892997c2885a5
Sandy Payette <spayette@duraspace.org>
2003-04-24 00:07:58 +0000

    initial add of dc element data structure.

---
+++ src/java/fedora/client/bmech/data/DCElement.java
@@ -0,0 +1,20 @@
+package fedora.client.bmech.data;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class DCElement
+{
+  public String elementName = null;
+  public String elementValue = null;
+
+  public DCElement()
+  {
+  }
+}
