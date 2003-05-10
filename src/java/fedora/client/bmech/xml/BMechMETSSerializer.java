d07826e1580c6167b24bcd484663e506e7d26258
Sandy Payette <spayette@duraspace.org>
2003-05-10 18:59:05 +0000

    added serialization for dsInputSpec, fileSec, structMap, and behaviorSec.

--- src/java/fedora/client/bmech/xml/BMechMETSSerializer.java
+++ src/java/fedora/client/bmech/xml/BMechMETSSerializer.java
@@ -58,12 +58,17 @@ public class BMechMETSSerializer
   private Document document;
   private Element root;
   private Element header;
+  private Element bMechFileSec;
+  private Vector docDSIDs = new Vector();
+  private Element bMechStructMap;
+  private Element bMechBehaviorSec;
   private Element profileNode;
+  private Element dsInputNode;
   private Element dcNode;
   private Element mmapNode;
   private Element wsdlNode;
 
-  public BMechMETSSerializer(BMechTemplate bMechData,
+  public BMechMETSSerializer(BMechTemplate bMechData, Element dsInputSpec,
     Element methodMap, Element wsdl) throws BMechBuilderException
   {
     try
@@ -78,7 +83,7 @@ public class BMechMETSSerializer
         " IO Exception initializing PrintWriter in constructor.");
     }
     initializeTree();
-    genMETS(bMechData, methodMap, wsdl);
+    genMETS(bMechData, dsInputSpec, methodMap, wsdl);
     finalizeTree();
   }
 
@@ -100,6 +105,13 @@ public class BMechMETSSerializer
     }
     root = (Element)document.createElementNS(METS, "METS:mets");
     header = (Element)document.createElementNS(METS, "METS:metsHdr");
+    bMechFileSec = (Element)document.createElementNS(METS, "METS:fileSec");
+    bMechStructMap = (Element)document.createElementNS(METS, "METS:structMap");
+    bMechStructMap.setAttribute("ID", "S1");
+    bMechStructMap.setAttribute("TYPE", "fedora:dsBindingMap");
+    bMechBehaviorSec = (Element)document.createElementNS(METS, "METS:behaviorSec");
+    bMechBehaviorSec.setAttribute("ID", "DISS1");
+    bMechBehaviorSec.setAttribute("STATUS", "A");
   }
 
   private void finalizeTree()
@@ -107,12 +119,16 @@ public class BMechMETSSerializer
     // put the tree together
     document.appendChild(root);
     root.appendChild(header);
+    root.appendChild(dsInputNode);
     root.appendChild(mmapNode);
     root.appendChild(wsdlNode);
+    root.appendChild(bMechFileSec);
+    root.appendChild(bMechStructMap);
+    root.appendChild(bMechBehaviorSec);
   }
 
-  private void genMETS(BMechTemplate bMechData, Element methodMap, Element wsdl)
-    throws BMechBuilderException
+  private void genMETS(BMechTemplate bMechData, Element dsInputSpec,
+    Element methodMap, Element wsdl) throws BMechBuilderException
   {
     // METS root element
     root.setAttributeNS(XMLNS, "xmlns:METS", METS);
@@ -142,33 +158,144 @@ public class BMechMETSSerializer
     agent.appendChild(agentName);
     agent.appendChild(agentNote);
     header.appendChild(agent);
-    setInlineMD(methodMap, wsdl);
+
+    setInlineMD(dsInputSpec, methodMap, wsdl);
+    setBMechFileSec(bMechData);
+    setBMechStructMap(bMechData);
+    setBMechBehaviorSec(bMechData);
   }
 
-  private void setInlineMD(Element methodMap, Element wsdl)
+  private void setBMechFileSec(BMechTemplate bMechData)
+  {
+    // METS fileSec element for the bMech
+    Element datastreamsFileGrp = document.createElementNS(METS, "METS:fileGrp");
+    datastreamsFileGrp.setAttribute("ID", "DATASTREAMS");
+    // FIXIT!  do a for loop to encode array of documents interned in the GUI
+    Datastream[] docs = bMechData.getDocDatastreams();
+    for (int i=0; i<docs.length; i++)
+    {
+      Element dsFileGrp = document.createElementNS(METS, "METS:fileGrp");
+      String dsid = "DS" + (i+1);
+      dsFileGrp.setAttribute("ID", dsid);
+      dsFileGrp.setAttribute("STATUS", "A");
+      Element dsFile = document.createElementNS(METS, "METS:file");
+      dsFile.setAttribute("ID", (dsid + ".0"));
+      dsFile.setAttribute("SEQ", "0");
+      dsFile.setAttribute("MIMETYPE", docs[i].dsMIMEType);
+      dsFile.setAttribute("OWNERID", docs[i].dsControlGrpType);
+      dsFile.setAttribute("STATUS", "A");
+      Element dsFileLoc = document.createElementNS(METS, "METS:FLocat");
+      dsFileLoc.setAttribute("LOCTYPE", "URL");
+      dsFileLoc.setAttributeNS(XLINK, "xlink:href", docs[i].dsURL);
+      dsFileLoc.setAttributeNS(XLINK, "xlink:title", docs[i].dsLabel);
+      dsFile.appendChild(dsFileLoc);
+      dsFileGrp.appendChild(dsFile);
+      datastreamsFileGrp.appendChild(dsFileGrp);
+      docDSIDs.add(dsid);
+    }
+    bMechFileSec.appendChild(datastreamsFileGrp);
+  }
+
+  private void setBMechStructMap(BMechTemplate bMechData)
+  {
+    // METS structMap element for the bMech
+    // This structMap represents the datastream bindings for the
+    // bootstrap disseminator known by the PID fedora-system:2
+
+    Element mainDiv = document.createElementNS(METS, "METS:div");
+    mainDiv.setAttribute("TYPE", "fedora-system:2");
+    mainDiv.setAttribute("LABEL", "Datastream Binding Map for Fedora Bootstrap Mechanism");
+
+    Element dsInputSpecDiv =
+      setDiv("FEDORA-TO-WSDL-DSINPUTSPEC",
+      "XML data that describes the requirements for Datastreams that will be used as input to this service",
+      "DSINPUTSPEC");
+    mainDiv.appendChild(dsInputSpecDiv);
+
+    Element methodMapDiv =
+      setDiv("FEDORA-TO-WSDL-METHODMAP",
+      "XML data that enables Fedora to understand how to use the service WSDL",
+      "METHODMAP");
+    mainDiv.appendChild(methodMapDiv);
+
+    Element wsdlDiv =
+      setDiv("WSDL",
+      "Service definition in WSDL format.",
+      "WSDL");
+    mainDiv.appendChild(wsdlDiv);
+
+    String[] DSIDs = (String[])docDSIDs.toArray(new String[0]);
+    for (int i=0; i<DSIDs.length; i++)
+    {
+      Element docDiv =
+        setDiv("SERVICEDOC",
+        "Documentation fo the Service",
+        DSIDs[i]);
+      mainDiv.appendChild(docDiv);
+    }
+    bMechStructMap.appendChild(mainDiv);
+  }
+
+  private Element setDiv(String type, String label, String dsid)
+  {
+    Element div = document.createElementNS(METS, "METS:div");
+    div.setAttribute("TYPE", type);
+    div.setAttribute("LABEL", label);
+    Element file = document.createElementNS(METS, "METS:fptr");
+    file.setAttribute("FILEID", dsid);
+    div.appendChild(file);
+    return div;
+  }
+
+  private void setBMechBehaviorSec(BMechTemplate bMechData)
+  {
+    // METS behaviorSec element for the Bootstrap Disseminator on the bMech
+    Element serviceBinding = document.createElementNS(METS, "METS:serviceBinding");
+    serviceBinding.setAttribute("ID", "DISS1.0");
+    serviceBinding.setAttribute("STRUCTID", "S1");
+    serviceBinding.setAttribute("BTYPE", "fedora-system:1");
+    serviceBinding.setAttribute("LABEL", "Bootstrap Behaviors for a bMech object");
+
+    Element bdef = document.createElementNS(METS, "METS:interfaceMD");
+    bdef.setAttribute("LABEL", "Bootstrap Behavior Definition");
+    bdef.setAttribute("LOCTYPE", "URN");
+    bdef.setAttributeNS(XLINK, "xlink:href", "fedora-system:1");
+    serviceBinding.appendChild(bdef);
+
+    Element bmech = document.createElementNS(METS, "METS:serviceBindingMD");
+    bmech.setAttribute("LABEL", "Bootstrap Behavior Mechanism");
+    bmech.setAttribute("LOCTYPE", "URN");
+    bmech.setAttributeNS(XLINK, "xlink:href", "fedora-system:2");
+    serviceBinding.appendChild(bmech);
+
+    bMechBehaviorSec.appendChild(serviceBinding);
+  }
+
+  private void setInlineMD(Element dsInputSpec, Element methodMap, Element wsdl)
     throws BMechBuilderException
   {
+    setDSInputSpec(dsInputSpec);
     setMethodMap(methodMap);
     setWSDL(wsdl);
   }
 
-  private void setServiceProfile(Element serviceProfile)
+  private void setDSInputSpec(Element dsInputSpec) throws BMechBuilderException
   {
-    profileNode = document.createElementNS(METS, "METS:amdSec");
-    profileNode.setAttribute("ID", "SERVICE-PROFILE");
+    dsInputNode = document.createElementNS(METS, "METS:amdSec");
+    dsInputNode.setAttribute("ID", "DSINPUTSPEC");
     Element techMD = document.createElementNS(METS, "METS:techMD");
-    techMD.setAttribute("ID", "SERVICE-PROFILE1.0");
+    techMD.setAttribute("ID", "DSINPUTSPEC1.0");
     techMD.setAttribute("STATUS", "A");
     Element mdWrap = document.createElementNS(METS, "METS:mdWrap");
     mdWrap.setAttribute("MIMETYPE", "text/xml");
     mdWrap.setAttribute("MDTYPE", "OTHER");
-    mdWrap.setAttribute("LABEL", "Service Profile - Technical description of the service");
+    mdWrap.setAttribute("LABEL", "Datastream Input Specification for Service");
     Element xmlData = document.createElementNS(METS, "METS:xmlData");
-    profileNode.appendChild(
-      techMD.appendChild(
-        mdWrap.appendChild(
-          xmlData.appendChild(
-            serviceProfile))));
+    Node importDSInput = document.importNode(dsInputSpec, true);
+    xmlData.appendChild(importDSInput);
+    mdWrap.appendChild(xmlData);
+    techMD.appendChild(mdWrap);
+    dsInputNode.appendChild(techMD);
   }
 
   private void setMethodMap(Element methodMap) throws BMechBuilderException
@@ -184,16 +311,10 @@ public class BMechMETSSerializer
     mdWrap.setAttribute("LABEL", "Mapping of WSDL to Fedora notion of Method Definitions");
     Element xmlData = document.createElementNS(METS, "METS:xmlData");
     Node importMethodMap = document.importNode(methodMap, true);
-    //xmlData.appendChild(importMethodMap);
-    //mdWrap.appendChild(xmlData);
-    //techMD.appendChild(mdWrap);
-    //mmap.appendChild(techMD);
-
-    mmapNode.appendChild(
-      techMD.appendChild(
-        mdWrap.appendChild(
-          xmlData.appendChild(
-            importMethodMap))));
+    xmlData.appendChild(importMethodMap);
+    mdWrap.appendChild(xmlData);
+    techMD.appendChild(mdWrap);
+    mmapNode.appendChild(techMD);
   }
 
   private void setWSDL(Element wsdl) throws BMechBuilderException
@@ -209,11 +330,29 @@ public class BMechMETSSerializer
     mdWrap.setAttribute("LABEL", "WSDL definition of service");
     Element xmlData = document.createElementNS(METS, "METS:xmlData");
     Node importWSDL = document.importNode(wsdl, true);
-    wsdlNode.appendChild(
-      techMD.appendChild(
-        mdWrap.appendChild(
-          xmlData.appendChild(
-            importWSDL))));
+    xmlData.appendChild(importWSDL);
+    mdWrap.appendChild(xmlData);
+    techMD.appendChild(mdWrap);
+    wsdlNode.appendChild(techMD);
+  }
+
+  private void setServiceProfile(Element serviceProfile)
+  {
+    profileNode = document.createElementNS(METS, "METS:amdSec");
+    profileNode.setAttribute("ID", "SERVICE-PROFILE");
+    Element techMD = document.createElementNS(METS, "METS:techMD");
+    techMD.setAttribute("ID", "SERVICE-PROFILE1.0");
+    techMD.setAttribute("STATUS", "A");
+    Element mdWrap = document.createElementNS(METS, "METS:mdWrap");
+    mdWrap.setAttribute("MIMETYPE", "text/xml");
+    mdWrap.setAttribute("MDTYPE", "OTHER");
+    mdWrap.setAttribute("LABEL", "Service Profile - Technical description of the service");
+    Element xmlData = document.createElementNS(METS, "METS:xmlData");
+    Node importProfile = document.importNode(serviceProfile, true);
+    xmlData.appendChild(importProfile);
+    mdWrap.appendChild(xmlData);
+    techMD.appendChild(mdWrap);
+    profileNode.appendChild(techMD);
   }
 
   public void printMETS()
