4df580dda5d89f1b5a315f2d2eefb39f4832fe0e
Sandy Payette <spayette@duraspace.org>
2003-05-10 18:57:25 +0000

    new class to collect data about bmech documentation datastreams.

---
+++ src/java/fedora/client/bmech/data/Datastream.java
@@ -0,0 +1,22 @@
+package fedora.client.bmech.data;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class Datastream
+{
+  public String dsMIMEType = null;
+  public String dsLabel = null;
+  public String dsURL = null;
+  public String dsControlGrpType = null;
+
+  public Datastream()
+  {
+  }
+}
