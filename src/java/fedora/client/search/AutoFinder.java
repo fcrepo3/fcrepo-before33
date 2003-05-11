21bf3e2877746d762bbd4defadb74c928a5b96a4
Chris Wilper <cwilper@duraspace.org>
2003-05-11 03:47:04 +0000

    minor update... autofinder does command-line stuff for fedora-find, and search is the replacement query panel.

--- src/java/fedora/client/search/AutoFinder.java
+++ src/java/fedora/client/search/AutoFinder.java
@@ -12,12 +12,18 @@ import org.apache.axis.types.NonNegativeInteger;
 
 import fedora.client.APIAStubFactory;
 import fedora.server.access.FedoraAPIA;
+import fedora.server.search.Condition;
 import fedora.server.types.gen.FieldSearchQuery;
 import fedora.server.types.gen.FieldSearchResult;
+import fedora.server.types.gen.ListSession;
+import fedora.server.types.gen.ObjectFields;
 
 public class AutoFinder {
 
-    private FedoraAPIA m_apia;    
+    private FedoraAPIA m_apia;
+    
+    public static SimpleDateFormat DATE_FORMATTER=
+            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
     public AutoFinder(String host, int port, String user, String pass) 
             throws MalformedURLException, ServiceException {
@@ -47,8 +53,90 @@ public class AutoFinder {
             throws RemoteException {
         return skeleton.resumeFindObjects(sessionToken);
     }
+    
+    public static void showUsage(String message) {
+        System.err.println(message);
+        System.err.println("Usage: fedora-find host port fields phrase");
+        System.err.println("");
+        System.err.println("    hostname - The Fedora server host or ip address.");
+        System.err.println("        port - The Fedora server port.");
+        System.err.println("      fields - Space-delimited list of fields.");
+        System.err.println("      phrase - Phrase to search for in any field (with ? and * wildcards)");
+    }
+    
+    public static void printValue(String name, String value) {
+        if (value!=null) System.out.println("   " + name + "  " + value);
+    }
+    
+    public static void printValue(String name, String[] value) {
+        if (value!=null) {
+            for (int i=0; i<value.length; i++) {
+                AutoFinder.printValue(name, value[i]);
+            }
+        }
+    }
+    
+    public static void printValue(String name, Calendar value) {
+        if (value!=null) {
+            AutoFinder.printValue(name, AutoFinder.DATE_FORMATTER.format(value.getTime()));
+        }
+    }
 
     public static void main(String[] args) {
+        if (args.length!=4) {
+            AutoFinder.showUsage("Four arguments required.");
+        }
+        try {
+            AutoFinder finder=new AutoFinder(args[0], Integer.parseInt(args[1]),
+                    null, null);
+            FieldSearchQuery query=new FieldSearchQuery();
+            query.setTerms(args[3]);
+            FieldSearchResult result=finder.findObjects(args[2].split(" "), 
+                    20, query);
+            int matchNum=0;
+            while (result!=null) {
+                for (int i=0; i<result.getResultList().length; i++) {
+                    ObjectFields o=result.getResultList()[i];
+                    matchNum++;
+                    System.out.println("#" + matchNum);
+                    AutoFinder.printValue("pid        ", o.getPid());
+                    AutoFinder.printValue("fType      ", o.getFType());
+                    AutoFinder.printValue("cModel     ", o.getCModel());
+                    AutoFinder.printValue("state      ", o.getState());
+                    AutoFinder.printValue("locker     ", o.getLocker());
+                    AutoFinder.printValue("cDate      ", o.getCDate());
+                    AutoFinder.printValue("mDate      ", o.getMDate());
+                    AutoFinder.printValue("dcmDate    ", o.getDcmDate());
+                    AutoFinder.printValue("bDef       ", o.getBDef());
+                    AutoFinder.printValue("bMech      ", o.getBMech());
+                    AutoFinder.printValue("title      ", o.getTitle());
+                    AutoFinder.printValue("creator    ", o.getCreator());
+                    AutoFinder.printValue("subject    ", o.getSubject());
+                    AutoFinder.printValue("description", o.getDescription());
+                    AutoFinder.printValue("publisher  ", o.getPublisher());
+                    AutoFinder.printValue("contributor", o.getContributor());
+                    AutoFinder.printValue("date       ", o.getDate());
+                    AutoFinder.printValue("type       ", o.getType());
+                    AutoFinder.printValue("format     ", o.getFormat());
+                    AutoFinder.printValue("identifier ", o.getIdentifier());
+                    AutoFinder.printValue("source     ", o.getSource());
+                    AutoFinder.printValue("language   ", o.getLanguage());
+                    AutoFinder.printValue("relation   ", o.getRelation());
+                    AutoFinder.printValue("coverage   ", o.getCoverage());
+                    AutoFinder.printValue("rights     ", o.getRights());
+                    System.out.println("");
+                }
+                ListSession sess=result.getListSession();
+                if (sess!=null) {
+                    result=finder.resumeFindObjects(sess.getToken());
+                } else {
+                    result=null;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e.getClass().getName()
+                    + ((e.getMessage()==null) ? "" : ": " + e.getMessage()));
+        }
     }
 
 }
