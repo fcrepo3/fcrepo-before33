865297b20bde81a06df5b3cb44d7152effac8e2a
Ross Wayland <rlw@virginia.edu>
2003-07-18 16:37:39 +0000

    fixed typo in comment

--- src/sh/client/fedora-admin.sh
+++ src/sh/client/fedora-admin.sh
@@ -36,7 +36,7 @@ if [ ! -f "$THIS_JAVA_HOME/bin/orbd" ]; then
   exit 1
 fi
 
-echo "Starting Fedora server..."
+echo "Starting Fedora Administrative Client..."
 
 OLD_JAVA_HOME=$JAVA_HOME
 JAVA_HOME=$THIS_JAVA_HOME
