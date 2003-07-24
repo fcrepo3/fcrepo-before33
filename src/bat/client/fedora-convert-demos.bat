7f1055240a6d26e29e6b0e5380dd1a56f59eaf7a
Ross Wayland <rlw@virginia.edu>
2003-07-24 13:28:34 +0000

    initial import Windows .bat script to run DemoObjectConverter

---
+++ src/bat/client/fedora-convert-demos.bat
@@ -0,0 +1,58 @@
+@echo off
+
+goto checkEnv
+:envOk
+
+echo Starting Fedora Demo Object Converter...
+
+set OLD_JAVA_HOME=%JAVA_HOME%
+set JAVA_HOME=%THIS_JAVA_HOME%
+
+
+:runMinimized
+%JAVA_HOME%\bin\java -cp %FEDORA_HOME%\client;%FEDORA_HOME%\client\client.jar fedora.client.demo.DemoObjectConverter %1 %2 %3 %4 %5
+
+set JAVA_HOME=%OLD_JAVA_HOME%
+
+goto end
+
+:checkEnv
+if "%FEDORA_HOME%" == "" goto noFedoraHome
+if not exist %FEDORA_HOME%\server\config\fedora.fcfg goto configNotFound
+if "%FEDORA_JAVA_HOME%" == "" goto tryJavaHome
+set THIS_JAVA_HOME=%FEDORA_JAVA_HOME%
+:checkJava
+if not exist %THIS_JAVA_HOME%\bin\java.exe goto noJavaBin
+if not exist %THIS_JAVA_HOME%\bin\orbd.exe goto badJavaVersion
+goto envOk
+
+:tryJavaHome
+if "%JAVA_HOME%" == "" goto noJavaHome
+set THIS_JAVA_HOME=%JAVA_HOME%
+goto checkJava
+
+:noFedoraHome
+echo ERROR: Environment variable, FEDORA_HOME must be set.
+goto end
+
+:configNotFound
+echo ERROR: FEDORA_HOME does not appear correctly set.
+echo Configuration cannot be found at %FEDORA_HOME%\server\config\fedora.fcfg
+goto end
+
+:noJavaHome
+echo ERROR: FEDORA_JAVA_HOME was not defined, nor was (the fallback) JAVA_HOME.
+goto end
+
+:noJavaBin
+echo ERROR: java.exe was not found in %THIS_JAVA_HOME%
+echo Make sure FEDORA_JAVA_HOME or JAVA_HOME is set correctly.
+goto end
+
+:badJavaVersion
+echo ERROR: java was found in %THIS_JAVA_HOME%, but it was not version 1.4
+echo Make sure FEDORA_JAVA_HOME or JAVA_HOME points to a 1.4JRE/JDK base.
+goto end
+
+:end
+
