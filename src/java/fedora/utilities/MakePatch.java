87270c8921123a51d11195a54286d101ab248fa5
Chris Wilper <cwilper@duraspace.org>
2003-09-02 21:33:05 +0000

    this will be a utility for making patches

---
+++ src/java/fedora/utilities/MakePatch.java
@@ -0,0 +1,11 @@
+public class MakePatch {
+
+    public static void showUsage(String message) {
+        System.err.println("MakePatch ");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
