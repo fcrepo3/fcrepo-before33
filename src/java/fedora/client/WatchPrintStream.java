83cc6b8b132060fda9c33b32a040a6a220436ada
Chris Wilper <cwilper@duraspace.org>
2003-09-11 07:31:25 +0000

    added comments

--- src/java/fedora/client/WatchPrintStream.java
+++ src/java/fedora/client/WatchPrintStream.java
@@ -2,25 +2,35 @@ package fedora.client;
 
 import java.io.*;
 
-// a wrapper around PrintWriter that sends all its content to WATCH_AREA
-    // via WATCH_AREA.append(String)
-    public class WatchPrintStream
-            extends PrintStream {
+/**
+ * A PrintStream that sends its output to Administrator.WATCH_AREA,
+ * the JTextArea of the Tools->Advanced->STDOUT/STDERR window.
+ * This is used for redirecting System.out/err output to the UI.
+ *
+ * @author cwilper@cs.cornell.edu
+ */
+public class WatchPrintStream
+        extends PrintStream {
 
-        private ByteArrayOutputStream m_out;
+    /** Output is buffered here until a call to println(String) */
+    private ByteArrayOutputStream m_out;
 
-        public WatchPrintStream(ByteArrayOutputStream out) {
-            super(out);
-            m_out=out;
-        }
-
-        public void println(String str) {
-            super.println(str);
-            if (Administrator.WATCH_AREA!=null) {
-                String buf=m_out.toString();
-                m_out.reset();
-                Administrator.WATCH_AREA.append(buf);
-            }
-        }
-
+    public WatchPrintStream(ByteArrayOutputStream out) {
+        super(out);
+        m_out=out;
     }
+
+    /** 
+     * Every time this is called, the buffer is cleared an output is
+     * sent to the JTextArea.
+     */
+    public void println(String str) {
+        super.println(str);
+        if (Administrator.WATCH_AREA!=null) {
+            String buf=m_out.toString();
+            m_out.reset();
+            Administrator.WATCH_AREA.append(buf);
+        }
+    }
+
+}
