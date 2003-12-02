eba15d429aaace07da93f99ab9d843ab2d0cacb7
Chris Wilper <cwilper@duraspace.org>
2003-12-02 14:21:25 +0000

    oops, now also ingesting open-server-demos dir

--- src/sh/client/fedora-ingest-demos.sh
+++ src/sh/client/fedora-ingest-demos.sh
@@ -45,6 +45,7 @@ export JAVA_HOME
 echo "Ingesting Demonstration Objects..."
 
 (exec $JAVA_HOME/bin/java -Xms64m -Xmx96m -cp $FEDORA_HOME/client:$FEDORA_HOME/client/client.jar -Dfedora.home=$FEDORA_HOME -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl -Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl fedora.client.ingest.Ingest dir $FEDORA_HOME/client/demo/local-server-demos DMO $1:$2 $3 $4)
+(exec $JAVA_HOME/bin/java -Xms64m -Xmx96m -cp $FEDORA_HOME/client:$FEDORA_HOME/client/client.jar -Dfedora.home=$FEDORA_HOME -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl -Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl fedora.client.ingest.Ingest dir $FEDORA_HOME/client/demo/open-server-demos DMO $1:$2 $3 $4)
 
 echo "Finished."
 
