61f1f2b4dc8f1357de0fc8d2755a566769b33f84
Chris Wilper <cwilper@duraspace.org>
2003-12-20 05:02:36 +0000

    added legal info to top of file

--- src/java/fedora/client/objecteditor/MechanismInputPanel.java
+++ src/java/fedora/client/objecteditor/MechanismInputPanel.java
@@ -12,6 +12,25 @@ import fedora.client.objecteditor.types.DatastreamInputSpec;
 import fedora.server.types.gen.Datastream;
 import fedora.server.types.gen.DatastreamBinding;
 
+/**
+ * Panel for providing mechanism input.
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002, 2003 by The
+ * Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
 public class MechanismInputPanel
         extends JPanel {
 
