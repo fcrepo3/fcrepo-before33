c706f1dbf6c4a16a382e2a7b9adc7ec5cc5cdaf2
Ross Wayland <rlw@virginia.edu>
2004-02-18 21:46:18 +0000

    initial import
    data structure for digital object used in modify batch utility

---
+++ src/java/fedora/client/batch/types/DigitalObject.java
@@ -0,0 +1,37 @@
+package fedora.client.batch.types;
+
+/**
+ * Launch a dialog for entering information for a new object
+ * (title, content model, and possibly a specified pid),
+ * then create the object on the server and launch an editor on it.
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002, 2003 by The
+ * Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @author rlw@virginia.edu
+ * @version $Id $
+ */
+public class DigitalObject {
+
+    public String pid;
+    public String label;
+    public String contentModel;
+
+    public DigitalObject()
+    {}
+
+}
