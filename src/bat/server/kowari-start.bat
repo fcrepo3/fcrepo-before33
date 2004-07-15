76b14f87171f224426753a4226603a738e5be95f
Edwin Shin <edwin.shin@yourmediashelf.com>
2004-07-15 19:54:12 +0000

    startup script for kowari

---
+++ src/bat/server/kowari-start.bat
@@ -0,0 +1,79 @@
+@echo off
+
+goto EndOfComment
+  kowari-start.bat
+    Starts the Kowari server. 
+    Kowari provides the RDF store for the Fedora ResourceIndex
+    
+    If Kowari will be running on a different host (i.e. different server) 
+    than the Fedora server, make sure to set the KOWARI_HOSTNAME below to a
+    hostname or ip address accessible by the Fedora server--i.e., do
+    not use localhost.
+    
+    Change the ports as needed, making sure to update any corresponding
+    entries in fedora.fcfg.
+    
+:EndOfComment
+
+set KOWARI_HOSTNAME=localhost
+set KOWARI_HTTP_PORT=8081
+set KOWARI_RMI_PORT=1099
+
+goto checkEnv
+
+:envOk
+set KOWARI_HOME=%FEDORA_HOME%\server\kowari
+SET KOWARI_JAR=kowari-1.0.4.jar
+if not exist %KOWARI_HOME%\%KOWARI_JAR% goto kowariNotFound
+
+echo Starting Kowari Server...
+
+set OLD_JAVA_HOME=%JAVA_HOME%
+set JAVA_HOME=%THIS_JAVA_HOME%
+
+%JAVA_HOME%\bin\java -jar %KOWARI_HOME%\%KOWARI_JAR% --serverhost %SERVERHOST% --path %KOWARI_HOME% --servername fedoraResourceIndex --port KOWARI_HTTP_PORT --rmiport KOWARI_RMI_PORT
+
+:doneRunning
+set JAVA_HOME=%OLD_JAVA_HOME%
+echo ...
+
+goto end
+
+:kowariNotFound
+echo ERROR: No %KOWARI_JAR% found in %KOWARI_HOME%
+goto end
+
+:checkEnv
+if "%FEDORA_HOME%" == "" goto noFedoraHome
+if not exist %FEDORA_HOME%\server\config\fedora.fcfg goto configNotFound
+if "%FEDORA_JAVA_HOME%" == "" goto tryJavaHome
+set THIS_JAVA_HOME=%FEDORA_JAVA_HOME%
+
+:tryJavaHome
+if "%JAVA_HOME%" == "" goto noJavaHome
+set THIS_JAVA_HOME=%JAVA_HOME%
+goto checkJava
+
+:checkJava
+if not exist %THIS_JAVA_HOME%\bin\java.exe goto noJavaBin
+goto envOk
+
+:noFedoraHome
+echo ERROR: Environment variable, FEDORA_HOME must be set.
+goto end
+
+:configNotFound
+echo ERROR: FEDORA_HOME does not appear correctly set.
+echo Configuration cannot be found at %FEDORA_HOME%\server\config\fedora.fcfg
+goto end
+
+:noJavaHome
+echo ERROR: FEDORA_JAVA_HOME was not defined, nor was (the fallback) JAVA_HOME.
+goto end
+
+:noJavaBin
+echo ERROR: java.exe was not found in %THIS_JAVA_HOME%
+echo Make sure FEDORA_JAVA_HOME or JAVA_HOME is set correctly.
+goto end
+
+:end
