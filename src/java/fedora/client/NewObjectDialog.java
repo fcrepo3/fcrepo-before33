46ee0386632095c87d26d41a2c4f1d15da4f395e
Sandy Payette <spayette@duraspace.org>
2004-09-10 18:51:58 +0000

    Changed foxml namespace URI.

--- src/java/fedora/client/NewObjectDialog.java
+++ src/java/fedora/client/NewObjectDialog.java
@@ -200,13 +200,13 @@ public class NewObjectDialog
 				StringBuffer xml=new StringBuffer();
 				xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 				xml.append("<foxml:digitalObject xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
-				xml.append("           xmlns:foxml=\"info:fedora/def:foxml1.0\"\n");
-				xml.append("           xsi:schemaLocation=\"info:fedora/def:foxml1.0 http://www.fedora.info/definitions/1/0/foxml1.0.xsd\"\n");
+				xml.append("           xmlns:foxml=\"info:fedora/fedora-system:def/foxml#\"\n");
+				xml.append("           xsi:schemaLocation=\"info:fedora/fedora-system:def/foxml# http://www.fedora.info/definitions/1/0/foxml1.0.xsd\"\n");
 				xml.append("           PID=\"" + StreamUtility.enc(pid) + "\">\n");
 				xml.append("  <foxml:objectProperties>\n");
-				xml.append("    <foxml:property NAME=\"info:fedora/def:dobj:type\">FedoraObject</foxml:property>\n");
-				xml.append("    <foxml:property NAME=\"info:fedora/def:dobj:label\">" + label + "</foxml:property>\n");
-				xml.append("    <foxml:property NAME=\"info:fedora/def:dobj:cmodel\">" + cModel + "</foxml:property>\n");
+				xml.append("    <foxml:property NAME=\"info:fedora/fedora-system:def/fType\">FedoraObject</foxml:property>\n");
+				xml.append("    <foxml:property NAME=\"info:fedora/fedora-system:def/label\">" + label + "</foxml:property>\n");
+				xml.append("    <foxml:property NAME=\"info:fedora/fedora-system:def/cModel\">" + cModel + "</foxml:property>\n");
 				xml.append("  </foxml:objectProperties>\n");
 				xml.append("</foxml:digitalObject>");
 				String objXML=xml.toString();
