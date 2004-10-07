b81b92483a9e2da2f7e610556fe8ffc0a82f87e7
Chris Wilper <cwilper@duraspace.org>
2004-10-07 16:16:31 +0000

    javadoc and copyright, minor cleanup

--- src/java/fedora/common/MalformedPIDException.java
+++ src/java/fedora/common/MalformedPIDException.java
@@ -1,8 +1,34 @@
 package fedora.common;
 
+/**
+ * Thrown when a PID is not well-formed.
+ * <p/>
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002-2004 by The
+ * Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @version $Id$
+ * @author cwilper@cs.cornell.edu
+ */
 public class MalformedPIDException
         extends Exception {
 
+    /**
+     * Construct a MalformedPIDException with the given reason.
+     */
     public MalformedPIDException(String why) {
         super(why);
     }
--- src/java/fedora/common/PID.java
+++ src/java/fedora/common/PID.java
@@ -5,6 +5,47 @@ import java.io.*;  // BufferedReader, InputStreamReader
 /**
  * A persistent identifier for Fedora digital objects.
  *
+ * <p>The following describes the syntactic constraints for PIDs in normalized
+ * form.  The only differences with non-normalized PIDs are that the 
+ * colon delimiter may be encoded as "%3a" or "%3A", and hex-digits may
+ * use lowercase [a-f].</p>
+ *
+ * <pre>
+ * PID:
+ *   Length : maximum 64
+ *   Syntax : namespace-id ":" object-id
+ *
+ * namespace-id:
+ *   Syntax : ( [A-Z] / [a-z] / [0-9] / "-" / "." ) 1+
+ *
+ * object-id:
+ *   Syntax : ( [A-Z] / [a-z] / [0-9] / "-" / "." / "~" / "_" / escaped-octet ) 1+
+ *
+ * escaped-octet:
+ *   Syntax : "%" hex-digit hex-digit
+ *
+ * hex-digit:
+ *   Syntax : [0-9] / [A-F]
+ * </pre>
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002-2004 by The
+ * Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @version $Id$
  * @author cwilper@cs.cornell.edu
  */
 public class PID {
@@ -12,6 +53,7 @@ public class PID {
     public static final int MAX_LENGTH = 64;
 
     private String m_normalized;
+    private String m_filename;
 
     /**
      * Construct a PID from a string, throwing a MalformedPIDException
@@ -22,9 +64,17 @@ public class PID {
         m_normalized = normalize(pidString);
     }
 
-    public PID fromFilenameString(String filenameString) 
+    /**
+     * Construct a PID given a filename of the form produced by toFilename(), 
+     * throwing a MalformedPIDException if it's not well-formed.
+     */
+    public static PID fromFilename(String filenameString) 
             throws MalformedPIDException {
-        return new PID(decodeFilename(filenameString));
+        String decoded = filenameString.replaceFirst("_", ":");
+        while (decoded.endsWith("%")) {
+            decoded = decoded.substring(0, decoded.length() - 1) + ".";
+        }
+        return new PID(decoded);
     }
 
     /**
@@ -33,7 +83,7 @@ public class PID {
      */
     public static String normalize(String pidString) 
             throws MalformedPIDException {
-        // Null & length checks
+        // First, do null & length checks
         if (pidString == null) {
             throw new MalformedPIDException("PID is null.");
         }
@@ -42,31 +92,50 @@ public class PID {
                     + MAX_LENGTH + ".");
         }
 
+        // Then normalize while checking syntax
         StringBuffer out = new StringBuffer();
         for (int i = 0; i < pidString.length(); i++) {
             char c = pidString.charAt(i);
            
         }
+
+        // If we got here, it's well-formed, so return it.
         return out.toString();
     }
 
     /**
-     * Returns the normalized form of this PID.
+     * Return the normalized form of this PID.
      */
     public String toString() {
         return m_normalized;
     }
 
     /**
-     * Returns a string representing this PID that can be safely used
-     * as a filename on any OS.
+     * Return the URI form of this PID.
+     * 
+     * This is just the PID, prepended with "info:fedora/".
      */
-    public String toFilenameString() {
-        return m_normalized;  // TODO
+    public String toURI() {
+        return "info:fedora/" + m_normalized;
     }
 
-    private static String decodeFilename(String filenameString) {
-        return filenameString; // TODO
+    /**
+     * Return a string representing this PID that can be safely used
+     * as a filename on any OS.
+     *
+     * <ul>
+     *   <li> The colon (:) is replaced with an underscore (_).</li>
+     *   <li> Trailing dots are encoded as percents (%).</li>
+     * </ul>
+     */
+    public String toFilename() {
+        if (m_filename == null) { // lazily convert, since not always needed
+            m_filename = m_normalized.replaceAll(":", "_");
+            while (m_filename.endsWith(".")) {
+                m_filename = m_filename.substring(0, m_filename.length() - 1) + "%";
+            }
+        }
+        return m_filename;
     }
 
     /**
@@ -77,7 +146,10 @@ public class PID {
      */
     public static void main(String[] args) throws Exception {
         if (args.length > 0) {
-            System.out.println(new PID(args[0]).toString());
+            PID p = new PID(args[0]);
+            System.out.println("Normalized    : " + p.toString());
+            System.out.println("To filename   : " + p.toFilename());
+            System.out.println("From filename : " + PID.fromFilename(p.toFilename()).toString());
         } else {
             System.out.println("--------------------------------------");
             System.out.println("PID Syntax Checker - Interactive mode");
@@ -91,10 +163,13 @@ public class PID {
                     if (line.equals("")) {
                         done = true;
                     } else {
-                        System.out.println("GOOD: " + new PID(line).toString());
+                        PID p = new PID(line);
+                        System.out.println("Normalized    : " + p.toString());
+                        System.out.println("To filename   : " + p.toFilename());
+                        System.out.println("From filename : " + PID.fromFilename(p.toFilename()).toString());
                     }
                 } catch (MalformedPIDException e) {
-                    System.out.println("BAD: " + e.getMessage());
+                    System.out.println("ERROR: " + e.getMessage());
                 }
             }
         }
