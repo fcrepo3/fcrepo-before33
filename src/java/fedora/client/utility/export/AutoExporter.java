a5b314d6f812a3f49d2fcd9460467daa20b9ed1a
Sandy Payette <spayette@duraspace.org>
2004-11-09 21:29:23 +0000

    separation of general-purpose utility functionality for export into utility package.  Changes to separate out functionality from Administrator.  Changes for new exportContext parm.

---
+++ src/java/fedora/client/utility/export/AutoExporter.java
@@ -0,0 +1,168 @@
+package fedora.client.utility.export;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+
+import javax.xml.rpc.ServiceException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+
+import fedora.client.APIMStubFactory;
+import fedora.client.APIAStubFactory;
+import fedora.server.management.FedoraAPIM;
+import fedora.server.access.FedoraAPIA;
+import fedora.server.types.gen.RepositoryInfo;
+
+/**
+ *
+ * <p><b>Title:</b> AutoExporter.java</p>
+ * <p><b>Description: Utility class to make API-M SOAP calls to a repository.</b> </p>
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002-2004 by The
+ * Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @author cwilper@cs.cornell.edu
+ * @version $Id$
+ */
+public class AutoExporter {
+
+    private FedoraAPIM m_apim;
+	private FedoraAPIA m_apia;
+    private static HashMap s_repoInfo=new HashMap();
+
+    public AutoExporter(String host, int port, String user, String pass)
+            throws MalformedURLException, ServiceException {
+		m_apia=APIAStubFactory.getStub(host, port, user, pass);
+        m_apim=APIMStubFactory.getStub(host, port, user, pass);
+    }
+    
+	public void export(String pid, String format, String exportContext, 
+			OutputStream outStream) 
+			throws RemoteException, IOException {
+		export(m_apia, m_apim, pid, format, exportContext, outStream);
+	}
+
+	
+    public static void export(FedoraAPIA apia, FedoraAPIM apim, String pid, String format,
+            String exportContext, OutputStream outStream)
+            throws RemoteException, IOException {
+
+        // Get the repository info from the hash for the repository that APIA is using,
+        // unless it isn't in the hash yet... in which case, ask the server.
+		RepositoryInfo repoinfo=(RepositoryInfo) s_repoInfo.get(apia);
+        if (repoinfo == null) {
+            repoinfo=apia.describeRepository();
+            s_repoInfo.put(apia, repoinfo);
+        }
+				
+        byte[] bytes;
+    	// For backward compatibility:
+    	// For pre-2.0 repositories, use the "exportObject" APIM method. 
+    	// Also pre-2.0 repositories will only export "metslikefedora1".
+    	// For 2.0+ repositories use the "export" method which takes a format arg.
+		StringTokenizer stoken = new StringTokenizer(repoinfo.getRepositoryVersion(), ".");
+		if (new Integer(stoken.nextToken()).intValue() < 2){
+			if (format==null ||
+				format.equals("metslikefedora1") ||
+				format.equals("default")) {
+				bytes=apim.exportObject(pid);					
+			} else {
+				throw new IOException("You are connected to a pre-2.0 Fedora repository " +
+					"which will only export the XML format \"metslikefedora1\".");
+			}
+		} else {
+			validateFormat(format);				
+			bytes=apim.export(pid, format, exportContext);
+		}
+        try {
+            // use xerces to pretty print the xml, assuming it's well formed
+            OutputFormat fmt=new OutputFormat("XML", "UTF-8", true);
+            fmt.setIndent(2);
+            fmt.setLineWidth(120);
+            fmt.setPreserveSpace(false);
+            XMLSerializer ser=new XMLSerializer(outStream, fmt);
+            DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder=factory.newDocumentBuilder();
+            Document doc=builder.parse(new ByteArrayInputStream(bytes));
+            ser.serialize(doc);
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getClass().getName() + " : " + e.getMessage());
+        } finally
+        {
+          outStream.close();
+        }
+    }
+
+	public void getObjectXML(String pid, OutputStream outStream) 
+			throws RemoteException, IOException {
+		getObjectXML(m_apia, m_apim, pid, outStream);
+	}
+	
+	public static void getObjectXML(FedoraAPIA apia, FedoraAPIM apim, String pid, OutputStream outStream)
+			throws RemoteException, IOException {
+
+		// Get the repository info from the hash for the repository that APIA is using,
+		// unless it isn't in the hash yet... in which case, ask the server.
+		RepositoryInfo repoinfo=(RepositoryInfo) s_repoInfo.get(apia);
+		if (repoinfo == null) {
+			repoinfo=apia.describeRepository();
+			s_repoInfo.put(apia, repoinfo);
+		}
+		// get the object XML as it exists in the repository's 
+		// persitent storage area.		
+		byte[] bytes=apim.getObjectXML(pid);
+		try {
+			// use xerces to pretty print the xml, assuming it's well formed
+			OutputFormat fmt=new OutputFormat("XML", "UTF-8", true);
+			fmt.setIndent(2);
+			fmt.setLineWidth(120);
+			fmt.setPreserveSpace(false);
+			XMLSerializer ser=new XMLSerializer(outStream, fmt);
+			DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true);
+			DocumentBuilder builder=factory.newDocumentBuilder();
+			Document doc=builder.parse(new ByteArrayInputStream(bytes));
+			ser.serialize(doc);
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e.getClass().getName() + " : " + e.getMessage());
+		} finally
+		{
+		  outStream.close();
+		}
+	}
+
+	public static void validateFormat(String format)
+		throws IOException {
+			if (!format.equals("foxml1.0") && 
+				!format.equals("metslikefedora1") && 
+				!format.equals("default")) {
+				throw new IOException("Invalid export format. Valid FORMAT values are: " +
+					"'foxml1.0' 'metslikefedora1' and 'default'");
+			}
+		}
+}
---
+++ src/java/fedora/client/utility/export/Export.java
@@ -0,0 +1,291 @@
+package fedora.client.utility.export;
+
+import java.io.*;
+import java.util.*;
+
+import fedora.client.APIAStubFactory;
+import fedora.client.APIMStubFactory;
+import fedora.client.utility.export.AutoExporter;
+import fedora.client.utility.AutoFinder;
+
+import fedora.server.access.FedoraAPIA;
+import fedora.server.management.FedoraAPIM;
+
+import fedora.server.types.gen.Condition;
+import fedora.server.types.gen.ComparisonOperator;
+import fedora.server.types.gen.FieldSearchQuery;
+import fedora.server.types.gen.FieldSearchResult;
+import fedora.server.types.gen.ObjectFields;
+import fedora.server.types.gen.RepositoryInfo;
+
+/**
+ * <p><b>Title:</b> Export.java</p>
+ * <p><b>Description: A utility class to initiate an export of one or more objects.
+ * This class provides static utility methods, and it is also called by
+ * command line utilities.
+ * 
+ * This class calls AutoExporter.class which is reponsible for making
+ * the API-M SOAP calls for the export.
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002-2004 by The
+ * Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+public class Export {
+    
+    public static String getDuration(long millis) {
+        long tsec=millis/1000;
+        long h=tsec/60/60;
+        long m=(tsec - (h*60*60))/60;
+        long s=(tsec - (h*60*60) - (m*60));
+        StringBuffer out=new StringBuffer();
+        if (h>0) {
+            out.append(h + " hour");
+            if (h>1) out.append('s');
+        }
+        if (m>0) {
+            if (h>0) out.append(", ");
+            out.append(m + " minute");
+            if (m>1) out.append('s');
+        }
+        if (s>0 || (h==0 && m==0)) {
+            if (h>0 || m>0) out.append(", ");
+            out.append(s + " second");
+            if (s!=1) out.append('s');
+        }
+        return out.toString();
+    }
+
+    public static void one(FedoraAPIA apia, FedoraAPIM apim, 
+    					   String pid, String format, String exportContext, File dir)
+            throws Exception {
+        String fName=pid.replaceAll(":", "_") + ".xml";
+        File file=new File(dir, fName);
+        System.out.println("Exporting " + pid + " to " + file.getPath());
+        AutoExporter.export(apia, apim, pid, format, exportContext, 
+        	new FileOutputStream(file));
+    }
+    
+    public static String[] multi(FedoraAPIA apia, FedoraAPIM apim, 
+    							 String fTypes, String format, String exportContext, File dir)
+            throws Exception {
+        String tps=fTypes.toUpperCase();
+        Set toExport=new HashSet();
+        Set pidSet=new HashSet();
+        if (tps.indexOf("D")!=-1) {
+            toExport=getPIDs(apia, "D");
+            System.out.println("Found " + toExport.size() + " behavior definitions.");
+            pidSet.addAll(toExport);
+        }
+        if (tps.indexOf("M")!=-1) {
+            toExport=getPIDs(apia, "M");
+            System.out.println("Found " + toExport.size() + " behavior mechanisms.");
+            pidSet.addAll(toExport);
+        }
+        if (tps.indexOf("O")!=-1) {
+            toExport=getPIDs(apia, "O");
+            System.out.println("Found " + toExport.size() + " data objects.");
+            pidSet.addAll(toExport);
+        }
+        Iterator iter=pidSet.iterator();
+        String[] pids=new String[pidSet.size()];
+        int i=0;
+        while (iter.hasNext()) {
+            String pid=(String) iter.next();
+            one(apia, apim, pid, format, exportContext, dir);
+            pids[i++]=pid;
+        }
+        return pids;
+    }
+
+    public static Set getPIDs(FedoraAPIA apia,
+                              String fType)
+            throws Exception {
+        // get pids with fType='$fType', adding all to set at once,
+        // then returning the entire set.
+        HashSet set=new HashSet();
+        Condition cond=new Condition();
+        cond.setProperty("fType");
+        cond.setOperator(ComparisonOperator.fromValue("eq"));
+        cond.setValue(fType);
+        Condition[] conds=new Condition[1];
+        conds[0]=cond;
+        FieldSearchQuery query=new FieldSearchQuery();
+        query.setConditions(conds);
+        query.setTerms(null);
+        String[] fields=new String[1];
+        fields[0]="pid";
+        FieldSearchResult res=AutoFinder.findObjects(apia,
+                                                     fields,
+                                                     1000,
+                                                     query);
+        boolean exhausted=false;
+        while (res!=null && !exhausted) {
+            ObjectFields[] ofs=res.getResultList();
+            for (int i=0; i<ofs.length; i++) {
+                set.add(ofs[i].getPid());
+            }
+            if (res.getListSession()!=null && res.getListSession().getToken()!=null) {
+                res=AutoFinder.resumeFindObjects(apia,
+                                                 res.getListSession().getToken());
+            } else {
+                exhausted=true;
+            }
+        }
+        return set;
+    }
+
+    /**
+     * Print error message and show usage for command-line interface.
+     */
+    public static void badArgs(String msg) {
+        System.err.println("Command: fedora-export");
+        System.err.println();
+        System.err.println("Summary: Exports one or more objects from a Fedora repository.");
+        System.err.println();
+        System.err.println("Syntax:");
+        System.err.println("  fedora-export HST:PRT USR PSS PID|FTYPS FORMAT ECONTEXT PATH");
+        System.err.println();
+        System.err.println("Where:");
+        System.err.println("  HST    is the repository hostname.");
+        System.err.println("  PRT    is the repository port number.");
+        System.err.println("  USR    is the id of the repository user.");
+        System.err.println("  PSS    is the password of repository user.");
+        System.err.println("  PID    is the id of the object to export from the source repository.");
+        System.err.println("  FTYPS  is any combination of the characters O, D, and M, specifying");
+        System.err.println("         which Fedora object type(s) should be exported. O=data objects,");
+        System.err.println("         D=behavior definitions, and M=behavior mechanisms.");
+		System.err.println("  FORMAT is the XML format to export ");
+		System.err.println("         ('foxml1.0', 'metslikefedora1', or 'default')");
+		System.err.println("  ECONTEXT is the export context (which indicates what use case");
+		System.err.println("         the output should be prepared for.");
+		System.err.println("         ('public', 'migrate', 'archive' or 'default')");
+		System.err.println("  PATH   is the directory to export the object.");
+        System.err.println();
+        System.err.println("Examples:");
+        System.err.println("fedora-export myrepo.com:80 user pw demo:1 foxml1.0 migrate .");
+        System.err.println();
+        System.err.println("  Exports demo:1 for migration in FOXML format ");
+        System.err.println("  (from myrepo.com:80 to the current directory).");
+        System.err.println();
+        System.err.println("fedora-export myrepo.com:80 user pw DMO default default /tmp/fedoradump");
+        System.err.println();
+        System.err.println("  Exports all objects in the default export format and context ");
+        System.err.println("  (from myrepo.com:80 to directory /tmp/fedoradump).");
+        System.err.println();
+		System.err.println("ERROR  : " + msg);
+        System.exit(1);
+    }
+
+    /**
+     * Command-line interface for doing exports.
+     */
+    public static void main(String[] args) {
+        try {
+			// USAGE: fedora-export HST:PRT USR PSS PID|FTYPS FORMAT ECONTEXT PATH
+			if (args.length!=7) {
+                Export.badArgs("Wrong number of arguments.");
+            }
+            String[] hp=args[0].split(":");
+            if (hp.length!=2) {
+                Export.badArgs("First arg must be of the form 'host:portnum'");
+            }
+            
+			FedoraAPIA sourceRepoAPIA=
+					APIAStubFactory.getStub(hp[0],
+											Integer.parseInt(hp[1]),
+											args[1],
+											args[2]);
+			FedoraAPIM sourceRepoAPIM=
+					APIMStubFactory.getStub(hp[0],
+											Integer.parseInt(hp[1]),
+											args[1],
+											args[2]);
+				
+			String exportFormat = args[4];
+			String exportContext = args[5];
+			if ((!exportFormat.equals("metslikefedora1")) &&
+			    (!exportFormat.equals("foxml1.0")) &&
+				(!exportFormat.equals("default"))) {
+					Export.badArgs("FORMAT arg must be 'metslikefedora1', 'foxml1.0', or 'default'");			   
+			}
+			if ((!exportContext.equals("public"))  &&
+				(!exportContext.equals("migrate")) &&
+				(!exportContext.equals("archive")) &&
+				(!exportContext.equals("default"))) {
+					Export.badArgs("ECONTEXT arg must be 'public', 'migrate', 'archive', or 'default'");			   
+			}
+            
+			RepositoryInfo repoinfo = sourceRepoAPIA.describeRepository();
+			StringTokenizer stoken = new StringTokenizer(repoinfo.getRepositoryVersion(), ".");
+			if (new Integer(stoken.nextToken()).intValue() < 2 // pre-2.0 repo
+				&& ((!exportFormat.equals("metslikefedora1") && 
+					 !exportFormat.equals("default"))))
+					Export.badArgs("FORMAT arg must be 'metslikefedora1' or 'default' for pre-2.0 repository.");				
+			
+			if (exportFormat.equals("default")){
+				exportFormat=null;		
+			}
+			if (exportContext.equals("default")){
+				exportContext=null;		
+			}
+            if (args[3].indexOf(":")==-1) {
+                // assume args[3] is FTYPS... so multi-export
+                String[] pids=Export.multi(sourceRepoAPIA,
+										   sourceRepoAPIM,
+					                       args[3], // FTYPS
+					                       exportFormat,
+					                       exportContext,
+					                       //args[4], // format
+										   //args[5], // export context
+					                       new File(args[6])); // path
+                System.out.print("Exported ");
+                for (int i=0; i<pids.length; i++) {
+                    if (i>0) System.out.print(", ");
+                    System.out.print(pids[i]);
+                }
+                System.out.println();
+            } else {
+                // assume args[3] is a PID...they only want to export one object
+                Export.one(APIAStubFactory.getStub(
+                				hp[0],
+								Integer.parseInt(hp[1]),
+								args[1],
+								args[2]),
+							APIMStubFactory.getStub(
+								hp[0],
+                            	Integer.parseInt(hp[1]),
+                                args[1],
+                                args[2]),
+                           	args[3], // PID
+                           	exportFormat,
+                           	exportContext,
+							//args[4], // format
+							//args[5], // export context
+							new File(args[6])); // path
+                System.out.println("Exported " + args[3]);
+            }
+        } catch (Exception e) {
+            System.err.print("Error  : ");
+            if (e.getMessage()==null) {
+                e.printStackTrace();
+            } else {
+                System.err.print(e.getMessage());
+            }
+        }
+    }
+
+}
