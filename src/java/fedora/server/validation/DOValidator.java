12a95ae304083cb2a37dd835d3f19d7d88b0c252
Sandy Payette <spayette@duraspace.org>
2004-11-28 18:33:47 +0000

    Cleanup - fixed method documentation to reflect removal of level 3 validation.

--- src/java/fedora/server/validation/DOValidator.java
+++ src/java/fedora/server/validation/DOValidator.java
@@ -1,9 +1,6 @@
 package fedora.server.validation;
 
-import fedora.server.errors.ObjectValidityException;
-import fedora.server.errors.GeneralException;
 import fedora.server.errors.ServerException;
-
 import java.io.InputStream;
 import java.io.File;
 
@@ -39,18 +36,16 @@ public interface DOValidator {
    *
    * @param in The digital object provided as a bytestream.
    * @param validationLevel The level of validation to perform on the digital object.
-   *        This is an integer from 0-3 with the following meanings:
-   *        0 = do all validation levels
-   *        1 = perform only XML Schema validation
-   *        2 = perform only Schematron Rules validation
-   *        3 = perform only referential integrity checks for the object
+   *        This is an integer from 0-2 with the following meanings:
+   *        0 = VALIDATE_ALL (do all validation levels)
+   *        1 = VALIDATE_XML_SCHEMA (perform only XML Schema validation)
+   *        2 = VALIDATE_SCHEMATRON (perform only Schematron Rules validation)
    * @param phase The stage in the work flow for which the validation should be contextualized.
    *        "ingest" = the object is in the submission format for the ingest stage phase
    *        "store" = the object is in the authoritative format for the final storage phase
    * @throws ServerException If validation fails for any reason.
    */
     public void validate(InputStream in, String format, int validationLevel, String phase)
-        //throws ObjectValidityException, GeneralException;
         throws ServerException;
 
   /**
@@ -58,18 +53,16 @@ public interface DOValidator {
    *
    * @param in The digital object provided as a file.
    * @param validationLevel The level of validation to perform on the digital object.
-   *        This is an integer from 0-3 with the following meanings:
-   *        0 = do all validation levels
-   *        1 = perform only XML Schema validation
-   *        2 = perform only Schematron Rules validation
-   *        3 = perform only referential integrity checks for the object
+   *        This is an integer from 0-2 with the following meanings:
+   *        0 = VALIDATE_ALL (do all validation levels)
+   *        1 = VALIDATE_XML_SCHEMA (perform only XML Schema validation)
+   *        2 = VALIDATE_SCHEMATRON (perform only Schematron Rules validation)
    * @param phase The stage in the work flow for which the validation should be contextualized.
    *        "ingest" = the object is in the submission format for the ingest stage phase
    *        "store" = the object is in the authoritative format for the final storage phase
    * @throws ServerException If validation fails for any reason.
    */
     public void validate(File in, String format, int validationLevel, String phase)
-        //throws ObjectValidityException, GeneralException;
         throws ServerException;
 
 }
