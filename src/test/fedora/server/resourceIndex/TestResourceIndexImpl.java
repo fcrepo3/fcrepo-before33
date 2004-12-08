b6f8eb17805c2fd8a942de9dadb830e2f537e2be
Edwin Shin <edwin.shin@yourmediashelf.com>
2004-12-08 08:54:32 +0000

    sanity tests

--- src/test/fedora/server/resourceIndex/TestResourceIndexImpl.java
+++ src/test/fedora/server/resourceIndex/TestResourceIndexImpl.java
@@ -3,7 +3,6 @@ package fedora.server.resourceIndex;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -19,14 +18,14 @@ import junit.framework.TestCase;
 
 /**
  * @author eddie
- *
+ *  
  */
 public class TestResourceIndexImpl extends TestCase {
     private static final String LOCAL_SERVER_PATH = "/tmp/fedoraTest";
     private static final String MODEL_NAME = "testResourceIndex";
     private static final String SERVER_NAME = "testFedora";
     private static final String DEMO_OBJECTS_ROOT_DIR = "src/test/fedora/server/resourceIndex/foxmlTestObjects";
-    
+
     private static final String CP_DRIVER = "com.mysql.jdbc.Driver";
     private static final String CP_URL = "jdbc:mysql://localhost/fedora20?useUnicode=true&amp;characterEncoding=UTF-8&amp;autoReconnect=true";
     private static final String CP_USERNAME = "fedoraAdmin";
@@ -34,17 +33,14 @@ public class TestResourceIndexImpl extends TestCase {
     private static final int CP_ICONN = 1;
     private static final int CP_MAXCONN = 1;
     private static final boolean CP_WAIT = true;
-    
+
     private static final String TRIPPI_CONNECTOR_CLASSNAME = "org.trippi.impl.kowari.KowariConnector";
-    
-    private ResourceIndex ri;
-    private DigitalObject digitalObject, m_bDef, m_bMech;
-    
-    private ConnectionPool cPool;
-    
+
+    private ResourceIndex m_ri;
+    private ConnectionPool m_cPool;
     private TriplestoreConnector m_conn;
     private TriplestoreWriter m_writer;
-    
+
     public static void main(String[] args) {
         junit.textui.TestRunner.run(TestResourceIndexImpl.class);
     }
@@ -53,8 +49,9 @@ public class TestResourceIndexImpl extends TestCase {
      * @see TestCase#setUp()
      */
     protected void setUp() throws Exception {
-		// the database connection pool
-        cPool = new ConnectionPool(CP_DRIVER, CP_URL, CP_USERNAME, CP_PASSWORD, CP_ICONN, CP_MAXCONN, CP_WAIT);
+        // the database connection pool
+        m_cPool = new ConnectionPool(CP_DRIVER, CP_URL, CP_USERNAME, CP_PASSWORD,
+                CP_ICONN, CP_MAXCONN, CP_WAIT);
 
         // trippi
         Map config = new HashMap();
@@ -72,88 +69,119 @@ public class TestResourceIndexImpl extends TestCase {
         config.put("autoFlushBufferSize", "20000");
         config.put("remote", "false");
         config.put("memoryBuffer", "true");
-        
+
         m_conn = TriplestoreConnector.init(TRIPPI_CONNECTOR_CLASSNAME, config);
-        ri = new ResourceIndexImpl(3, m_conn, cPool, null);
-        
+        m_writer = m_conn.getWriter();
+        m_ri = new ResourceIndexImpl(3, m_conn, m_cPool, null);
+
         // needed by the deserializer
         System.setProperty("fedoraServerHost", "localhost");
-		System.setProperty("fedoraServerPort", "8080");
-        
-        
+        System.setProperty("fedoraServerPort", "8080");
     }
 
     /*
      * @see TestCase#tearDown()
      */
     protected void tearDown() throws Exception {
-        try {
-            if (m_writer != null) {
-                m_writer.close();
-            }
-            if (m_conn != null) {
-                m_conn.close();
-            }
-        } finally {
-            ri = null;
-            digitalObject = null;
+        if (m_writer != null) {
+            m_writer.close();
+            m_writer = null;
         }
-        
+        if (m_conn != null) {
+            m_conn.close();
+            m_conn = null;
+        }
+        if (m_cPool != null) {
+            m_cPool.closeAllConnections();
+            m_cPool = null;
+        }
+        m_ri = null;
+        deleteDirectory(LOCAL_SERVER_PATH);
     }
 
-//    public void testExecuteQuery() {
+    //    public void testExecuteQuery() {
+    //    }
+
+    //    public void testAddDigitalObjects() throws Exception {
+    //        addBDefs();
+    //        addBMechs();
+    //        addDataObjects();
+    //    	
+    //    	m_writer.write(new FileOutputStream("/tmp/out.rdf"));
+    //    }
+
+//    public void testAddDigitalObject() throws Exception {
+//        DigitalObject obj = getDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR
+//                + "/dataobjects/demo_10.xml"));
+//        m_ri.addDigitalObject(obj);
 //    }
 
-//    public void testAddDigitalObjects() throws Exception {
-//        addBDefs();
-//        addBMechs();
-//        addDataObjects();
-//    	
-//    	m_writer.write(new FileOutputStream("/tmp/out.rdf"));
-//    }
-    
-    public void testAddDigitalObject() throws Exception {
-        //addBDefs();
-        //addBMechs();
-        //addDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR + "/dataobjects/demo_5.xml"));
+    public void testAddAndDelete() throws Exception {
+        DigitalObject bdef = getDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR
+                + "/bdefs/demo_8.xml"));
+        DigitalObject bmech = getDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR
+                + "/bmechs/demo_9.xml"));
+        DigitalObject dataobject = getDigitalObject(new File(
+                DEMO_OBJECTS_ROOT_DIR + "/dataobjects/demo_10.xml"));
+
+        m_ri.addDigitalObject(bdef);
+        m_writer.flushBuffer();
+        int a = m_ri.countTriples(null, null, null, 0);
+        assertTrue(m_ri.countTriples(null, null, null, 0) > 0);
         
-        //addDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR + "/bdefs/demo_1.xml"));
-        //addDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR + "/bmechs/demo_2.xml"));
-        //addDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR + "/dataobjects/demo_5.xml"));
-        addDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR + "/bdefs/demo_8.xml"));
-        addDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR + "/bmechs/demo_9.xml"));
-        addDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR + "/dataobjects/demo_10.xml"));
-        try {
-    	m_writer.dump(new FileOutputStream("/tmp/out.rdf"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        m_ri.addDigitalObject(bmech);
+        m_writer.flushBuffer();
+        int b = m_ri.countTriples(null, null, null, 0);
+        assertTrue(b > a);
+        
+        m_ri.addDigitalObject(dataobject);
+        m_writer.flushBuffer();
+        int c = m_ri.countTriples(null, null, null, 0);
+        assertTrue(c > b);
+        
+        m_ri.deleteDigitalObject(dataobject);
+        m_writer.flushBuffer();
+        int d = m_ri.countTriples(null, null, null, 0);
+        assertTrue(d == b);
+        
+        m_ri.deleteDigitalObject(bmech);
+        m_writer.flushBuffer();
+        int e = m_ri.countTriples(null, null, null, 0);
+        assertTrue(e == a);
+        
+        m_ri.deleteDigitalObject(bdef);
+        m_writer.flushBuffer();
+        int f = m_ri.countTriples(null, null, null, 0);
+        assertEquals(m_ri.countTriples(null, null, null, 0), 0);
     }
 
-//    public void testAddDatastream() {
-//    }
-//
-//    public void testAddDissemination() {
-//    }
-//
-//    public void testModifyDigitalObject() {
-//    }
-//
-//    public void testModifyDatastream() {
-//    }
-//
-//    public void testModifyDissemination() {
-//    }
-//
-//    public void testDeleteDigitalObject() {
-//    }
-//
-//    public void testDeleteDatastream() {
-//    }
-//
-//    public void testDeleteDissemination() {
-//    }
-    
+    //    public void testAddDatastream() {
+    //    }
+    //
+    //    public void testAddDissemination() {
+    //    }
+    //
+    //    public void testModifyDigitalObject() {
+    //    }
+    //
+    //    public void testModifyDatastream() {
+    //    }
+    //
+    //    public void testModifyDissemination() {
+    //    }
+    //
+    //    public void testDeleteDigitalObject() throws Exception {
+    //        DigitalObject obj = getDigitalObject(new File(DEMO_OBJECTS_ROOT_DIR +
+    // "/dataobjects/demo_10.xml"));
+    //        m_ri.deleteDigitalObject(obj);
+    //    }
+    //
+    //    public void testDeleteDatastream() {
+    //    }
+    //
+    //    public void testDeleteDissemination() {
+    //    }
+
     private void addDigitalObjects(File dir) throws Exception {
         FilenameFilter filter = new FilenameFilter() {
             public boolean accept(File file, String name) {
@@ -165,30 +193,70 @@ public class TestResourceIndexImpl extends TestCase {
             addDigitalObject(files[i]);
         }
     }
-    
+
     private void addDigitalObject(File file) throws Exception {
         FileInputStream in;
         DigitalObject obj = new BasicDigitalObject();
-        
+
         in = new FileInputStream(file);
-        FOXMLDODeserializer deser=new FOXMLDODeserializer();
+        FOXMLDODeserializer deser = new FOXMLDODeserializer();
         deser.deserialize(in, obj, "UTF-8", 0);
-        ri.addDigitalObject(obj);
+        addDigitalObject(obj);
     }
-    
+
+    private void addDigitalObject(DigitalObject digitalObject) throws Exception {
+        m_ri.addDigitalObject(digitalObject);
+    }
+
     private void addBDefs() throws Exception {
         File dir = new File(DEMO_OBJECTS_ROOT_DIR + "/bdefs");
         addDigitalObjects(dir);
     }
-    
+
     private void addBMechs() throws Exception {
         File dir = new File(DEMO_OBJECTS_ROOT_DIR + "/bmechs");
         addDigitalObjects(dir);
     }
-    
+
     private void addDataObjects() throws Exception {
         File dir = new File(DEMO_OBJECTS_ROOT_DIR + "/dataobjects");
         addDigitalObjects(dir);
     }
+
+    private DigitalObject getDigitalObject(File file) throws Exception {
+        FileInputStream in;
+        DigitalObject obj = new BasicDigitalObject();
+
+        in = new FileInputStream(file);
+        FOXMLDODeserializer deser = new FOXMLDODeserializer();
+        deser.deserialize(in, obj, "UTF-8", 0);
+        return obj;
+    }
+
+    private boolean deleteDirectory(String directory) {
+        boolean result = false;
+
+        if (directory != null) {
+            File file = new File(directory);
+            if (file.exists() && file.isDirectory()) {
+                // 1. delete content of directory:
+                File[] files = file.listFiles();
+                result = true; //init result flag
+                int count = files.length;
+                for (int i = 0; i < count; i++) { //for each file:
+                    File f = files[i];
+                    if (f.isFile()) {
+                        result = result && f.delete();
+                    } else if (f.isDirectory()) {
+                        result = result && deleteDirectory(f.getAbsolutePath());
+                    }
+                }//next file
+
+                file.delete(); //finally delete (empty) input directory
+            }//else: input directory does not exist or is not a directory
+        }//else: no input value
+
+        return result;
+    }//deleteDirectory()
 
 }
