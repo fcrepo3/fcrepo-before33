fe4c552a67ad7bed555a17c72a63a659a956d8a7
Bill Niebel <wdn5e@virginia.edu>
2005-02-10 16:06:36 +0000

    merging authn/authz with 2.0 source

--- src/java/fedora/server/access/ListDatastreamsServlet.java
+++ src/java/fedora/server/access/ListDatastreamsServlet.java
@@ -30,6 +30,7 @@ import fedora.server.errors.InitializationException;
 import fedora.server.errors.GeneralException;
 import fedora.server.errors.ServerException;
 import fedora.server.errors.StreamIOException;
+import fedora.server.security.Authorization;
 import fedora.server.storage.types.DatastreamDef;
 import fedora.server.utilities.DateUtility;
 import fedora.server.utilities.Logger;
@@ -114,41 +115,6 @@ public class ListDatastreamsServlet extends HttpServlet
   private static Logger logger = null;
 
   /**
-   * Get the userId if the provided base64-encoded user:pass string
-   * provides a correct user-to-password match.  Otherwise, return null.
-   *
-   * @param basicAuthString base64-encoder user:pass string.
-   * @return the user id.
-   */
-  private String getAuthenticatedUser(String basicAuthString) {
-      String authUser=null;
-      if (basicAuthString!=null) {
-          String userAndPass=new String(StreamUtility.decodeBase64(basicAuthString.substring(6).trim()));
-          int i=userAndPass.indexOf(":");
-          if (i>0) {
-              String user=userAndPass.substring(0, i);
-              String pass=userAndPass.substring(i+1);
-              if (isUserPassword(user, pass)) {
-                  authUser=user;
-              }
-          }
-      }
-      return authUser;
-  }
-
-  /**
-   * Tell whether the password is the user's password.
-   *
-   * @param user the userId.
-   * @param pass the possible password.
-   * @return true if the password is correct, false otherwise.
-   */
-  private boolean isUserPassword(String user, String pass) {
-      // currently only recognizes fedoraAdmin.
-      return (user.equals("fedoraAdmin") && pass.equals(s_server.getParameter("adminPassword")));
-  }
-
-  /**
    * <p>Process Fedora Access Request. Parse and validate the servlet input
    * parameters and then execute the specified request.</p>
    *
@@ -168,29 +134,7 @@ public class ListDatastreamsServlet extends HttpServlet
       long servletStartTime = new Date().getTime();
       boolean isListDatastreamsRequest = false;
       boolean xml = false;
-
-      HashMap h=new HashMap();
-      h.put("application", "apia");
-      h.put("useCachedObject", "false");
-      String authenticatedUser=getAuthenticatedUser(request.getHeader("Authorization"));
-      if (authenticatedUser!=null) {
-          h.put("userId", authenticatedUser);
-          // add permissions to context, based on userId.
-          // FIXME:
-          // For now, fedoraAdmin has all important permissions.  How to do this
-          // eventually is to-be-decided, but for now it's hardcoded.
-          if (authenticatedUser.equals("fedoraAdmin")) {
-              h.put("canUseInactiveDatastream", "true");
-              h.put("canUseDeletedDatastream", "true");
-              h.put("canUseDeletedObject", "true");
-              h.put("canUseInactiveObject", "true");
-          }
-      } else {
-          h.put("userId", "guest");
-      }
-      h.put("host", request.getRemoteAddr());
-      ReadOnlyContext context = new ReadOnlyContext(h);
-
+ 
       requestURI = request.getRequestURL().toString() + "?" + request.getQueryString();
 
       // Parse servlet URL.
@@ -260,7 +204,8 @@ public class ListDatastreamsServlet extends HttpServlet
       }
 
       try {
-          if (isListDatastreamsRequest) {
+          if (isListDatastreamsRequest) {    
+              Context context = ReadOnlyContext.getContext(Authorization.ENVIRONMENT_REQUEST_SOAP_OR_REST_REST, request, ReadOnlyContext.DO_NOT_USE_CACHED_OBJECT);
               listDatastreams(context, PID, asOfDateTime, xml, request, response);
               long stopTime = new Date().getTime();
               long interval = stopTime - servletStartTime;
