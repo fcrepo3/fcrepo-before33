e1d8c21fa41c8e2ce1f16b2086467b8c8433522b
Edwin Shin <edwin.shin@yourmediashelf.com>
2005-03-09 23:21:08 +0000

    added MODEL.DEPENDS_ON property removed license text

--- src/java/fedora/common/rdf/FedoraModelNamespace.java
+++ src/java/fedora/common/rdf/FedoraModelNamespace.java
@@ -2,39 +2,20 @@ package fedora.common.rdf;
 
 import fedora.common.Constants;
 
-/**
- *
- * -----------------------------------------------------------------------------
- *
- * <p><b>License and Copyright: </b>The contents of this file are subject to the
- * Mozilla Public License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
- *
- * <p>Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.</p>
- *
- * <p>The entire file consists of original code.  Copyright &copy; 2002-2005 by The
- * Rector and Visitors of the University of Virginia and Cornell University.
- * All rights reserved.</p>
- *
- * -----------------------------------------------------------------------------
- *
- */
 public class FedoraModelNamespace extends RDFNamespace {
 
     // Properties
     public final RDFName CONTENT_MODEL;
     public final RDFName CREATED_DATE;
+    public final RDFName DATASTREAM_STATE;
     public final RDFName DEFINES_METHOD;
+    public final RDFName DEPENDS_ON;
     public final RDFName IMPLEMENTS_BDEF;
     public final RDFName LABEL;
     public final RDFName OWNER;
     public final RDFName STATE;
     public final RDFName USES_BMECH;
     public final RDFName OBJECT_STATE;
-    public final RDFName DATASTREAM_STATE;
 
     // Values
     public final RDFName ACTIVE;
@@ -55,6 +36,7 @@ public class FedoraModelNamespace extends RDFNamespace {
         this.CONTENT_MODEL        = new RDFName(this, "contentModel");
         this.CREATED_DATE         = new RDFName(this, "createdDate");
         this.DEFINES_METHOD       = new RDFName(this, "definesMethod");
+        this.DEPENDS_ON           = new RDFName(this, "dependsOn");
         this.IMPLEMENTS_BDEF      = new RDFName(this, "implementsBDef");
         this.LABEL                = new RDFName(this, "label");
 
