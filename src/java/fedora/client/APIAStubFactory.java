1efe59bd557e2d638ffe89aa474fa6174c32b871
Sandy Payette <spayette@duraspace.org>
2005-04-14 19:57:20 +0000

    Changes related to http/s:  rewrote the stub factory to return the appropriate SOAP stub based on protocol argument in the getStub method.

--- src/java/fedora/client/APIAStubFactory.java
+++ src/java/fedora/client/APIAStubFactory.java
@@ -12,52 +12,78 @@ import fedora.server.access.FedoraAPIAServiceLocator;
  * <p><b>Title:</b> APIAStubFactory.java</p>
  * <p><b>Description:</b> </p>
  *
- * -----------------------------------------------------------------------------
- *
- * <p><b>License and Copyright: </b>The contents of this file are subject to the
- * Mozilla Public License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
- *
- * <p>Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.</p>
- *
- * <p>The entire file consists of original code.  Copyright &copy; 2002-2005 by The
- * Rector and Visitors of the University of Virginia and Cornell University.
- * All rights reserved.</p>
- *
- * -----------------------------------------------------------------------------
  *
  * @author cwilper@cs.cornell.edu
  * @version $Id$
  */
 public abstract class APIAStubFactory {
 
-public static FedoraAPIA getStub(String host, int port, String username, String password)
+public static FedoraAPIA getStub(String protocol, String host, int port, String username, String password)
         throws MalformedURLException, ServiceException {
-    FedoraAPIAServiceLocator locator=new FedoraAPIAServiceLocator(username, password);  
-    URL ourl=new URL(locator.getFedoraAPIAPortSOAPHTTPAddress());
-    StringBuffer nurl=new StringBuffer();
-    nurl.append("http://");
-    nurl.append(host);
-    nurl.append(':');
-    nurl.append(port);
-    nurl.append(ourl.getPath());
-    if ((ourl.getQuery()!=null) && (!ourl.getQuery().equals("")) ) {
-        nurl.append('?');
-        nurl.append(ourl.getQuery());
+        	
+    FedoraAPIAServiceLocator locator=new FedoraAPIAServiceLocator(username, password);
+
+	//SDP - HTTPS support added
+	URL ourl = null;
+	URL nurl = null;  
+    if (protocol.equalsIgnoreCase("http")) { 
+		ourl=new URL(locator.getFedoraAPIAPortSOAPHTTPAddress());
+		nurl=rewriteServiceURL(ourl, protocol, host, port);
+		if (Administrator.INSTANCE==null) {
+			// if running without Administrator, don't wrap it with the statusbar stuff
+			return locator.getFedoraAPIAPortSOAPHTTP(nurl);
+		} else {
+			return new APIAStubWrapper(locator.getFedoraAPIAPortSOAPHTTP(nurl));
+		}
+    } else if (protocol.equalsIgnoreCase("https")){
+		ourl=new URL(locator.getFedoraAPIAPortSOAPHTTPSAddress());
+		nurl=rewriteServiceURL(ourl, protocol, host, port);
+		if (Administrator.INSTANCE==null) {
+			// if running without Administrator, don't wrap it with the statusbar stuff
+			return locator.getFedoraAPIAPortSOAPHTTPS(nurl);
+		} else {
+			return new APIAStubWrapper(locator.getFedoraAPIAPortSOAPHTTPS(nurl));
+		}
+    } else {
+		throw new javax.xml.rpc.ServiceException("The protocol" + " " + protocol 
+			+ " is not supported by this service.");
     }
-    if ((ourl.getRef()!=null) && (!ourl.getRef().equals("")) ) {
-        nurl.append('#');
-        nurl.append(ourl.getRef());
-    }
+
+/*
     if (Administrator.INSTANCE==null) {
         // if running without Administrator, don't wrap it with the statusbar stuff
         return locator.getFedoraAPIAPortSOAPHTTP(new URL(nurl.toString()));
     } else {
         return new APIAStubWrapper(locator.getFedoraAPIAPortSOAPHTTP(new URL(nurl.toString())));
     }
+*/
+}
+private static URL rewriteServiceURL(URL ourl, String protocol, String host, int port) 
+	throws MalformedURLException, ServiceException {
+
+	StringBuffer nurl=new StringBuffer();    
+	if (protocol.equalsIgnoreCase("http")) { 
+		nurl.append("http://"); 
+	} else if (protocol.equalsIgnoreCase("https")){
+		nurl.append("https://"); 
+	} else {
+		throw new javax.xml.rpc.ServiceException("The protocol" + " " + protocol 
+			+ " is not supported by this service.");
+	}
+
+	nurl.append(host);
+	nurl.append(':');
+	nurl.append(port);
+	nurl.append(ourl.getPath());
+	if ((ourl.getQuery()!=null) && (!ourl.getQuery().equals("")) ) {
+		nurl.append('?');
+		nurl.append(ourl.getQuery());
+	}
+	if ((ourl.getRef()!=null) && (!ourl.getRef().equals("")) ) {
+		nurl.append('#');
+		nurl.append(ourl.getRef());
+	}
+	return new URL(nurl.toString());
 }
 
 }
--- src/java/fedora/client/APIMStubFactory.java
+++ src/java/fedora/client/APIMStubFactory.java
@@ -12,52 +12,70 @@ import fedora.server.management.FedoraAPIMServiceLocator;
  * <p><b>Title:</b> APIMStubFactory.java</p>
  * <p><b>Description:</b> </p>
  *
- * -----------------------------------------------------------------------------
- *
- * <p><b>License and Copyright: </b>The contents of this file are subject to the
- * Mozilla Public License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
- *
- * <p>Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.</p>
- *
- * <p>The entire file consists of original code.  Copyright &copy; 2002-2005 by The
- * Rector and Visitors of the University of Virginia and Cornell University.
- * All rights reserved.</p>
- *
- * -----------------------------------------------------------------------------
  *
  * @author cwilper@cs.cornell.edu
  * @version $Id$
  */
 public abstract class APIMStubFactory {
 
-public static FedoraAPIM getStub(String host, int port, String username, String password)
-        throws MalformedURLException, ServiceException {
-    FedoraAPIMServiceLocator locator=new FedoraAPIMServiceLocator(username, password);
-    URL ourl=new URL(locator.getFedoraAPIMPortSOAPHTTPAddress());
-    StringBuffer nurl=new StringBuffer();
-    nurl.append("http://");
-    nurl.append(host);
-    nurl.append(':');
-    nurl.append(port);
-    nurl.append(ourl.getPath());
-    if ((ourl.getQuery()!=null) && (!ourl.getQuery().equals("")) ) {
-        nurl.append('?');
-        nurl.append(ourl.getQuery());
-    }
-    if ((ourl.getRef()!=null) && (!ourl.getRef().equals("")) ) {
-        nurl.append('#');
-        nurl.append(ourl.getRef());
-    }
-    if (Administrator.INSTANCE==null) {
-        // don't wrap if they're not using the GUI
-        return locator.getFedoraAPIMPortSOAPHTTP(new URL(nurl.toString()));
-    } else {
-        return new APIMStubWrapper(locator.getFedoraAPIMPortSOAPHTTP(new URL(nurl.toString())));
-    }
-}
+	public static FedoraAPIM getStub(String protocol, String host, int port, String username, String password)
+			throws MalformedURLException, ServiceException {
+        	
+		FedoraAPIMServiceLocator locator=new FedoraAPIMServiceLocator(username, password);
+
+		//SDP - HTTPS support added
+		URL ourl = null;
+		URL nurl = null;  
+		if (protocol.equalsIgnoreCase("http")) { 
+			ourl=new URL(locator.getFedoraAPIMPortSOAPHTTPAddress());
+			nurl=rewriteServiceURL(ourl, protocol, host, port);
+			if (Administrator.INSTANCE==null) {
+				// if running without Administrator, don't wrap it with the statusbar stuff
+				return locator.getFedoraAPIMPortSOAPHTTP(nurl);
+			} else {
+				return new APIMStubWrapper(locator.getFedoraAPIMPortSOAPHTTP(nurl));
+			}
+		} else if (protocol.equalsIgnoreCase("https")){
+			ourl=new URL(locator.getFedoraAPIMPortSOAPHTTPSAddress());
+			nurl=rewriteServiceURL(ourl, protocol, host, port);
+			if (Administrator.INSTANCE==null) {
+				// if running without Administrator, don't wrap it with the statusbar stuff
+				return locator.getFedoraAPIMPortSOAPHTTPS(nurl);
+			} else {
+				return new APIMStubWrapper(locator.getFedoraAPIMPortSOAPHTTPS(nurl));
+			}
+		} else {
+			throw new javax.xml.rpc.ServiceException("The protocol" + " " + protocol 
+				+ " is not supported by this service.");
+		}
+	}
+	private static URL rewriteServiceURL(URL ourl, String protocol, String host, int port) 
+		throws MalformedURLException, ServiceException {
+
+		StringBuffer nurl=new StringBuffer();    
+		if (protocol.equalsIgnoreCase("http")) { 
+			nurl.append("http://"); 
+		} else if (protocol.equalsIgnoreCase("https")){
+			nurl.append("https://"); 
+		} else {
+			throw new javax.xml.rpc.ServiceException("The protocol" + " " + protocol 
+				+ " is not supported by this service.");
+		}
+
+		nurl.append(host);
+		nurl.append(':');
+		nurl.append(port);
+		nurl.append(ourl.getPath());
+		if ((ourl.getQuery()!=null) && (!ourl.getQuery().equals("")) ) {
+			nurl.append('?');
+			nurl.append(ourl.getQuery());
+		}
+		if ((ourl.getRef()!=null) && (!ourl.getRef().equals("")) ) {
+			nurl.append('#');
+			nurl.append(ourl.getRef());
+		}
+		return new URL(nurl.toString());
+	}
+
 
 }
