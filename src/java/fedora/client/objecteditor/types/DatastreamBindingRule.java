67952d462316b750f21f33602d0b6e75ce99a1b0
Ross Wayland <rlw@virginia.edu>
2005-04-21 12:52:40 +0000

    removed license/copyright statement

--- src/java/fedora/client/objecteditor/types/DatastreamBindingRule.java
+++ src/java/fedora/client/objecteditor/types/DatastreamBindingRule.java
@@ -6,22 +6,7 @@ package fedora.client.objecteditor.types;
  * For a certain binding key (the "key" of the rule), a rule specifies a range
  * indicating the number of datastreams to be bound, whether order is important,
  * and the types of datastreams that may be bound.
- * -----------------------------------------------------------------------------
- *
- * <p><b>License and Copyright: </b>The contents of this file are subject to the
- * Mozilla Public License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
- *
- * <p>Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.</p>
- *
- * <p>The entire file consists of original code.  Copyright &copy; 2002-2005 by The
- * Rector and Visitors of the University of Virginia and Cornell University.
- * All rights reserved.</p>
- *
- * -----------------------------------------------------------------------------
+ * 
  */
 public class DatastreamBindingRule {
 
--- src/java/fedora/localservices/saxon/SaxonServlet.java
+++ src/java/fedora/localservices/saxon/SaxonServlet.java
@@ -22,15 +22,6 @@ import java.util.Properties;
  *
  * -----------------------------------------------------------------------------
  *
- * <p><b>License and Copyright: </b>The contents of this file are subject to the
- * Mozilla Public License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at <a href="http://www.mozilla.org/MPL/">http://www.mozilla.org/MPL/.</a></p>
- *
- * <p>Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.</p>
- *
  * <p>The original code is Copyright &copy; 2001 by Michael Kay. All rights
  * reserved. The current project homepage for Saxon may be found at:
  * <a href="http://saxon.sourceforge.net/">http://saxon.sourceforge.net/</a>.</p>
