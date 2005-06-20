ae6a36765ef36940f75d7bec9edf1af6b7916f62
Edwin Shin <edwin.shin@yourmediashelf.com>
2005-06-20 00:34:13 +0000

    cleanup of unused code. using LinkedHashSet should fix insertion order problem Ross was observing during tearDown's purge of demoObjects

--- src/test/junit/fedora/test/integration/TestIngestDemoObjects.java
+++ src/test/junit/fedora/test/integration/TestIngestDemoObjects.java
@@ -1,11 +1,10 @@
 package fedora.test.integration;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -15,16 +14,12 @@ import junit.extensions.TestSetup;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.custommonkey.xmlunit.HTMLDocumentBuilder;
 import org.custommonkey.xmlunit.SimpleXpathEngine;
-import org.custommonkey.xmlunit.TolerantSaxDocumentBuilder;
-import org.custommonkey.xmlunit.XMLUnit;
 import org.w3c.dom.Document;
 
 import fedora.client.FedoraClient;
 import fedora.client.search.SearchResultParser;
 import fedora.client.utility.ingest.Ingest;
-import fedora.server.config.ServerConfiguration;
 import fedora.test.FedoraServerTestCase;
 import fedora.test.FedoraServerTestSetup;
 import fedora.utilities.ExecUtility;
@@ -39,9 +34,6 @@ import fedora.utilities.ExecUtility;
 public class TestIngestDemoObjects extends FedoraServerTestCase {
     private String baseURL;
     private FedoraClient client;
-    private DocumentBuilderFactory factory;
-    private DocumentBuilder builder;
-    private ServerConfiguration fcfg;
     
     public static Test suite() {
         TestSuite suite = new TestSuite(TestIngestDemoObjects.class);
@@ -57,9 +49,6 @@ public class TestIngestDemoObjects extends FedoraServerTestCase {
     }
     
     public void setUp() throws Exception {
-        factory = DocumentBuilderFactory.newInstance();
-        builder = factory.newDocumentBuilder();
-        fcfg = getServerConfiguration();
         SimpleXpathEngine.registerNamespace("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc/");
         SimpleXpathEngine.registerNamespace(NS_FEDORA_TYPES_PREFIX, NS_FEDORA_TYPES);
         SimpleXpathEngine.registerNamespace("demo", "http://example.org/ns#demo");
@@ -117,7 +106,7 @@ public class TestIngestDemoObjects extends FedoraServerTestCase {
      * Gets the PIDs of objects of the specified type in the "demo" pid 
      * namespace that are in the repository
      * @param fTypes any combination of O, D, or M
-     * @return 
+     * @return set of PIDs of the specified object type
      * @throws Exception
      */
     public static Set getDemoObjects(String[] fTypes) throws Exception {
@@ -127,7 +116,7 @@ public class TestIngestDemoObjects extends FedoraServerTestCase {
         String baseURL = getBaseURL();
         FedoraClient client = new FedoraClient(baseURL, getUsername(), getPassword());
         InputStream queryResult;
-        Set pids = new HashSet();
+        Set pids = new LinkedHashSet();
         for (int i = 0; i < fTypes.length; i++) {
             queryResult = client.get(baseURL + "/search?query=pid~demo:*%20fType=" +
             		                 fTypes[i] + "&maxResults=1000&pid=true&xml=true", 
@@ -148,17 +137,6 @@ public class TestIngestDemoObjects extends FedoraServerTestCase {
                     getPassword() + " " + (String)it.next() + " " + getProtocol() + 
                     " for testing");
         }
-    }
-    
-    private void doViewItemIndex(String pid) throws Exception {
-        InputStream is = client.get(baseURL + "/get/" + pid + "/fedora-system:3/viewItemIndex", true);
-        Document result;
-        TolerantSaxDocumentBuilder tolerantSaxDocumentBuilder = new TolerantSaxDocumentBuilder(XMLUnit.getTestParser());
-        HTMLDocumentBuilder htmlDocumentBuilder = new HTMLDocumentBuilder(tolerantSaxDocumentBuilder);
-        result = htmlDocumentBuilder.parse(new BufferedReader(new InputStreamReader(is)));
-        
-        
-        //assertXpathEvaluatesTo("Item One", "/html/body//li[@id='1']", result);
     }
     
     public static void main(String[] args) {
