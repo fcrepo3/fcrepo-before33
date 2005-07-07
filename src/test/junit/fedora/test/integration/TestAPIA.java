069b386609fbc77faf4c2b058a1c3858689c20f9
Sandy Payette <spayette@duraspace.org>
2005-07-07 21:01:21 +0000

    added new getDissemination test cases.

--- src/test/junit/fedora/test/integration/TestAPIA.java
+++ src/test/junit/fedora/test/integration/TestAPIA.java
@@ -10,6 +10,9 @@ import junit.framework.TestSuite;
 
 import org.custommonkey.xmlunit.SimpleXpathEngine;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import fedora.client.APIAStubFactory;
 import fedora.server.access.FedoraAPIA;
 import fedora.server.types.gen.MIMETypedStream;
@@ -17,6 +20,7 @@ import fedora.server.types.gen.RepositoryInfo;
 import fedora.server.types.gen.ObjectProfile;
 import fedora.server.types.gen.DatastreamDef;
 import fedora.server.types.gen.ObjectMethodsDef;
+import fedora.server.types.gen.Property;
 import fedora.server.types.gen.MethodParmDef;
 import fedora.test.FedoraServerTestCase;
 import fedora.test.FedoraServerTestSetup;
@@ -28,19 +32,16 @@ import fedora.test.FedoraServerTestSetup;
  */
 public class TestAPIA extends FedoraServerTestCase {
     private FedoraAPIA apia;
-    private String testPID;
     
     public static Test suite() {
 		TestSuite suite = new TestSuite("Unit test for APIA");
 		suite.addTestSuite(TestAPIA.class);
 		TestSetup wrapper = new TestSetup(suite) {
-    	//
-        //TestSuite suite = new TestSuite(TestAPIA.class);
-        //TestSetup wrapper = new TestSetup(suite) {
             public void setUp() throws Exception {
                 TestIngestDemoObjects.ingestDemoObjects();
                 SimpleXpathEngine.registerNamespace("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc/");
                 SimpleXpathEngine.registerNamespace("dc", "http://purl.org/dc/elements/1.1/");
+				SimpleXpathEngine.registerNamespace("uvalibadmin", "http://dl.lib.virginia.edu/bin/admin/admin.dtd/");
             }
             
             public void tearDown() throws Exception {
@@ -52,7 +53,6 @@ public class TestAPIA extends FedoraServerTestCase {
     }
     
     public void setUp() throws Exception {
-		testPID = "demo:11";
         apia = APIAStubFactory.getStub(getProtocol(), getHost(), 
                 Integer.parseInt(getPort()), getUsername(), getPassword());
     }
@@ -63,8 +63,8 @@ public class TestAPIA extends FedoraServerTestCase {
     }
     
 	public void testGetObjectProfile() throws Exception {
-		ObjectProfile profile = apia.getObjectProfile(testPID, null);
-		assertEquals(testPID, profile.getPid());
+		ObjectProfile profile = apia.getObjectProfile("demo:11", null);
+		assertEquals("demo:11", profile.getPid());
 		assertEquals("UVA_MRSID_IMAGE", profile.getObjContentModel());
 		assertEquals("O", profile.getObjType());
 		assertTrue(!profile.getObjDissIndexViewURL().equals(""));
@@ -72,24 +72,63 @@ public class TestAPIA extends FedoraServerTestCase {
 	}
 	
 	public void testObjectHistory() throws Exception {
-		String[] timestamps = apia.getObjectHistory(testPID);
+		String[] timestamps = apia.getObjectHistory("demo:11");
 		assertTrue(timestamps.length > 0);
 	}
     
-    public void testGetDatastreamDissemination() throws Exception {
-        MIMETypedStream ds = apia.getDatastreamDissemination(testPID, "DC", null);
-        assertXpathExists("/oai_dc:dc", new String(ds.getStream()));
+    public void testGetDissemination() throws Exception {
+    	
+		MIMETypedStream diss = null;
+        diss = apia.getDissemination("demo:11", "demo:8", "getThumbnail", new Property[0], null);
+		assertEquals(diss.getMIMEType(),"image/jpeg");
+		assertTrue(diss.getStream().length > 0);
+		
+		diss = apia.getDissemination("demo:11", "fedora-system:3", "viewDublinCore", new Property[0], null);
+		assertEquals(diss.getMIMEType(),"text/html");
+		assertTrue(diss.getStream().length > 0);
+		
+		Property[] parms = new Property[2];
+		Property p = new Property();
+		p.setName("ZOOM");
+		p.setValue("no");
+		parms[0] = p;
+		Property p2 = new Property();
+		p2.setName("SIZE");
+		p2.setValue("small");
+		parms[1] = p2;
+		diss = apia.getDissemination("demo:11", "demo:8", "getImage", parms, null);
+		assertEquals(diss.getMIMEType(),"image/jpeg");
+		assertTrue(diss.getStream().length > 0);
+  
     }
+    
+	public void testGetDatastreamDissemination() throws Exception {
+		MIMETypedStream ds = null;
+		ds = apia.getDatastreamDissemination("demo:11", "DC", null);
+		assertXpathExists("/oai_dc:dc", new String(ds.getStream()));
+		
+		ds = apia.getDatastreamDissemination("demo:11", "TECH1", null);
+		String dsXML = new String(ds.getStream(), "UTF-8");
+		assertEquals(ds.getMIMEType(),"text/xml");
+		assertTrue(ds.getStream().length > 0);		
+		assertXpathExists("//uvalibadmin:technical",dsXML);
+		assertXpathEvaluatesTo(
+			"wavelet", "/uvalibadmin:admin/uvalibadmin:technical/uvalibadmin:compression/text( )", dsXML);
+			
+		ds = apia.getDatastreamDissemination("demo:11", "DS1", null);
+		assertEquals(ds.getMIMEType(),"image/x-mrsid-image");
+		assertTrue(ds.getStream().length > 0);		      
+	}
  
 	public void testListDatastreams() throws Exception {
-		DatastreamDef[] dsDefs = apia.listDatastreams(testPID, null);
+		DatastreamDef[] dsDefs = apia.listDatastreams("demo:11", null);
 		assertEquals(dsDefs.length,8);
 		verifyDatastreamDefs(dsDefs, "testListDatastream: ");
 	}
 	
 	public void testListMethods() throws Exception {
-		ObjectMethodsDef[] methodDefs = apia.listMethods(testPID, null);
-		assertEquals(methodDefs.length,7);
+		ObjectMethodsDef[] methodDefs = apia.listMethods("demo:11", null);
+		assertEquals(methodDefs.length,6);
 		verifyObjectMethods(methodDefs, "testListMethods: ");
 	}
 
