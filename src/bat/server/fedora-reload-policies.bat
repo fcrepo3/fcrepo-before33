2a58a0131749a4f4f240132617404f8ad46381c2
Bill Niebel <wdn5e@virginia.edu>
2005-08-02 19:56:32 +0000

    *** empty log message ***

---
+++ src/bat/server/fedora-reload-policies.bat
@@ -0,0 +1,59 @@
+@echo off
+@rem usage is fedora-reload-policies http(s) port
+
+goto checkEnv
+
+:envOk
+
+set TOMCAT_DIR=@tomcat.basename@
+set TC=%FEDORA_HOME%\server\%TOMCAT_DIR%
+set TC_COMMON=%TC%\common\lib
+set OLD_JAVA_HOME=%JAVA_HOME%
+set JAVA_HOME=%THIS_JAVA_HOME%
+set SERVER_CONTROLLER_LIBS=@ServerController.windows.libs@
+
+echo Reloading Fedora policies...
+"%JAVA_HOME%\bin\java" -cp %TC%\webapps\fedora\WEB-INF\classes;%TC%\webapps\fedora\WEB-INF\lib\commons-httpclient-2.0.1.jar;%TC%\webapps\fedora\WEB-INF\lib\commons-logging.jar;%SERVER_CONTROLLER_LIBS% -Dfedora.home=%FEDORA_HOME% fedora.server.utilities.ServerUtility reloadPolicies %1
+
+goto end
+
+:checkEnv
+if "%FEDORA_HOME%" == "" goto noFedoraHome
+if not exist %FEDORA_HOME%\server\config\fedora.fcfg goto configNotFound
+if "%FEDORA_JAVA_HOME%" == "" goto tryJavaHome
+set THIS_JAVA_HOME=%FEDORA_JAVA_HOME%
+:checkJava
+if not exist "%THIS_JAVA_HOME%\bin\java.exe" goto noJavaBin
+if not exist "%THIS_JAVA_HOME%\bin\orbd.exe" goto badJavaVersion
+goto envOk
+
+:tryJavaHome
+if "%JAVA_HOME%" == "" goto noJavaHome
+set THIS_JAVA_HOME=%JAVA_HOME%
+goto checkJava
+
+:noFedoraHome
+echo ERROR: Environment variable, FEDORA_HOME must be set.
+goto end
+
+:configNotFound
+echo ERROR: FEDORA_HOME does not appear correctly set.
+echo Configuration cannot be found at %FEDORA_HOME%\server\config\fedora.fcfg
+goto end
+
+:noJavaHome
+echo ERROR: FEDORA_JAVA_HOME was not defined, nor was (the fallback) JAVA_HOME.
+goto end
+
+:noJavaBin
+echo ERROR: java.exe was not found in %THIS_JAVA_HOME%
+echo Make sure FEDORA_JAVA_HOME or JAVA_HOME is set correctly.
+goto end
+
+:badJavaVersion
+echo ERROR: java was found in %THIS_JAVA_HOME%, but it was not version 1.4
+echo Make sure FEDORA_JAVA_HOME or JAVA_HOME points to a 1.4JRE/JDK base.
+goto end
+
+:end
+
