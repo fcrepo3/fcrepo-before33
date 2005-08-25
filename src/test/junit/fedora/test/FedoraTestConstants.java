04387c83b182d7bbe1e942a1a9bab84c773e4a96
Sandy Payette <spayette@duraspace.org>
2005-08-25 19:07:54 +0000

    Modified static variable that refers to fedora.fcfg within the source directory to now refer to the new file name of fedora-defaults.fcfg within src.

--- src/test/junit/fedora/test/FedoraTestConstants.java
+++ src/test/junit/fedora/test/FedoraTestConstants.java
@@ -14,7 +14,7 @@ public interface FedoraTestConstants {
 
     public static final String FEDORA_HOME = System.getProperty(PROP_FEDORA_HOME, new File("dist").getAbsolutePath());
     public static final String FCFG = FEDORA_HOME + "/server/config/fedora.fcfg";
-    public static final String FCFG_SRC = "src/fcfg/server/fedora.fcfg";
+    public static final String FCFG_SRC = "src/fcfg/server/fedora-defaults.fcfg";
     public static final String FCFG_SRC_DIR = "src/fcfg/server";
 
     public static final String BESECURITY_PATH = FEDORA_HOME + "/server/config/beSecurity.xml";
