ada690be1aaaca78b6243e04bd97690e73a0f362
Bill Niebel <wdn5e@virginia.edu>
2005-09-02 14:13:40 +0000

    toward security tests

--- src/test/junit/fedora/test/config/TestAPIALiteOpen.java
+++ src/test/junit/fedora/test/config/TestAPIALiteOpen.java
@@ -3,7 +3,7 @@ package fedora.test.config;
 import java.io.*;
 import java.util.Iterator;
 import java.util.Set;
-
+import java.util.HashSet;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.extensions.TestSetup;
@@ -56,12 +56,155 @@ public class TestAPIALiteOpen extends SuperAPIALite {
     	HTTPS403 = new HttpDataSource(Trial.HTTPS_BASE_URL, 403);
     }
 	
+    /*
     public void testDescribeRepository() throws Exception {
 		iterate(Trial.ALL_SHIPPED_POLICY_TRIALS, HTTP200, describeRepositoryTestXmlOnly);
     	iterate(Trial.ALL_SHIPPED_POLICY_TRIALS, HTTPS200, describeRepositoryTestXmlOnly);
     	iterate(Trial.ALL_NO_POLICY_TRIALS, HTTP403, describeRepositoryTestXmlOnly);  	
     	iterate(Trial.ALL_NO_POLICY_TRIALS, HTTPS403, describeRepositoryTestXmlOnly);  	    	
     }
+    */
+
+    
+    private static final Set policiesSet = new HashSet ();
+    static {
+    	policiesSet.add("defaultPolicies");
+    	policiesSet.add("noPolicies");
+    }
+    
+    private static final Set protocolSet = new HashSet ();
+    static {
+    	protocolSet.add("HTTP");
+    	protocolSet.add("HTTPS");
+    }
+    
+    private static final Set objectsetSet = new HashSet ();
+    static {
+    	objectsetSet.add("demoObjects");
+    	//objectsetSet.add("fewPids");
+    	//objectsetSet.add("missingPids");
+    	//objectsetSet.add("badPids");
+    }
+
+    private static final Set expectedStatusSet = new HashSet ();
+    static {
+    	expectedStatusSet.add(new Integer(302));
+    	expectedStatusSet.add(new Integer(401));
+    	expectedStatusSet.add(new Integer(403));
+    	expectedStatusSet.add(new Integer(500));
+    	expectedStatusSet.add(new Integer(200));
+    }
+
+    /*
+	private ObjectProfileTest objectProfileTestXmlOnly = null;
+	void iterateObjectProfile(Iterator iterator, DataSource dataSource, Set trials) throws Exception {
+        while (iterator.hasNext()) {
+        	String pid = (String) iterator.next();
+        	objectProfileTestXmlOnly = new ObjectProfileTest(pid, true);
+        	iterate(trials, dataSource, objectProfileTestXmlOnly);
+        }		
+	}
+	*/
+    
+    public static final Set getObjects(String name) {
+    	Set temp = null;
+    	if ("demoObjects".equals(name)) {
+    		temp = fewPids; //demoObjects;
+    	} else if ("badPids".equals(name)) {
+    		temp = badPids;
+    	} else if ("missingPids".equals(name)) {
+    		temp = missingPids;    		
+		} else if ("fewPids".equals(name)) {
+			temp = fewPids;    		
+		}
+    	return temp;
+    }
+	public void testPerObjectOperations() throws Exception {
+		//String op = "objectProfile";
+		Iterator policiesIterator = policiesSet.iterator();
+		while (policiesIterator.hasNext()) {
+			String policies = (String) policiesIterator.next();
+			Iterator protocolIterator = protocolSet.iterator();
+			while (protocolIterator.hasNext()) {
+				String protocol = (String) protocolIterator.next();
+				Iterator objectsetIterator = objectsetSet.iterator();
+				while (objectsetIterator.hasNext()) {
+					String objectset = (String) objectsetIterator.next();
+					Iterator expectedStatusIterator = expectedStatusSet.iterator();
+					while (expectedStatusIterator.hasNext()) {
+						int expectedStatus = ((Integer)expectedStatusIterator.next()).intValue();
+						System.out.println("PROTOCOL FOR DATASOURCE = " + protocol);
+						System.out.println("EXPECTEDSTATUS FOR DATASOURCE = " + expectedStatus);						
+						//DataSource dataSource = HttpDataSource.getDataSource(protocol, expectedStatus);
+						String baseurl = "HTTPS".equals(protocol) ? Trial.HTTPS_BASE_URL : Trial.HTTP_BASE_URL; 
+						DataSource dataSource = new HttpDataSource (baseurl, expectedStatus);						
+						System.out.println("DATASOURCE = " + dataSource);
+						System.out.println("in loop nest, policies==" + policies);						
+						Set objects = getObjects(objectset);
+						Iterator objectIterator = objects.iterator();
+						while (objectIterator.hasNext()) {
+				        	String pid = (String) objectIterator.next();
+				        	Set trials = null;
+							trials = Trial.getTrialSet(Trial.OPEN, policies, protocol, "objectProfile", objectset, expectedStatus);
+				        	ObjectProfileTest objectProfileTestXmlOnly = new ObjectProfileTest(pid, true);					        	
+				        	iterate(trials, dataSource, objectProfileTestXmlOnly);
+				        	
+							trials = Trial.getTrialSet(Trial.OPEN, policies, protocol, "objectProfile", objectset, expectedStatus);
+							ListMethodsTest listMethodsTestXmlOnly = new ListMethodsTest(pid, true);					        	
+				        	iterate(trials, dataSource, listMethodsTestXmlOnly);
+						}
+					}					
+				}
+			}
+		}
+    }       
+	
+    public void testOtherOperations() throws Exception {
+		//String op = "describeRepository";
+		Iterator policiesIterator = policiesSet.iterator();
+		while (policiesIterator.hasNext()) {
+			String policies = (String) policiesIterator.next();
+			Iterator protocolIterator = protocolSet.iterator();
+			while (protocolIterator.hasNext()) {
+				String protocol = (String) protocolIterator.next();
+				
+				//Iterator objectsetIterator = objectsetSet.iterator();
+				//while (objectsetIterator.hasNext()) {
+					//String objectset = (String) objectsetIterator.next();
+					Iterator expectedStatusIterator = expectedStatusSet.iterator();
+					while (expectedStatusIterator.hasNext()) {
+						int expectedStatus = ((Integer)expectedStatusIterator.next()).intValue();
+						System.out.println("PROTOCOL FOR DATASOURCE = " + protocol);
+						System.out.println("EXPECTEDSTATUS FOR DATASOURCE = " + expectedStatus);						
+						//DataSource dataSource = HttpDataSource.getDataSource(protocol, expectedStatus);
+						String baseurl = "HTTPS".equals(protocol) ? Trial.HTTPS_BASE_URL : Trial.HTTP_BASE_URL; 
+						DataSource dataSource = new HttpDataSource (baseurl, expectedStatus);						
+						System.out.println("DATASOURCE = " + dataSource);
+						System.out.println("in loop nest, policies==" + policies);						
+						//Set objects = getObjects(objectset);
+						//Iterator objectIterator = objects.iterator();
+						//while (objectIterator.hasNext()) {
+				        	//String pid = (String) objectIterator.next();
+							Set trials = null;
+							trials = Trial.getTrialSet(Trial.OPEN, policies, protocol, "describeRepository", expectedStatus);
+							DescribeRepositoryTest describeRepositoryTestXmlOnly = new DescribeRepositoryTest("Fedora Repository", true);					        	
+				        	iterate(trials, dataSource, describeRepositoryTestXmlOnly);
+
+							trials = Trial.getTrialSet(Trial.OPEN, policies, protocol, "describeRepository", expectedStatus);
+							FindTest findTestXmlOnly = new FindTest("pid=demo:*", 1000000, demoObjects.size(), true);					        	
+				        	iterate(trials, dataSource, findTestXmlOnly);
+				        	
+							trials = Trial.getTrialSet(Trial.OPEN, policies, protocol, "describeRepository", expectedStatus);
+							FindTest resumeFindTestXmlOnly = new FindTest("pid=demo:*", 10, demoObjects.size(), true);					        	
+							iterate(trials, dataSource, resumeFindTestXmlOnly);
+				        	
+						//}
+					}					
+				//}
+			}
+		}
+    }
+   
     
     public static Test suite() {
         TestSuite suite = new TestSuite("TestAPIALite Open Configuration");
@@ -69,6 +212,7 @@ public class TestAPIALiteOpen extends SuperAPIALite {
         TestSetup wrapper = new TestSetup(suite) {
         //TestSetup wrapper = new TestSetup(suite, TestAPIALiteOutOfTheBoxConfig.class.getName()) {
             public void setUp() throws Exception {
+            	FedoraServerTestCase.ssl = "";
                 TestIngestDemoObjects.ingestDemoObjects();
                 fcfg = getServerConfiguration();
                 client = new FedoraClient(getBaseURL(), getUsername(), getPassword());
