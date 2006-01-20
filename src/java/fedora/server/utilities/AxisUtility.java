663eb07e92813adc501de14a3e77529855f7380a
Edwin Shin <edwin.shin@yourmediashelf.com>
2006-01-20 08:02:14 +0000

    addresses the AxisFault SaxParseExceptions

--- src/java/fedora/server/utilities/AxisUtility.java
+++ src/java/fedora/server/utilities/AxisUtility.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import javax.xml.namespace.QName;
 import org.apache.axis.AxisFault;
 import org.apache.axis.client.AdminClient;
+
 import fedora.server.errors.ServerException;
 import fedora.server.errors.authorization.AuthzDeniedException;
 import fedora.server.errors.authorization.AuthzException;
@@ -24,11 +25,10 @@ public abstract class AxisUtility {
     /**
      * The (SOAP[version-specific] spec-dictated) namespace for fault codes.
      * See http://www.w3.org/TR/SOAP/#_Toc478383510 for SOAPv1.1
-     * (what Axis currently conforms to) and
-     * http://www.w3.org/TR/soap12-part1/#faultcodeelement for SOAPv1.2
-     * SOAP v1.1 here.
+     * and http://www.w3.org/TR/soap12-part1/#faultcodeelement for SOAPv1.2
+     * SOAP v1.2 here.
      */
-    public static String SOAP_FAULT_CODE_NAMESPACE="http://schemas.xmlsoap.org/soap/envelope/";
+    public static String SOAP_FAULT_CODE_NAMESPACE="http://www.w3.org/2003/05/soap-envelope";
 
     /**
      * Similar to above, this is "actor" in soap1_1 and "role"  in 1_2.
@@ -38,46 +38,45 @@ public abstract class AxisUtility {
      * (see http://www.w3.org/TR/soap12-part1/#soaproles) three special URIs --
      * one of which is for ultimate recievers, which is the category Fedora
      * falls into.  http://www.w3.org/2002/06/soap-envelope/role/ultimateReceiver
-     * is the URI v1.2 provides.  Since we're doing soap1.1 with axis, we
-     * interpolate and use http://schemas.xmlsoap.org/soap/actor/ultimateReceiver.
+     * is the URI v1.2 provides.
      */
-    public static String SOAP_ULTIMATE_RECEIVER="http://schemas.xmlsoap.org/soap/actor/ultimateReceiver";
+    public static String SOAP_ULTIMATE_RECEIVER="http://www.w3.org/2003/05/soap-envelope/role/ultimateReceiver";
 
     public static void throwFault(ServerException se)
             throws AxisFault {
-        String[] details=se.getDetails();
-        StringBuffer buf=new StringBuffer();
-        for (int i=0; i<details.length; i++) {
-            buf.append("<detail>");
-            buf.append(details[i]);
-            buf.append("</detail>\n");
-        }
-        AxisFault fault=new AxisFault(new QName(SOAP_FAULT_CODE_NAMESPACE,
-                se.getCode()), se.getMessage(), SOAP_ULTIMATE_RECEIVER,
-                null);
-        fault.setFaultDetailString(buf.toString());
+    	AxisFault fault =  AxisFault.makeFault(se);
+    	String[] details=se.getDetails();
+    	
+    	if (details.length > 0) {
+	        StringBuffer buf = new StringBuffer();
+	        for (int i = 0; i < details.length; i++) {
+	            buf.append("<detail>");
+	            buf.append(details[i]);
+	            buf.append("</detail>\n");
+	        }
+	    	fault.setFaultDetailString(buf.toString());
+    	}
         throw fault;
     }
 
-    public static AxisFault getFault(ServerException se) {
-        String[] details=se.getDetails();
-        StringBuffer buf=new StringBuffer();
-        for (int i=0; i<details.length; i++) {
-            buf.append("<detail>");
-            buf.append(details[i]);
-            buf.append("</detail>\n");
-        }
-        AxisFault fault=new AxisFault(new QName(SOAP_FAULT_CODE_NAMESPACE,
-                se.getCode()), se.getMessage(), SOAP_ULTIMATE_RECEIVER,
-                null);
-        fault.setFaultDetailString(buf.toString());
-        return fault;
+    public static AxisFault getFault(ServerException se) {    	
+    	AxisFault fault =  AxisFault.makeFault(se);
+    	String[] details=se.getDetails();
+    	
+    	if (details.length > 0) {
+	        StringBuffer buf = new StringBuffer();
+	        for (int i = 0; i < details.length; i++) {
+	            buf.append("<detail>");
+	            buf.append(details[i]);
+	            buf.append("</detail>\n");
+	        }
+	    	fault.setFaultDetailString(buf.toString());
+    	}
+    	return fault;
     }
     
     public static AxisFault getFault(AuthzException e) {
-        AxisFault fault=new AxisFault(new QName(SOAP_FAULT_CODE_NAMESPACE, e.getCode()), 
-				e.getMessage(), SOAP_ULTIMATE_RECEIVER,
-                null);
+        AxisFault fault=AxisFault.makeFault(e);
     	String reason = "";
     	if (e instanceof AuthzOperationalException) {
     		reason = AuthzOperationalException.BRIEF_DESC;
@@ -91,10 +90,11 @@ public abstract class AxisUtility {
     }
 
     public static AxisFault getFault(Throwable e) {
-        AxisFault fault=new AxisFault(new QName(SOAP_FAULT_CODE_NAMESPACE,
-                "Uncaught"),
-                e.getClass().getName() + ":" + e.getMessage(), SOAP_ULTIMATE_RECEIVER,
-                null);
+        AxisFault fault = new AxisFault(
+        				  	new QName(SOAP_FAULT_CODE_NAMESPACE, "Uncaught"),
+        				  e.getClass().getName() + ":" + e.getMessage(), 
+        				  SOAP_ULTIMATE_RECEIVER,
+        				  null);
         return fault;
     }
 
