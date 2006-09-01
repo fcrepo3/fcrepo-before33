113fe7d7fec75e78763d1cdee1eeae95f21e1f86
Chris Wilper <cwilper@duraspace.org>
2006-09-01 20:49:40 +0000

    marker interface supporting delegate pattern for management (currently used by journaling)

---
+++ src/java/fedora/server/management/ManagementDelegate.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * <p><b>Title:</b> ManagementDelegate.java</p>
+ * <p><b>Description:</b> An extension of the management subsystem interface
+ * that can be wrapped by a Journal module. At some point it may contain 
+ * methods, but for now it is just a tagging interface.</p>
+ *
+ * @author jblake@cs.cornell.edu
+ * @version $Id$
+ */
+public interface ManagementDelegate extends Management {
+
+}
