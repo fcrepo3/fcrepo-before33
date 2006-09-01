6b4707fb71e0943ffae5bc797f6f8c365b2f0ef0
Chris Wilper <cwilper@duraspace.org>
2006-09-01 20:30:31 +0000

    pre-initializes actionAttributes and resourceAttributes, for safety. the following lines were inserted:          m_actionAttributes=new MultiValueMap();          m_actionAttributes.lock();          m_resourceAttributes=new MultiValueMap();          m_resourceAttributes.lock();

--- src/java/fedora/server/ReadOnlyContext.java
+++ src/java/fedora/server/ReadOnlyContext.java
@@ -1,3 +1,23 @@
+/*
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Educational Community License (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.opensource.org/licenses/ecl1.txt">
+ * http://www.opensource.org/licenses/ecl1.txt.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002-2006 by 
+ * The Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
 package fedora.server;
 
 import java.util.Date;
@@ -69,7 +89,11 @@ public class ReadOnlyContext implements Context {
         if (m_subjectAttributes==null) {
             m_subjectAttributes=new MultiValueMap();
         }
-        m_subjectAttributes.lock();   
+        m_subjectAttributes.lock(); 
+        m_actionAttributes=new MultiValueMap();
+        m_actionAttributes.lock();
+        m_resourceAttributes=new MultiValueMap();
+        m_resourceAttributes.lock();
         if (password == null) {
         	password = "";
         }
