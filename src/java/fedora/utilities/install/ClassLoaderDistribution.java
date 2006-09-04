a23467269458a8bd15431d00d0f3fa531c76b456
Edwin Shin <edwin.shin@yourmediashelf.com>
2006-09-04 05:07:42 +0000

    the get method now returns resources in "resources/". i.e., get("foo") actually returns ClassLoader.getResourceAsStream("resources/foo").

--- src/java/fedora/utilities/install/ClassLoaderDistribution.java
+++ src/java/fedora/utilities/install/ClassLoaderDistribution.java
@@ -1,6 +1,8 @@
 package fedora.utilities.install;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ClassLoaderDistribution extends Distribution {
 
@@ -15,17 +17,29 @@ public class ClassLoaderDistribution extends Distribution {
     }
 
     public boolean contains(String path) {
-        return _cl.getResource(path) != null;
+        return _cl.getResource(rewritePath(path)) != null;
     }
-
+    
     public InputStream get(String path) throws IOException {
-
-        InputStream stream = _cl.getResourceAsStream(path);
+        InputStream stream = _cl.getResourceAsStream(rewritePath(path));
         if (stream == null) {
             throw new FileNotFoundException("Not found in classpath: " + path);
         } else {
             return stream;
         }
+    }
+    
+    /**
+     * Note: we don't check for backtracking
+     * @param path
+     * @return
+     */
+    private static String rewritePath(String path) {
+    	if (path.startsWith("/")) {
+    		path = path.substring(1);
+    	}
+    	// Note, ClassLoader paths are always absolute, so , so no leading slash
+    	return "resources/" + path;
     }
 
 }
