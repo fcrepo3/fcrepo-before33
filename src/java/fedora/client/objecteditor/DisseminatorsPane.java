bffca5d063cb3a1651ee4f15f8f0864171af6abd
Chris Wilper <cwilper@duraspace.org>
2006-09-11 12:30:40 +0000

    Updates to move wsdl to document/literal "wrapped" style, for better tool interop.

--- src/java/fedora/client/objecteditor/DisseminatorsPane.java
+++ src/java/fedora/client/objecteditor/DisseminatorsPane.java
@@ -1,3 +1,23 @@
+/*
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Educational Community License (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.opensource.org/licenses/ecl1.txt">
+ * http://www.opensource.org/licenses/ecl1.txt.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2002-2006 by 
+ * The Rector and Visitors of the University of Virginia and Cornell University.
+ * All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
 package fedora.client.objecteditor;
 
 import java.awt.*;
@@ -44,6 +64,7 @@ public class DisseminatorsPane
 		
         Disseminator[] currentVersions=Administrator.APIM.
                     getDisseminators(pid, null, null);
+        if (currentVersions == null) currentVersions = new Disseminator[0];
         m_disseminatorPanes=new DisseminatorPane[currentVersions.length];
         for (int i=0; i<currentVersions.length; i++) {
             Disseminator[] versions=Administrator.APIM.getDisseminatorHistory(
