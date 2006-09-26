1f562838a9cc809cb50448cedab116c0d05b2737
Edwin Shin <edwin.shin@yourmediashelf.com>
2006-09-26 17:12:34 +0000

    cleanup

--- src/test/junit/fedora/server/config/TestServerConfiguration.java
+++ src/test/junit/fedora/server/config/TestServerConfiguration.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.Properties;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -20,10 +19,8 @@ import fedora.test.FedoraTestCase;
  * @author Edwin Shin
  */
 public class TestServerConfiguration extends FedoraTestCase {
+	private static final File FCFG_BASE = new File("src/fcfg/server/fedora-base.fcfg");
     private static final String NS_FCFG_PREFIX = "fcfg";
-    //private File originalFile;
-    //private FileInputStream fis;
-    //private ServerConfiguration config;
     private DocumentBuilder builder;
     private ByteArrayOutputStream out;
     
@@ -39,10 +36,13 @@ public class TestServerConfiguration extends FedoraTestCase {
         // Namespace support is enabled by a patch to XMLUnit
         // see: http://sourceforge.net/tracker/index.php?func=detail&aid=953445&group_id=23187&atid=377770
         SimpleXpathEngine.registerNamespace(NS_FCFG_PREFIX, NS_FCFG);
+        XMLUnit.setIgnoreWhitespace(false);
     }
     
     protected void tearDown() throws Exception {
         SimpleXpathEngine.clearNamespaces();
+        XMLUnit.setIgnoreWhitespace(false);
+        out.close();
     }
     
     /*
@@ -54,25 +54,9 @@ public class TestServerConfiguration extends FedoraTestCase {
         //TODO Implement copy().
     }
     */
-    
-    public void testFoo() throws Exception {
-        File originalFile = new File(FCFG);
-        FileInputStream fis = new FileInputStream(originalFile);
-        ServerConfigurationParser scp = new ServerConfigurationParser(fis);
-        ServerConfiguration config = scp.parse();
-        
-        Properties props = new Properties();
-        props.load(new FileInputStream("src/fcfg/server/eddie.properties"));
-        config.applyProperties(props);
-        config.serialize(new FileOutputStream("/tmp/eddie.fcfg"));
-        
-    }
 
     public void testApplyProperties() throws Exception {
-        File originalFile = new File(FCFG);
-        FileInputStream fis = new FileInputStream(originalFile);
-        ServerConfigurationParser scp = new ServerConfigurationParser(fis);
-        ServerConfiguration config = scp.parse();
+        ServerConfiguration config = getServerConfiguration();
         
         String testVal = "9999";
         String xpath = "/" + NS_FCFG_PREFIX + ":server/" + NS_FCFG_PREFIX + 
@@ -93,29 +77,22 @@ public class TestServerConfiguration extends FedoraTestCase {
     }
 
     public void testSerialize() throws Exception {
-        File originalFile = new File(FCFG);
-        FileInputStream fis = new FileInputStream(originalFile);
-        ServerConfigurationParser scp = new ServerConfigurationParser(fis);
-        ServerConfiguration config = scp.parse();
-        
-        Document original = builder.parse(originalFile);
-        
+        ServerConfiguration config = getServerConfiguration();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         config.serialize(out);
         
-        //config.serialize(new FileOutputStream("/tmp/fcfg.fcfg"));
-        
+        Document original = builder.parse(FCFG_BASE);
         Document generated = builder.parse(new ByteArrayInputStream(out.toByteArray()));
         XMLUnit.setIgnoreWhitespace(true);
         assertXMLEqual(original, generated);
-        XMLUnit.setIgnoreWhitespace(false);
+    }
+    
+    public static ServerConfiguration getServerConfiguration() throws Exception {
+        return new ServerConfigurationParser(
+                new FileInputStream(FCFG_BASE)).parse();
     }
     
     private Document getDocument(ByteArrayOutputStream out) throws Exception {
         return builder.parse(new ByteArrayInputStream(out.toByteArray()));
-    }
-    
-    private String getDocumentAsString(ByteArrayOutputStream out) throws Exception {
-        return new String(out.toByteArray(), "UTF-8");
     }
 }
