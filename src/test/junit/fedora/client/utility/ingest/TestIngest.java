1a76598a85826752993869c45734bf5f1463d549
Edwin Shin <edwin.shin@yourmediashelf.com>
2006-09-30 11:58:04 +0000

    initial commit (just empty stub for moment)

---
+++ src/test/junit/fedora/client/utility/ingest/TestIngest.java
@@ -0,0 +1,17 @@
+package fedora.client.utility.ingest;
+
+import fedora.test.FedoraTestCase;
+
+public class TestIngest extends FedoraTestCase {
+	public void setUp() throws Exception {
+
+	}
+	
+	public void tearDown() throws Exception {
+		
+	}
+	
+	public void testMultiFromDirectory() throws Exception {
+
+	}
+}
