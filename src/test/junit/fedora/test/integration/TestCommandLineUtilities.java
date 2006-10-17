98ded736ed56132963f82193978823021ae0b56d
Robert Haschart <rh9ec@virginia.edu>
2006-10-17 18:32:36 +0000

    Code fixed to work with changes to junit tests

--- src/test/junit/fedora/test/integration/TestCommandLineUtilities.java
+++ src/test/junit/fedora/test/integration/TestCommandLineUtilities.java
@@ -3,11 +3,15 @@ package fedora.test.integration;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 
+import org.custommonkey.xmlunit.SimpleXpathEngine;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import fedora.client.FedoraClient;
 import fedora.server.management.FedoraAPIM;
+import fedora.test.DemoObjectTestSetup;
 import fedora.test.FedoraServerTestCase;
+import fedora.test.api.TestXACMLPolicies;
 import fedora.utilities.ExecUtility;
 
 /**
@@ -21,56 +25,38 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
     static TestCommandLineUtilities curTest = null;
     public static Test suite() 
     {
-        TestSuite suite = new TestSuite(TestCommandLineUtilities.class);
-        /*
-        TestSetup wrapper = new FedoraServerTestSetup(suite) 
-        {
-            public void setUp() throws Exception 
-            {
-                TestIngestDemoObjects.ingestDemoObjects();
-                sbOut = new ByteArrayOutputStream();
-                sbErr = new ByteArrayOutputStream();
-            }
-            
-            public void tearDown() throws Exception 
-            {
-                sbOut = null;
-                sbErr = null;
-                TestIngestDemoObjects.purgeDemoObjects();
-            }
-        };
-        return new FedoraServerTestSetup(wrapper);
-        */
-        return suite;
-                
+        TestSuite suite = new TestSuite("Command Line Utilities TestSuite");
+        suite.addTestSuite(TestCommandLineUtilities.class);
+        return new DemoObjectTestSetup(suite);
     }
     
     public void testFedoraPurgeAndIngest() 
     {
         System.out.println("Purging object demo:5");
+        System.out.println("FEDORA-HOME = " + FEDORA_HOME);
         purgeUsingScript("demo:5");
         assertEquals(0, sbErr.size());
         System.out.println("Re-ingesting object demo:5");
-        ingestFoxmlFile(new File("src/demo-objects/foxml/local-server-demos/simple-image-demo/obj_demo_5.xml"));
+        ingestFoxmlFile(new File(FEDORA_HOME + "/client/demo/foxml/local-server-demos/simple-image-demo/obj_demo_5.xml"));
         String out = sbOut.toString();
         String err = sbErr.toString();
-        assertEquals(out.indexOf("Ingested PID: demo:5")!= -1, true );
+        assertEquals(out.indexOf("Ingested PID: demo:5")!= -1, true );        
         System.out.println("Purge and ingest test succeeded");
     }
     
     public void testBatchBuildAndBatchIngestAndPurge() throws Exception
     {
         System.out.println("Building batch objects");
-        batchBuild(new File("dist/client/demo/batch-demo/foxml-template.xml"),
-                   new File("dist/client/demo/batch-demo/object-specifics"),
-                   new File("dist/client/demo/batch-demo/objects"),
-                   new File("dist/client/logs/build.log"));
+        batchBuild(new File(FEDORA_HOME + "/client/demo/batch-demo/foxml-template.xml"),
+                   new File(FEDORA_HOME + "/client/demo/batch-demo/object-specifics"),
+                   new File(FEDORA_HOME + "/client/demo/batch-demo/objects"),
+                   new File(FEDORA_HOME + "/client/logs/build.log"));
         String out = sbOut.toString();
         String err = sbErr.toString();
         assertEquals(err, true, err.indexOf("10 Fedora FOXML XML documents successfully created")!= -1);
         System.out.println("Ingesting batch objects");
-        batchIngest(new File("dist/client/demo/batch-demo/objects"), 
-                    new File("dist/client/logs/ingest.log"));
+        batchIngest(new File(FEDORA_HOME + "/client/demo/batch-demo/objects"), 
+                    new File(FEDORA_HOME + "/client/logs/ingest.log"));
         out = sbOut.toString();
         err = sbErr.toString();
         assertEquals(err.indexOf("10 objects successfully ingested into Fedora")!= -1, true ); 
@@ -84,10 +70,10 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
     public void testBatchBuildIngestAndPurge() throws Exception
     {
         System.out.println("Building and Ingesting batch objects");
-        batchBuildIngest(new File("dist/client/demo/batch-demo/foxml-template.xml"),
-                   new File("dist/client/demo/batch-demo/object-specifics"),
-                   new File("dist/client/demo/batch-demo/objects"),
-                   new File("dist/client/logs/buildingest.log"));
+        batchBuildIngest(new File(FEDORA_HOME + "/client/demo/batch-demo/foxml-template.xml"),
+                   new File(FEDORA_HOME + "/client/demo/batch-demo/object-specifics"),
+                   new File(FEDORA_HOME + "/client/demo/batch-demo/objects"),
+                   new File(FEDORA_HOME + "/client/logs/buildingest.log"));
         String out = sbOut.toString();
         String err = sbErr.toString();
         assertEquals("Response did not contain expected string re: FOXML XML documents: <reponse>" + err + "</response>", err.indexOf("10 Fedora FOXML XML documents successfully created")!= -1, true );
@@ -102,32 +88,34 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
     public void testBatchModify() throws Exception 
     {
         System.out.println("Running batch modify of objects");
-        batchModify(new File("dist/client/demo/batch-demo/modify-batch-directives.xml"),
-                    new File("dist/client/logs/modify.log"));
+        batchModify(new File(FEDORA_HOME + "/client/demo/batch-demo/modify-batch-directives.xml"),
+                    new File(FEDORA_HOME + "/client/logs/modify.log"));
         String out = sbOut.toString();
         String err = sbErr.toString();
-        assertEquals(err, true, out.indexOf("24 modify directives successfully processed.")!= -1);
-        assertEquals(err, true, out.indexOf("0 modify directives failed.")!= -1);
+//        System.out.println(" out = " + out);
+//        System.out.println(" err = " + err);
+        assertEquals(false, out.indexOf("24 modify directives successfully processed.")== -1);
+        assertEquals(false, out.indexOf("0 modify directives failed.")== -1);
         System.out.println("Purging batch modify object");
         purgeFast("demo:32");
         System.out.println("Batch modify test succeeded");
     }
-    
+   
     public void testExport()
     {
         System.out.println("Testing fedora-export");
-        File outFile = new File("dist/client/demo/batch-demo/demo_5.xml");
+        File outFile = new File(FEDORA_HOME + "/client/demo/batch-demo/demo_5.xml");
         String absPath = outFile.getAbsolutePath();
         if (outFile.exists())
         {
             outFile.delete();
         }
         System.out.println("Exporting object demo:5");
-        exportObj("demo:5", new File("dist/client/demo/batch-demo"));
+        exportObj("demo:5", new File(FEDORA_HOME + "/client/demo/batch-demo"));
         String out = sbOut.toString();
         String err = sbErr.toString();
         assertEquals(out.indexOf("Exported demo:5")!= -1, true );
-        File outFile2 = new File("dist/client/demo/batch-demo/demo_5.xml");
+        File outFile2 = new File(FEDORA_HOME + "/client/demo/batch-demo/demo_5.xml");
         String absPath2 = outFile2.getAbsolutePath();
         assertEquals(outFile2.exists(), true );
         System.out.println("Deleting exported file");
@@ -171,10 +159,10 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
         {
             System.out.println("Checking "+(expectValid ? "valid" : "invalid") +" policy: "+
                                testFiles[i].getName());
-            execute("/server/bin/validate-policy", testFiles[i].getAbsolutePath());
+            execute(FEDORA_HOME+ "/server/bin/validate-policy", testFiles[i].getAbsolutePath());
             String out = sbOut.toString();
             String err = sbErr.toString();
-             
+            
             if (expectValid)
             {
                 assertEquals(err.length() == 0, true);
@@ -189,7 +177,7 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
     private void ingestFoxmlDirectory(File dir) 
     {
         //fedora-ingest f obj1.xml foxml1.0 myrepo.com:8443 jane jpw https
-        execute("/client/bin/fedora-ingest", "d " + dir.getAbsolutePath() + 
+        execute(FEDORA_HOME + "/client/bin/fedora-ingest", "d " + dir.getAbsolutePath() + 
                 " foxml1.0 DMO " + getHost() + ":" + getPort() + " " + getUsername() + 
                 " " + getPassword() + " " + getProtocol() + " \"junit ingest\"");
     }
@@ -197,14 +185,17 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
     private void ingestFoxmlFile(File f) 
     {
         //fedora-ingest f obj1.xml foxml1.0 myrepo.com:8443 jane jpw https
-        execute("/client/bin/fedora-ingest", "f " + f.getAbsolutePath() + 
+        
+    //    File exe = new File("client/bin/fedora-ingest");
+        execute(FEDORA_HOME + "/client/bin/fedora-ingest", "f " + f.getAbsolutePath() + 
                 " foxml1.0 " + getHost() + ":" + getPort() + " " + getUsername() + 
                 " " + getPassword() + " " + getProtocol() + " junit-ingest");
     }
     
     private static void purgeUsingScript(String pid) 
     {
-        execute("/client/bin/fedora-purge", getHost() + ":" + getPort() +
+    //    File exe = new File("client/bin/fedora-purge");
+        execute(FEDORA_HOME + "/client/bin/fedora-purge", getHost() + ":" + getPort() +
                 " " + getUsername() + " " + getPassword() + " " + pid + " " + 
                 getProtocol() + " junit-purge");
     }
@@ -232,21 +223,21 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
 
     private void batchBuild(File objectTemplateFile, File objectSpecificDir, File objectDir, File logFile)
     {
-        execute("/client/bin/fedora-batch-build", objectTemplateFile.getAbsolutePath() + " " + 
+        execute(FEDORA_HOME + "/client/bin/fedora-batch-build", objectTemplateFile.getAbsolutePath() + " " + 
                 objectSpecificDir.getAbsolutePath() + " " + objectDir.getAbsolutePath() + " " + 
                 logFile.getAbsolutePath() + " text");
     }
     
     private void batchIngest(File objectDir, File logFile)
     {
-        execute("/client/bin/fedora-batch-ingest", objectDir.getAbsolutePath() + " " + 
+        execute(FEDORA_HOME + "/client/bin/fedora-batch-ingest", objectDir.getAbsolutePath() + " " + 
                 logFile.getAbsolutePath() + " text foxml1.0 " + getHost() + ":" + getPort() +
                 " " + getUsername() + " " + getPassword() + " " + getProtocol() );
     }
     
     private void batchBuildIngest(File objectTemplateFile, File objectSpecificDir, File objectDir, File logFile)
     {
-        execute("/client/bin/fedora-batch-buildingest", objectTemplateFile.getAbsolutePath() + " " + 
+        execute(FEDORA_HOME + "/client/bin/fedora-batch-buildingest", objectTemplateFile.getAbsolutePath() + " " + 
                 objectSpecificDir.getAbsolutePath() + " " + objectDir.getAbsolutePath() + " " + 
                 logFile.getAbsolutePath() + " text " + getHost() + ":" + getPort() +
                     " " + getUsername() + " " + getPassword() + " " + getProtocol() );
@@ -254,31 +245,47 @@ public class TestCommandLineUtilities extends FedoraServerTestCase
     
     private void batchModify(File batchDirectives, File logFile)
     {
-        execute("/client/bin/fedora-modify", getHost() + ":" + getPort() + " " + 
+        execute(FEDORA_HOME + "/client/bin/fedora-modify", getHost() + ":" + getPort() + " " + 
                 getUsername() + " " + getPassword() + " " + batchDirectives.getAbsolutePath() + " " + 
                 logFile.getAbsolutePath() + " " + getProtocol() );
     }
 
     private void exportObj(String pid, File dir)
     {
-        execute("/client/bin/fedora-export", getHost() + ":" + getPort() + " " + 
+        execute(FEDORA_HOME + "/client/bin/fedora-export", getHost() + ":" + getPort() + " " + 
                 getUsername() + " " + getPassword() + " " + pid + " foxml1.0 public " + 
                 dir.getAbsolutePath() + " " + getProtocol() );
     }
     
     public static void execute(String cmd, String args) 
     {
+ //       System.out.println("Executing cmd:" + cmd + " " + args);
         if (sbOut != null && sbErr != null)
         {
             sbOut.reset();
             sbErr.reset();
-            ExecUtility.execCommandLineUtility(FEDORA_HOME + cmd + " " + args, sbOut, sbErr);
+            ExecUtility.execCommandLineUtility(cmd + " " + args, sbOut, sbErr);
         }
         else
         {
-            ExecUtility.execCommandLineUtility(FEDORA_HOME + cmd + " " + args);
+            ExecUtility.execCommandLineUtility(cmd + " " + args);
         }
+ //       System.out.println("Executed cmd:" + cmd );
+   }
+
+    public void setUp() throws Exception {
+        
+        SimpleXpathEngine.registerNamespace("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc/");
+        SimpleXpathEngine.registerNamespace("uvalibadmin", "http://dl.lib.virginia.edu/bin/admin/admin.dtd/");
+        sbOut = new ByteArrayOutputStream();
+        sbErr = new ByteArrayOutputStream();
     }
+    
+    public void tearDown() 
+    {
+        SimpleXpathEngine.clearNamespaces();
+    }
+
     
     public static void main(String[] args) 
     {
