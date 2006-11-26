83c699d2ca5395f6cf1ada9df02739868b97b490
Chris Wilper <cwilper@duraspace.org>
2006-11-26 10:35:53 +0000

    unix versions of fedora-reload-policies and validate-policy work again

--- src/sh/server/validate-policy.sh
+++ src/sh/server/validate-policy.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
-# ----------------------------------------------------------------------
-# XACML Policy Validation script
-# ----------------------------------------------------------------------
 
-# ----------------------------------------------------------------------
-# Environment setup
+if [ -z "$JAVA_HOME" ]; then
+    echo "ERROR: Environment variable, JAVA_HOME must be set."
+    exit 1
+fi
 
-# Cannot proceed if FEDORA_HOME is not set
 if [ -z "$FEDORA_HOME" ]; then
-  echo "ERROR: The FEDORA_HOME environment variable is not defined."
-  exit 1
+    echo "ERROR: Environment variable, FEDORA_HOME must be set."
+    exit 1
 fi
 
-if [ -r "$FEDORA_HOME"/server/bin/set-env.sh ]; then
-  . "$FEDORA_HOME"/server/bin/set-env.sh
+if [ -z "$CATALINA_HOME" ]; then
+    echo "ERROR: Environment variable, CATALINA_HOME must be set."
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "ERROR: Must supply filename."
+    exit 1
+fi
+
+_CP=$CATALINA_HOME/webapps/fedora/WEB-INF/classes
+_ED=$CATALINA_HOME/webapps/fedora/WEB-INF/lib:$CATALINA_HOME/common/endorsed:$CATALINA_HOME/common/lib
+
+(exec "$JAVA_HOME/bin/java" -cp "$_CP" -Djava.endorsed.dirs="$_ED" -Dfedora.home="$FEDORA_HOME" \
+    -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl \
+    -Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl \
+    -Dcom.sun.xacml.PolicySchema="$FEDORA_HOME/server/xsd/cs-xacml-schema-policy-01.xsd" \
+    fedora.server.security.ValidatePolicy "$1")
+
+if [ $? -eq 0 ]; then
+    echo "Validation successful"
+    exit 0
 else
-  echo "ERROR: $FEDORA_HOME/server/bin/set-env.sh was not found."
-  exit 1
+    echo "Validation failed"
+    exit 1
 fi
-
-TOMCAT_DIR=@tomcat.basename@
-TC="$FEDORA_HOME"/server/"$TOMCAT_DIR"
-TC_COMMON="$TC/common"
-
-echo "Starting XACML Policy Validation script..."
-(exec "$JAVA" -cp "$TC"/webapps/fedora/WEB-INF/classes:"$TC_COMMON"/lib/sunxacml.jar:"$TC_COMMON"/lib/xercesImpl.jar:"$TC_COMMON"/lib/xml-apis.jar \
-              -Dfedora.home="$FEDORA_HOME" \
-              -Dtomcat.dir="$TOMCAT_DIR"  \
-              -Dcom.sun.xacml.PolicySchema="$FEDORA_HOME"/server/xsd/cs-xacml-schema-policy-01.xsd \
-              -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl \
-              fedora.server.security.ValidatePolicy "$1")
-
-restoreJavaHome
