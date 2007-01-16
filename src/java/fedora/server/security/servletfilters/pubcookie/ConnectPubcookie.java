ae99f68eadb18a9ce1d60f276cc0baa6c3d37da0
Bill Niebel <wdn5e@virginia.edu>
2007-01-16 17:46:51 +0000

    backup development state

--- src/java/fedora/server/security/servletfilters/pubcookie/ConnectPubcookie.java
+++ src/java/fedora/server/security/servletfilters/pubcookie/ConnectPubcookie.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpState;
 import org.apache.commons.httpclient.Cookie;
+//import org.apache.commons.httpclient.CookiePolicy;
 import org.apache.commons.httpclient.HttpMethodBase;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
@@ -37,7 +38,8 @@ public class ConnectPubcookie {
 	private boolean completedFully = false;
 	private Node responseDocument = null;
 	private Cookie[] responseCookies = null;
-
+	Header[] responseCookies2 = null;
+	
 	public final boolean completedFully() {
 		return completedFully;
 	}
@@ -47,6 +49,10 @@ public class ConnectPubcookie {
 	}
 	
 	public final Cookie[] getResponseCookies() {
+		log.debug(this.getClass().getName() + ".getResponseCookies() " + "cookies are:");		
+		for (int i=0; i<responseCookies.length; i++) {
+			log.debug(this.getClass().getName() + ".getResponseCookies() " + "cookie==" + responseCookies[i]);		
+		}
 		return responseCookies;
 	}
 	
@@ -96,6 +102,7 @@ public class ConnectPubcookie {
 			//XXX MultipartRequestEntity multipartRequestEntity = new MultipartRequestEntity(parts, method.getParams());
 			// ((PostMethod)method).setRequestEntity(multipartRequestEntity); //new way			
 		}
+		//method.getParams().setCookiePolicy(CookiePolicy.RFC_2109);
 		HttpState state = client.getState();
 		for (int i=0; i < requestCookies.length; i++) {
 			Cookie cookie = requestCookies[i];
@@ -110,12 +117,13 @@ public class ConnectPubcookie {
 	}
 
 	public final void connect(String urlString, Map requestParameters, Cookie[] requestCookies, String truststoreLocation, String truststorePassword) {
-		log.fatal(this.getClass().getName() + ".connect() " + " url==" + urlString + " requestParameters==" + requestParameters + " requestCookies==" + requestCookies);		
+		log.debug(this.getClass().getName() + ".connect() " + " url==" + urlString + " requestParameters==" + requestParameters + " requestCookies==" + requestCookies);		
+		responseCookies2 = null;
 		URL url = null;
 		try {
 			url = new URL(urlString);
 		} catch (MalformedURLException mue) {
-			log.fatal(this.getClass().getName() + ".connect() " + "bad configured url==" + urlString);
+			log.error(this.getClass().getName() + ".connect() " + "bad configured url==" + urlString);
 		}
 		
 		if (urlString.startsWith("https:") 
@@ -163,25 +171,26 @@ public class ConnectPubcookie {
 		*/
 		
 		HttpClient client = new HttpClient();
-		log.fatal(this.getClass().getName() + ".connect() " + " b4 calling setup");		
+		log.debug(this.getClass().getName() + ".connect() " + " b4 calling setup");		
+		log.debug(this.getClass().getName() + ".connect() requestCookies==" + requestCookies);
 		HttpMethodBase method = setup(client, url, requestParameters, requestCookies);
-		log.fatal(this.getClass().getName() + ".connect() " + " after calling setup");	
+		log.debug(this.getClass().getName() + ".connect() " + " after calling setup");	
 		int statusCode = 0;
 		try {
-			log.fatal(this.getClass().getName() + ".connect() " + " b4 calling executeMethod");	
+			log.debug(this.getClass().getName() + ".connect() " + " b4 calling executeMethod");	
 			client.executeMethod(method);
-			log.fatal(this.getClass().getName() + ".connect() " + " after calling executeMethod");	
+			log.debug(this.getClass().getName() + ".connect() " + " after calling executeMethod");	
 			statusCode = method.getStatusCode();
-			log.fatal(this.getClass().getName() + ".connect() " + "(with configured url) statusCode==" + statusCode);
+			log.debug(this.getClass().getName() + ".connect() " + "(with configured url) statusCode==" + statusCode);
 		} catch (Exception e) {
-			log.fatal(this.getClass().getName() + ".connect() " + "failed original connect, url==" + urlString);
-			log.debug(e);
-			log.debug(e.getMessage());
-			if (e.getCause() != null) log.debug(e.getCause().getMessage());
+			log.error(this.getClass().getName() + ".connect() " + "failed original connect, url==" + urlString);
+			log.error(e);
+			log.error(e.getMessage());
+			if (e.getCause() != null) log.error(e.getCause().getMessage());
 			e.printStackTrace();
 		}
 
-		log.fatal(this.getClass().getName() + ".connect() " + " status code==" + statusCode);		
+		log.debug(this.getClass().getName() + ".connect() " + " status code==" + statusCode);		
 
 		if (302 == statusCode) {
 			Header redirectHeader = method.getResponseHeader("Location");
@@ -193,35 +202,35 @@ public class ConnectPubcookie {
 						redirectURL = new URL(redirectString);
 						method = setup(client, redirectURL, requestParameters, requestCookies);
 					} catch (MalformedURLException mue) {
-						log.fatal(this.getClass().getName() + ".connect() " + "bad redirect, url==" + urlString);
+						log.error(this.getClass().getName() + ".connect() " + "bad redirect, url==" + urlString);
 					}
 					statusCode = 0;
 					try {
 						client.executeMethod(method);
 						statusCode = method.getStatusCode();
-						log.fatal(this.getClass().getName() + ".connect() " + "(on redirect) statusCode==" + statusCode);
+						log.debug(this.getClass().getName() + ".connect() " + "(on redirect) statusCode==" + statusCode);
 					} catch (Exception e) {
-						log.fatal(this.getClass().getName() + ".connect() " + "failed redirect connect");
+						log.error(this.getClass().getName() + ".connect() " + "failed redirect connect");
 					}
 				}
 			}
 		}
 		if (statusCode == 200) { // this is either the original, non-302, status code or the status code after redirect
-			log.fatal(this.getClass().getName() + ".connect() " + "status code 200");
+			log.debug(this.getClass().getName() + ".connect() " + "status code 200");
 			String content = null;
 			try {
-			    log.fatal(this.getClass().getName() + ".connect() " + "b4 gRBAS()");
+			    log.debug(this.getClass().getName() + ".connect() " + "b4 gRBAS()");
 				content = method.getResponseBodyAsString();
-				log.fatal(this.getClass().getName() + ".connect() " + "after gRBAS() content==" + content);
+				log.debug(this.getClass().getName() + ".connect() " + "after gRBAS() content==" + content);
 			} catch (IOException e) {
 				log.error(this.getClass().getName() + ".connect() " + "couldn't get content");
 				return;
 			}
 			if (content == null) {
-				log.fatal(this.getClass().getName() + ".connect() content==null");
+				log.error(this.getClass().getName() + ".connect() content==null");
 				return;
 			} else {
-				log.fatal(this.getClass().getName() + ".connect() content != null, about to new Tidy");
+				log.debug(this.getClass().getName() + ".connect() content != null, about to new Tidy");
 				Tidy tidy = null;
 				try {
 					tidy = new Tidy();
@@ -231,26 +240,31 @@ public class ConnectPubcookie {
 					log.debug(t.getMessage());
 					if (t != null) log.debug(t.getCause().getMessage());
 				}
-				log.fatal(this.getClass().getName() + ".connect() after newing Tidy, tidy==" + tidy);
+				log.debug(this.getClass().getName() + ".connect() after newing Tidy, tidy==" + tidy);
 				byte[] inputBytes = content.getBytes();
-				log.fatal(this.getClass().getName() + ".connect() A1");
+				log.debug(this.getClass().getName() + ".connect() A1");
 				ByteArrayInputStream inputStream = new ByteArrayInputStream(inputBytes); 
-				log.fatal(this.getClass().getName() + ".connect() A2");
+				log.debug(this.getClass().getName() + ".connect() A2");
 				responseDocument = tidy.parseDOM(inputStream, null); //use returned root node as only output
-				log.fatal(this.getClass().getName() + ".connect() A3");
+				log.debug(this.getClass().getName() + ".connect() A3");
 			}
-			log.fatal(this.getClass().getName() + ".connect() " + "b4 getState()");
+			log.debug(this.getClass().getName() + ".connect() " + "b4 getState()");
 			HttpState state = client.getState();
-			log.fatal(this.getClass().getName() + ".connect() state==" + state);
+			log.debug(this.getClass().getName() + ".connect() state==" + state);
 	        try {
+	        	responseCookies2 = method.getRequestHeaders();
+				log.debug(this.getClass().getName() + ".connect() just got headers");
+				for (int i=0; i<responseCookies2.length; i++) {
+					log.debug(this.getClass().getName() + ".connect() header==" + responseCookies2[i]);
+				}
 				responseCookies = state.getCookies();
-				log.fatal(this.getClass().getName() + ".connect() responseCookies==" + responseCookies);
+				log.debug(this.getClass().getName() + ".connect() responseCookies==" + responseCookies);
 	        } catch (Throwable t) {
-				log.fatal(this.getClass().getName() + ".connect() exception==" + t.getMessage());
-				if (t.getCause() != null) log.fatal(this.getClass().getName() + ".connect() cause==" + t.getCause().getMessage());
+				log.error(this.getClass().getName() + ".connect() exception==" + t.getMessage());
+				if (t.getCause() != null) log.error(this.getClass().getName() + ".connect() cause==" + t.getCause().getMessage());
 	        }
 			completedFully = true;
-			log.fatal(this.getClass().getName() + ".connect() completedFully==" + completedFully);
+			log.debug(this.getClass().getName() + ".connect() completedFully==" + completedFully);
 		}
 	}
 	
