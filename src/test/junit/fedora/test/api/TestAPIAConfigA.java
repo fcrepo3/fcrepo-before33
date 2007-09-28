433b6df9d0550cd52313d47e977a83ab7b803710
Chris Wilper <cwilper@duraspace.org>
2007-09-28 11:54:15 +0000

    Updated system tests: 1) Added test configuration C for "remote" dissemination service testing.    The intent of this change is to make routine system testing easier for    developers:    Configurations A and B can now be run without any dependencies on    external hosts, or any need to be externally accessible. Configuration C    will only run those tests (currently only one) that rely on external    access/accessibility. 2) Uncommented chained dissemination test and moved it to TestConfigurationA    suite, since having this coverage is good, but dissemination chaining    currently only works properly when authentication is disabled on APi-A.

---
+++ src/test/junit/fedora/test/api/TestAPIAConfigA.java
@@ -0,0 +1,44 @@
+package fedora.test.api;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import fedora.client.FedoraClient;
+
+import fedora.server.access.FedoraAPIA;
+import fedora.server.types.gen.MIMETypedStream;
+import fedora.server.types.gen.Property;
+
+import fedora.test.DemoObjectTestSetup;
+import fedora.test.FedoraServerTestCase;
+
+/**
+ * Test API-A SOAP in configuration A (Authentication disabled on API-A).
+ * 
+ * @author cwilper@cs.cornell.edu
+ */
+public class TestAPIAConfigA extends FedoraServerTestCase {
+
+	private FedoraAPIA apia;
+	
+	public static Test suite() {
+		TestSuite suite = new TestSuite("APIAConfigA TestSuite");
+		suite.addTestSuite(TestAPIAConfigA.class);
+		return new DemoObjectTestSetup(suite);
+	}
+	
+	public void testGetChainedDissemination() throws Exception {
+		// test chained dissemination using local bmech services
+		// The object contains an E datastream that is a dissemination of the local SAXON service.
+		// This datastream is input to another dissemination that uses the local FOP service.
+		MIMETypedStream diss = apia.getDissemination("demo:26", "demo:19", "getPDF", new Property[0], null);
+		assertEquals(diss.getMIMEType(),"application/pdf");
+		assertTrue(diss.getStream().length > 0);
+    }
+	
+	public void setUp() throws Exception {
+		FedoraClient client = getFedoraClient();
+		apia = client.getAPIA();
+    }
+	
+}
---
+++ src/test/junit/fedora/test/api/TestAPIALiteConfigA.java
@@ -0,0 +1,40 @@
+package fedora.test.api;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import fedora.client.FedoraClient;
+import fedora.client.HttpInputStream;
+
+import fedora.test.DemoObjectTestSetup;
+import fedora.test.FedoraServerTestCase;
+
+/**
+ * Test API-A Lite in configuration A (Authentication disabled on API-A).
+ * 
+ * @author cwilper@cs.cornell.edu
+ */
+public class TestAPIALiteConfigA extends FedoraServerTestCase {
+
+    private static FedoraClient client;
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite("APIALiteConfigA TestSuite");
+		suite.addTestSuite(TestAPIALiteConfigA.class);
+		return new DemoObjectTestSetup(suite);
+    }
+    
+    public void testGetChainedDissemination() throws Exception {
+		// test chained dissemination using local bmech services
+		// The object contains an E datastream that is a dissemination of the local SAXON service.
+		// This datastream is input to another dissemination that uses the local FOP service.
+    	HttpInputStream his = client.get("/get/demo:26/demo:19/getPDF", false);
+    	assertEquals(his.getContentType(),"application/pdf");    	
+    	his.close();
+    }
+    
+    public void setUp() throws Exception {
+        client = getFedoraClient();
+    }
+    
+}
