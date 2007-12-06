732ab6f31dfd811597344d1b23df43f77443ddb8
Edwin Shin <edwin.shin@yourmediashelf.com>
2007-12-06 16:57:00 +0000

    enable Fedora to build using Java 6 (backported from trunk).

--- src/java/fedora/server/utilities/ConnectionWrapper.java
+++ src/java/fedora/server/utilities/ConnectionWrapper.java
@@ -21,10 +21,7 @@ public abstract class ConnectionWrapper
         m_wrappedConnection=wrapped;
     }
 
-/*  
-    NOTE: To compile with JDK 1.6, uncomment the following methods
-
-    ///// start of 1.6 methods /////
+    /* JDBC_4_ANT_TOKEN_BEGIN -
 
     public boolean isWrapperFor(Class<?> iface) {
         throw new UnsupportedOperationException(
@@ -91,9 +88,7 @@ public abstract class ConnectionWrapper
                 "Java 1.6 Connection methods are not supported");
     }
 
-    ///// end of 1.6 methods /////
-
-    */
+    - JDBC_4_ANT_TOKEN_END */
 
     public Statement createStatement()
             throws SQLException {
