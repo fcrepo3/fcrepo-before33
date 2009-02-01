e9b4fe8ca535d15c9c8d88f249f077cbafc76341
Chris Wilper <cwilper@duraspace.org>
2009-02-01 06:46:23 +0000

    saving all server logs for each systest as build artifact

--- src/test/scripts/sanity.sh
+++ src/test/scripts/sanity.sh
@@ -43,6 +43,9 @@ echo "Running sanity system tests"
 echo "==========================="
 echo ""
 
+# Where to put server log artifacts after each sys test
+mkdir $BUILD_HOME/build/server-logs
+
 #
 # Config B Tests
 #
@@ -56,33 +59,29 @@ if [ $? -ne 0 ]; then
 fi
 
 $CATALINA_HOME/bin/startup.sh
-
 if [ $? -ne 0 ]; then
   echo ""
   echo "ERROR: Failed while starting Fedora for ConfigB tests; see above"
   exit 1
 fi
-
-echo "Waiting 30 seconds for Fedora to start..."
-sleep 30
-
+echo "Waiting 20 seconds for Fedora to start..."
+sleep 20
 echo ""
 echo "[Running ConfigB Tests...]"
-
 $SCRIPTDIR/systest.sh $1 -Dtest=fedora.test.AllSystemTestsConfigB
-
 if [ $? -ne 0 ]; then
   echo ""
   echo "ERROR: Failed ConfigB tests; see above"
   echo "Shutting down Tomcat..."
   $CATALINA_HOME/bin/shutdown.sh
   sleep 5
+  mv $FEDORA_HOME/server/logs $BUILD_HOME/build/server-logs/fedora.test.AllSystemTestsConfigB
   exit 1
 fi
-
 echo "Shutting down tomcat..."
 $CATALINA_HOME/bin/shutdown.sh
 sleep 5
+mv $FEDORA_HOME/server/logs $BUILD_HOME/build/server-logs/fedora.test.AllSystemTestsConfigB
 
 #
 # End of Config B Tests
@@ -101,48 +100,54 @@ if [ $? -ne 0 ]; then
 fi
 
 $CATALINA_HOME/bin/startup.sh
-
 if [ $? -ne 0 ]; then
   echo ""
-  echo "ERROR: Failed while starting Fedora for ConfigA and ConfigC tests; see above"
+  echo "ERROR: Failed while starting Fedora for ConfigA tests; see above"
   exit 1
 fi
-
-echo "Waiting 30 seconds for Fedora to start..."
-sleep 30
-
+echo "Waiting 20 seconds for Fedora to start..."
+sleep 20
 echo ""
 echo "[Running ConfigA Tests...]"
-
 $SCRIPTDIR/systest.sh $1 -Dtest=fedora.test.AllSystemTestsConfigA
-
 if [ $? -ne 0 ]; then
   echo ""
   echo "ERROR: Failed ConfigA tests; see above"
   echo "Shutting down Tomcat..."
   $CATALINA_HOME/bin/shutdown.sh
   sleep 5
+  mv $FEDORA_HOME/server/logs $BUILD_HOME/build/server-logs/fedora.test.AllSystemTestsConfigA
   exit 1
 fi
+echo "Shutting down tomcat..."
+$CATALINA_HOME/bin/shutdown.sh
+sleep 5
+mv $FEDORA_HOME/server/logs $BUILD_HOME/build/server-logs/fedora.test.AllSystemTestsConfigA
 
+$CATALINA_HOME/bin/startup.sh
+if [ $? -ne 0 ]; then
+  echo ""
+  echo "ERROR: Failed while starting Fedora for ConfigA tests; see above"
+  exit 1
+fi
+echo "Waiting 20 seconds for Fedora to start..."
+sleep 20
 echo ""
-echo "[Running ConfigB Tests...]"
-
+echo "[Running ConfigC Tests...]"
 $SCRIPTDIR/systest.sh $1 -Dtest=fedora.test.AllSystemTestsConfigC
-
 if [ $? -ne 0 ]; then
   echo ""
   echo "ERROR: Failed ConfigC tests; see above"
   echo "Shutting down Tomcat..."
   $CATALINA_HOME/bin/shutdown.sh
   sleep 5
+  mv $FEDORA_HOME/server/logs $BUILD_HOME/build/server-logs/fedora.test.AllSystemTestsConfigC
   exit 1
 fi
-
 echo "Shutting down tomcat..."
 $CATALINA_HOME/bin/shutdown.sh
 sleep 5
-
+mv $FEDORA_HOME/server/logs $BUILD_HOME/build/server-logs/fedora.test.AllSystemTestsConfigC
 
 #
 # End of Config A and C Tests
