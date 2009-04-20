73b8f7fa5c12787e31c2daa38b1e8733866d7946
Chris Wilper <cwilper@duraspace.org>
2009-04-20 23:34:06 +0000

    javadoc'd ILowlevelStorage

--- src/java/fedora/server/storage/lowlevel/ILowlevelStorage.java
+++ src/java/fedora/server/storage/lowlevel/ILowlevelStorage.java
@@ -1,5 +1,5 @@
 /* The contents of this file are subject to the license and copyright terms
- * detailed in the license directory at the root of the source tree (also 
+ * detailed in the license directory at the root of the source tree (also
  * available online at http://www.fedora.info/license/).
  */
 
@@ -10,89 +10,150 @@ import java.io.InputStream;
 import fedora.server.errors.LowlevelStorageException;
 
 /**
- * ILowlevelStorage.java
- * 
+ * Provides read/write access to all serialized objects and managed datastream
+ * content within Fedora.
+ *
  * @author Bill Niebel
  */
 public interface ILowlevelStorage {
 
     /**
-     * @param pid
-     * @param content
-     * @throws LowlevelStorageException
+     * Adds a new object.
+     *
+     * @param pid the pid of the object.
+     * @param content the serialized object.
+     * @throws LowlevelStorageException if the object already exists or
+     *         cannot be added for any other reason.
      */
     public void addObject(String pid, InputStream content)
             throws LowlevelStorageException;
 
     /**
-     * @param pid
-     * @param content
-     * @throws LowlevelStorageException
+     * Replaces an existing object.
+     *
+     * @param pid the pid of the object.
+     * @param content the serialized object.
+     * @throws LowlevelStorageException if the object does not already exist
+     *         or cannot be replaced for any other reason.
      */
     public void replaceObject(String pid, InputStream content)
             throws LowlevelStorageException;
 
     /**
-     * @param pid
-     * @return bytestream containing data object
-     * @throws LowlevelStorageException
+     * Gets an existing object.
+     *
+     * @param pid the pid of the object.
+     * @return the serialized form of the object, as stored.
+     * @throws LowlevelStorageException if the object does not exist or
+     *         cannot be read for any other reason.
      */
     public InputStream retrieveObject(String pid)
             throws LowlevelStorageException;
 
     /**
-     * @param pid
-     * @throws LowlevelStorageException
+     * Removes an object.
+     *
+     * @param pid the pid of the object.
+     * @throws LowlevelStorageException if the object does not exist or
+     *         cannot be removed for any other reason.
      */
     public void removeObject(String pid) throws LowlevelStorageException;
 
     /**
-     * @throws LowlevelStorageException
+     * Reconstructs the object index if such an index exists. The object index
+     * associates an object with a stored location. If the implementation does
+     * not use an index, this is a no-op.
+     * <p>
+     * <h2>Warning</h2>
+     * Rebuilding the object index is not expected to be an atomic operation
+     * and should only be run while the system is offline or reads and writes
+     * are otherwise prevented.
+     *
+     * @throws LowlevelStorageException if an error occurs that prevents the
+     *         index from being rebuilt.
      */
     public void rebuildObject() throws LowlevelStorageException;
 
     /**
-     * @throws LowlevelStorageException
+     * Performs a consistency check against the object index if such an index
+     * exists. The object index associates an object with a stored location.
+     * If the implementation does not use an index, this is a no-op. If any
+     * inconsistencies are found, they will be reported to the system log.
+     *
+     * @throws LowlevelStorageException if an error occurs that prevents the
+     *         consistency check from taking place.
      */
     public void auditObject() throws LowlevelStorageException;
 
     /**
-     * @param pid
-     * @param content
-     * @throws LowlevelStorageException
+     * Sets the content of a new datastream version.
+     *
+     * @param key the $pid "+" $dsId "+" $dsVersionId string that uniquely
+     *        identifies the datastream version.
+     * @param content the content.
+     * @throws LowlevelStorageException if the datastream version already
+     *         exists or cannot be added for any other reason.
      */
-    public void addDatastream(String pid, InputStream content)
+    public void addDatastream(String key, InputStream content)
             throws LowlevelStorageException;
 
     /**
-     * @param pid
-     * @param content
-     * @throws LowlevelStorageException
+     * Sets the content of an existing datastream version.
+     *
+     * @param key the $pid "+" $dsId "+" $dsVersionId string that uniquely
+     *        identifies the datastream version.
+     * @param content the content.
+     * @throws LowlevelStorageException if the datastream version does not
+     *         already exist or cannot be replaced for any other reason.
      */
-    public void replaceDatastream(String pid, InputStream content)
+    public void replaceDatastream(String key, InputStream content)
             throws LowlevelStorageException;
 
     /**
-     * @param pid
-     * @return bytestream containing datastream
-     * @throws LowlevelStorageException
+     * Gets the content of an existing datastream version.
+     *
+     * @param key the $pid "+" $dsId "+" $dsVersionId string that uniquely
+     *        identifies the datastream version.
+     * @return the content.
+     * @throws LowlevelStorageException if the datastream version does not
+     *         exist or cannot be read for any other reason.
      */
-    public InputStream retrieveDatastream(String pid)
+    public InputStream retrieveDatastream(String key)
             throws LowlevelStorageException;
 
     /**
-     * @param pid
-     * @throws LowlevelStorageException
+     * Removes the content of an existing datastream version.
+     *
+     * @param key the $pid "+" $dsId "+" $dsVersionId string that uniquely
+     *        identifies the datastream version.
+     * @throws LowlevelStorageException if the datastream version does not
+     *         exist or cannot be removed for any other reason.
      */
-    public void removeDatastream(String pid) throws LowlevelStorageException;
+    public void removeDatastream(String key) throws LowlevelStorageException;
 
     /**
-     * @throws LowlevelStorageException
+     * Reconstructs the datastream index if such an index exists. The datastream
+     * index associates a datastream version with a stored location. If the
+     * implementation does not use an index, this is a no-op.
+     * <p>
+     * <h2>Warning</h2>
+     * Rebuilding the datastream index is not expected to be an atomic operation
+     * and should only be run while the system is offline or reads and writes
+     * are otherwise prevented.
+     *
+     * @throws LowlevelStorageException if an error occurs that prevents the
      */
     public void rebuildDatastream() throws LowlevelStorageException;
 
     /**
-     * @throws LowlevelStorageException
+     * Performs a consistency check against the datastream index if such an
+     * index exists. The datastream index associates a datastream version with
+     * a stored location. If the implementation does not use an index, this is
+     * a no-op. If any inconsistencies are found, they will be reported to the
+     * system log.
+     *
+     * @throws LowlevelStorageException if an error occurs that prevents the
+     *         consistency check from taking place.
      */
     public void auditDatastream() throws LowlevelStorageException;
 }
