a72ecd8375e100be95fc113e0123797cda493506
Daniel Davis <ddavis@fedora-commons.org>
2009-09-21 17:31:26 +0000

    Pushed down and resolved activemq to central.  Moved more libraries to org.fedorarepo.

--- resources/scripts/installLibs.bat
+++ resources/scripts/installLibs.bat
@@ -1,6 +1,5 @@
 REM Install libraries not in maven2 central repository
 
-call mvn install:install-file -Dfile=../../lib/activemq-all-5.1.0.jar -DgroupId=duraspace -DartifactId=activemq-all-5.1.0 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/akubra-core-0.1.jar -DgroupId=org.fedorarepo -DartifactId=akubra-core -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/akubra-fs-0.1.jar -DgroupId=org.fedorarepo -DartifactId=akubra-fs -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/akubra-map-0.1.jar -DgroupId=org.fedorarepo -DartifactId=akubra-map -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
@@ -12,7 +11,7 @@ call mvn install:install-file -Dfile=../../lib/GroboTestingJUnit-1.2.1-core.jar 
 call mvn install:install-file -Dfile=../../lib/ij.jar -DgroupId=org.fedorarepo -DartifactId=ij -Dversion=1.32 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/jai_codec.jar -DgroupId=org.fedorarepo -DartifactId=jai_codec -Dversion=1.1.2_01 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/jai_core.jar -DgroupId=org.fedorarepo -DartifactId=jai_core -Dversion=1.1.2_01 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../../lib/jakarta-oro-2.0.5.jar -DgroupId=duraspace -DartifactId=jakarta-oro-2.0.5 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=../../lib/jakarta-oro-2.0.5.jar -DgroupId=org.fedorarepo -DartifactId=jakarta-oro -Dversion=2.0.5 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/jmx.jar -DgroupId=duraspace -DartifactId=jmx -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/lucene-core-2.4.0.jar -DgroupId=duraspace -DartifactId=lucene-core-2.4.0 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/mptstore-0.9.1.jar -DgroupId=org.fedorarepo -DartifactId=mptstore -Dversion=0.9.1 -Dpackaging=jar -DgeneratePom=true
@@ -21,7 +20,7 @@ call mvn install:install-file -Dfile=../../lib/one-jar-boot-0.96.jar -DgroupId=o
 call mvn install:install-file -Dfile=../../lib/openrdf-sesame-2.2.1-onejar.jar -DgroupId=duraspace -DartifactId=openrdf-sesame-2.2.1-onejar -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/saxon9-dom.jar -DgroupId=duraspace -DartifactId=saxon9-dom -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/saxon9.jar -DgroupId=duraspace -DartifactId=saxon9 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../../lib/sunxacml-patched.jar -DgroupId=duraspace -DartifactId=sunxacml-patched -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=../../lib/sunxacml-patched.jar -DgroupId=org.fedorarepo -DartifactId=sunxacml -Dversion=1.2-PATCHED -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/Tidy.jar -DgroupId=org.fedorarepo -DartifactId=Tidy -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/trippi-1.4.1-core.jar -DgroupId=org.fedorarepo -DartifactId=trippi-core -Dversion=1.4.1 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/trippi-1.4.1-mptstore.jar -DgroupId=org.fedorarepo -DartifactId=trippi-mptstore -Dversion=1.4.1 -Dpackaging=jar -DgeneratePom=true
