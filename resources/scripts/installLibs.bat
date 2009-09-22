92c9d9e1e768f85e1bee909edefd11abc03c5507
Daniel Davis <ddavis@fedora-commons.org>
2009-09-22 14:14:36 +0000

    Reassigned the rest of the libraries to org.fedorarepo and moved them down.

--- resources/scripts/installLibs.bat
+++ resources/scripts/installLibs.bat
@@ -12,14 +12,14 @@ call mvn install:install-file -Dfile=../../lib/ij.jar -DgroupId=org.fedorarepo -
 call mvn install:install-file -Dfile=../../lib/jai_codec.jar -DgroupId=org.fedorarepo -DartifactId=jai_codec -Dversion=1.1.2_01 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/jai_core.jar -DgroupId=org.fedorarepo -DartifactId=jai_core -Dversion=1.1.2_01 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/jakarta-oro-2.0.5.jar -DgroupId=org.fedorarepo -DartifactId=jakarta-oro -Dversion=2.0.5 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../../lib/jmx.jar -DgroupId=duraspace -DartifactId=jmx -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../../lib/lucene-core-2.4.0.jar -DgroupId=duraspace -DartifactId=lucene-core-2.4.0 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM call mvn install:install-file -Dfile=../../lib/jmx.jar -DgroupId=duraspace -DartifactId=jmx -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM call mvn install:install-file -Dfile=../../lib/lucene-core-2.4.0.jar -DgroupId=duraspace -DartifactId=lucene-core-2.4.0 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/mptstore-0.9.1.jar -DgroupId=org.fedorarepo -DartifactId=mptstore -Dversion=0.9.1 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/mulgara-core-2.1.1.jar -DgroupId=org.fedorarepo -DartifactId=mulgara-core -Dversion=2.1.1 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/one-jar-boot-0.96.jar -DgroupId=org.fedorarepo -DartifactId=one-jar-boot -Dversion=0.96 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../../lib/openrdf-sesame-2.2.1-onejar.jar -DgroupId=duraspace -DartifactId=openrdf-sesame-2.2.1-onejar -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../../lib/saxon9-dom.jar -DgroupId=duraspace -DartifactId=saxon9-dom -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../../lib/saxon9.jar -DgroupId=duraspace -DartifactId=saxon9 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=../../lib/openrdf-sesame-2.2.1-onejar.jar -DgroupId=org.fedorarepo -DartifactId=openrdf-sesame-onejar -Dversion=2.2.1 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=../../lib/saxon9-dom.jar -DgroupId=org.fedorarepo -DartifactId=saxon-dom -Dversion=9.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=../../lib/saxon9.jar -DgroupId=org.fedorarepo -DartifactId=saxon -Dversion=9.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/sunxacml-patched.jar -DgroupId=org.fedorarepo -DartifactId=sunxacml -Dversion=1.2-PATCHED -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/Tidy.jar -DgroupId=org.fedorarepo -DartifactId=Tidy -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../../lib/trippi-1.4.1-core.jar -DgroupId=org.fedorarepo -DartifactId=trippi-core -Dversion=1.4.1 -Dpackaging=jar -DgeneratePom=true
